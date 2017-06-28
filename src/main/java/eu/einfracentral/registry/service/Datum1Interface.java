a2573fd896d1c0a74f2411090e7c923aa06f60d4
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-06-28 17:33:01 +0300

    Renamed first interface

--- src/main/java/eu/einfracentral/registry/service/Datum1Interface.java
+++ src/main/java/eu/einfracentral/registry/service/Datum1Interface.java
@@ -1,9 +1,12 @@
 package eu.einfracentral.registry.service;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by pgl on 27/6/2017.
  */
-public interface MyFirstInterface {
+
+public interface Datum1Interface {
 
     String get(String id);
 }
