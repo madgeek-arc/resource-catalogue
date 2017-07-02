a7c3a0c04a6cc88ff3f4b15bd9b2c639878d4a1d
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-03 01:43:26 +0300

    User expansion

--- src/main/java/eu/einfracentral/domain/aai/User.java
+++ src/main/java/eu/einfracentral/domain/aai/User.java
@@ -1,6 +1,10 @@
 package eu.einfracentral.domain.aai;
 
+import eu.einfracentral.domain.Provider;
+
 import javax.xml.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by pgl on 30/6/2017.
@@ -12,4 +16,22 @@ import javax.xml.bind.annotation.*;
 public class User {
     @XmlElement(required = true, nillable = false)
     private int id;
+
+    @XmlElement(required = true, nillable = false)
+    private Provider org;
+
+    @XmlElement(required = true, nillable = false)
+    private Role role;
+
+    @XmlElementWrapper(required = true, nillable = false)
+    @XmlElement(name = "grant")
+    private List<Grant> grants;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        throw new Error("No.");
+    }
 }
