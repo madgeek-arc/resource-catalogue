84d837584fd494f136c8dc3a4a9ad851676e280d
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-03 16:29:56 +0300

    Added solitary vocab class

---
+++ src/main/java/eu/einfracentral/domain/Vocabulary.java
@@ -0,0 +1,49 @@
+package eu.einfracentral.domain;
+
+import javax.xml.bind.annotation.*;
+
+/**
+ * Created by pgl on 3/7/2017.
+ */
+@XmlType(namespace = "http://einfracentral.eu", propOrder = {"id", "name", "parent"})
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
+public class Vocabulary {
+
+    @XmlElement(required = true)
+    private int id;
+
+    @XmlElement(required = true)
+    private String name;
+
+    @XmlElement
+    private String parent;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParent() {
+        return parent;
+    }
+
+    public void setParent(String parent) {
+        this.parent = parent;
+    }
+
+    //Categories: Storage, Computing, Networking, Data, DataManagement, Identification, Consultancy;
+    //Phases: Beta, Production;
+
+}
