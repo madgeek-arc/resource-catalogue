72872ab306398267a71a843f80e149218d75cbce
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-04 18:46:30 +0300

    Added ServiceService

---
+++ src/main/java/eu/einfracentral/registry/service/ServiceService.java
@@ -0,0 +1,11 @@
+package eu.einfracentral.registry.service;
+
+import eu.einfracentral.domain.Service;
+import eu.openminted.registry.service.ResourceCRUDService;
+
+/**
+ * Created by pgl on 27/6/2017.
+ */
+public interface ServiceService extends ResourceCRUDService<Service> {
+
+}
