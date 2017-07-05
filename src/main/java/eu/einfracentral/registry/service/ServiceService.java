68196b6c999cca0d3b1e12ade2ce6e369e50e4dd
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-05 17:20:39 +0300

    +uploadService

--- src/main/java/eu/einfracentral/registry/service/ServiceService.java
+++ src/main/java/eu/einfracentral/registry/service/ServiceService.java
@@ -3,9 +3,18 @@ package eu.einfracentral.registry.service;
 import eu.einfracentral.domain.Service;
 import eu.openminted.registry.service.ResourceCRUDService;
 
+import java.io.InputStream;
+
 /**
  * Created by pgl on 27/6/2017.
  */
 public interface ServiceService extends ResourceCRUDService<Service> {
-
+    /**
+     * Uploads a zipped service
+     *
+     * @param filename
+     * @param inputStream
+     * @return archive id where it was saved
+     */
+    public String uploadService(String filename, InputStream inputStream);
 }
