9eed54a24d9458fd5faf02a8c8dfe5d745e529e2
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-10 18:07:54 +0300

    Temp disabled performance

--- src/main/java/eu/einfracentral/domain/performance/Indicator.java
+++ src/main/java/eu/einfracentral/domain/performance/Indicator.java
@@ -1,46 +1,59 @@
 package eu.einfracentral.domain.performance;
 
-import javax.xml.bind.annotation.*;
-import javax.xml.datatype.XMLGregorianCalendar;
-import java.time.temporal.ChronoUnit;
-import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
 /**
  * Created by pgl on 30/6/2017.
  */
 
-@XmlType(namespace = "http://einfracentral.eu", propOrder = {"id", "description", "target", "timeGranularity", "measurements"})
+@XmlType(namespace = "http://einfracentral.eu", propOrder = {
+//    "id", "description", "targets", "timeGranularity", "measurements"
+})
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement
-public class Indicator<T extends Comparable<T>> {
-
-    @XmlElement(required = true)
-    private int id;
-
-    @XmlElement(required = true)
-    private String description;
-
-    @XmlElement(required = true)
-    private Target target;
-
-    @XmlElement(required = true)
-    private ChronoUnit timeGranularity;
-
-    @XmlElementWrapper(required = true)
-    @XmlElement(name = "measurement")
-    private List<Measurement<T>> measurements;
-
-    public boolean satisfiedWithin(XMLGregorianCalendar from, XMLGregorianCalendar to) {
-        for (Measurement<T> m : measurements) {
-
-            if (m.getTime().toGregorianCalendar().compareTo(from.toGregorianCalendar()) < 0 &&
-                m.getTime().toGregorianCalendar().compareTo(to.toGregorianCalendar()) > 0) {
-                continue;
-            }
-            if (!m.satisfies(target)) {
-                return false;
-            }
-        }
-        return true;
-    }
+public class Indicator
+//<T extends JAXBComparable<T>>
+{
+//
+//    @XmlElement(required = true)
+//    private int id;
+//
+//    @XmlElement(required = true)
+//    private String description;
+//
+//    @XmlElementWrapper(required = true)
+//    @XmlElement(name = "target")
+//    private List<Target<T>> targets;
+//
+//    @XmlElement(required = true)
+//    private ChronoUnit timeGranularity;
+//
+//    @XmlElementWrapper(required = true)
+//    @XmlElement(name = "measurement")
+//    private List<Measurement<T>> measurements;
+//
+//    public boolean satisfiedWithin(XMLGregorianCalendar from, XMLGregorianCalendar to) {
+//        boolean ret = false;
+//        for (Target<T> t : targets) {
+//            boolean allMeasurementsAreSatisfactoryForTarget = false;
+//            for (Measurement<T> m : measurements) {
+//                if (m.getTime().toGregorianCalendar().compareTo(from.toGregorianCalendar()) < 0 &&
+//                    m.getTime().toGregorianCalendar().compareTo(to.toGregorianCalendar()) > 0) {
+//                    allMeasurementsAreSatisfactoryForTarget = true;
+//                    allMeasurementsAreSatisfactoryForTarget &= t.satisfiedBy(m);
+//                }
+//                if (allMeasurementsAreSatisfactoryForTarget) {
+//                    ret = allMeasurementsAreSatisfactoryForTarget;
+//                    break;
+//                }
+//            }
+//            if (satisfied) {
+//                break;
+//            }
+//        }
+//        return satisfied;
+//    }
 }
--- src/main/java/eu/einfracentral/domain/performance/Measurement.java
+++ src/main/java/eu/einfracentral/domain/performance/Measurement.java
@@ -1,56 +1,59 @@
 package eu.einfracentral.domain.performance;
 
-import javax.xml.bind.annotation.*;
-import javax.xml.datatype.XMLGregorianCalendar;
-import java.time.Instant;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
 
 /**
  * Created by pgl on 30/6/2017.
  */
 
-@XmlType(namespace = "http://einfracentral.eu", propOrder = {"id", "time", "value"})
+@XmlType(namespace = "http://einfracentral.eu", propOrder = {
+//    "id", "time", "value"
+})
 @XmlAccessorType(XmlAccessType.FIELD)
-
-public class Measurement<T extends Comparable<T>> implements Comparable<T> {
-    @XmlElement(required = true)
-    private int id;
-
-    @XmlElement(required = true)
-    private XMLGregorianCalendar time;
-
-    @XmlElement(required = true)
-    private T value;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public XMLGregorianCalendar getTime() {
-        return time;
-    }
-
-    public void setTime(XMLGregorianCalendar time) {
-        this.time = time;
-    }
-
-    public T getValue() {
-        return value;
-    }
-
-    public void setValue(T value) {
-        this.value = value;
-    }
-
-    public boolean satisfies(Target<T> t) {
-        return t.satisfiedBy(this.value);
-    }
-
-    @Override
-    public int compareTo(T value) {
-        return this.value.compareTo(value);
-    }
+public class Measurement
+//    <T extends JAXBComparable>
+{
+//    @XmlElement(required = true)
+//    private int id;
+//
+//    @XmlElement(required = true)
+//    private XMLGregorianCalendar time;
+//
+//    @XmlElement(required = true)
+//    private T value;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public XMLGregorianCalendar getTime() {
+//        return time;
+//    }
+//
+//    public void setTime(XMLGregorianCalendar time) {
+//        this.time = time;
+//    }
+//
+//    public T getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(T value) {
+//        this.value = value;
+//    }
+//
+//    public boolean satisfies(Target<T> t) {
+//        return t.satisfiedBy(this.value);
+//    }
+//
+//    @Override
+//    public int compareTo(T value) {
+//        return this.value.compareTo(value);
+//    }
 }
