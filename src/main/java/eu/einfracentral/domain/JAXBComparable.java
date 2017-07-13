269b7312f2b03e94b0f5bc494002f14b79a05496
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-13 18:20:07 +0300

    Added placeholder Comparable class

---
+++ src/main/java/eu/einfracentral/domain/JAXBComparable.java
@@ -0,0 +1,19 @@
+package eu.einfracentral.domain;
+
+/**
+ * Created by pgl on 4/7/2017.
+ */
+public abstract class JAXBComparable
+//    <T extends Comparable<T>> implements Comparable<T>
+{
+
+//    private transient T value;
+//
+//    public T getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(T value) {
+//        this.value = value;
+//    }
+}
