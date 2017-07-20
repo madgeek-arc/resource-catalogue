d347d18e8080fc5e243bbab922a3fbdb2097e391
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-20 10:44:56 +0300

    Tweaked xmlns

--- src/main/java/eu/einfracentral/domain/package-info.java
+++ src/main/java/eu/einfracentral/domain/package-info.java
@@ -1,9 +1,9 @@
 @XmlSchema(
     namespace = "http://einfracentral.eu",
-    elementFormDefault = XmlNsForm.QUALIFIED,
-    xmlns = {
-        @XmlNs(prefix="tns", namespaceURI="http://einfracentral.eu")
-    }
+    elementFormDefault = XmlNsForm.QUALIFIED//,
+//    xmlns = {
+//        @XmlNs(prefix="tns", namespaceURI="http://einfracentral.eu")
+//    }
 )
 package eu.einfracentral.domain;
 
