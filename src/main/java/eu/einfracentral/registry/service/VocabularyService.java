7ba57939e9dec9e755f989460fee1b800c762484
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-24 19:07:15 +0300

    Added Vocabulary

---
+++ src/main/java/eu/einfracentral/registry/service/VocabularyService.java
@@ -0,0 +1,11 @@
+package eu.einfracentral.registry.service;
+
+import eu.einfracentral.domain.Vocabulary;
+import eu.openminted.registry.core.service.ResourceCRUDService;
+
+/**
+ * Created by pgl on 24/7/2017.
+ */
+public interface VocabularyService extends ResourceCRUDService<Vocabulary> {
+
+}
