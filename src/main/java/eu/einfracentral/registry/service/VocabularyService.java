e3e7a059876574a867d36ada9a2d0eb5e39802d9
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-25 16:12:34 +0300

    Wired VocabularyService

--- src/main/java/eu/einfracentral/registry/service/VocabularyService.java
+++ src/main/java/eu/einfracentral/registry/service/VocabularyService.java
@@ -6,6 +6,8 @@ import eu.openminted.registry.core.service.ResourceCRUDService;
 /**
  * Created by pgl on 24/7/2017.
  */
+@org.springframework.stereotype.Service("vocabularyService")
+
 public interface VocabularyService extends ResourceCRUDService<Vocabulary> {
 
 }
