a35c9a5ec8e672d932c5a0b022a0a7817dae722a
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-25 16:11:57 +0300

    ServiceController now extends GenericController

--- src/main/java/eu/einfracentral/registry/service/ServiceController.java
+++ src/main/java/eu/einfracentral/registry/service/ServiceController.java
@@ -19,57 +19,13 @@ import java.util.Base64;
  */
 @RestController
 @RequestMapping("service")
-public class ServiceController {
+public class ServiceController extends GenericRestController<Service> {
+
+    final private ServiceService serviceService;
+
     @Autowired
-    ServiceService serviceService;
-
-    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
-    public ResponseEntity<Service> getService(@PathVariable("id") String id) {
-        String id_decoded = new String(Base64.getDecoder().decode(id));
-        Service service = serviceService.get(id_decoded);
-        if (service == null) {
-            throw new ResourceNotFoundException();
-        } else {
-            return new ResponseEntity<>(service, HttpStatus.OK);
-        }
-    }
-
-    @RequestMapping(value="all",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Browsing> getAllServices() {
-        FacetFilter filter = new FacetFilter();
-        return new ResponseEntity<>(serviceService.getAll(filter),HttpStatus.OK);
-    }
-
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<String> addService(@RequestBody Service service) {
-        serviceService.add(service);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_XML_VALUE)
-    public ResponseEntity<String> addServiceXml(@RequestBody Service service) {
-        serviceService.add(service);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @RequestMapping(method = RequestMethod.PUT, headers = "Accept=application/json")
-    public ResponseEntity<String> updateService(@RequestBody Service service) {
-        serviceService.update(service);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @RequestMapping(method = RequestMethod.DELETE, headers = "Accept=application/json")
-    public ResponseEntity<String> deleteService(@RequestBody Service service) {
-        serviceService.delete(service);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "upload", method = RequestMethod.POST)
-    public ResponseEntity<String> uploadService(@RequestParam("filename") String filename, @RequestParam("file") MultipartFile file) {
-        try {
-            return new ResponseEntity<>(serviceService.uploadService(filename, file.getInputStream()), HttpStatus.OK);
-        } catch (IOException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    ServiceController(ServiceService serviceService) {
+        super(serviceService);
+        this.serviceService = serviceService;
     }
 }
