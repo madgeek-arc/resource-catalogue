de3e318b2bd0cfb25478fc4ec9820e942010dfa6
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-25 16:10:26 +0300

    Fixed container id

--- src/main/resources/eu/einfracentral/domain/resourceTypes/loadResourceTypes.sh
+++ src/main/resources/eu/einfracentral/domain/resourceTypes/loadResourceTypes.sh
@@ -5,7 +5,7 @@ UPLOADED=""
 FAILED=""
 function post_resourceType {
 	data=`cat $1`
-	response=$(curl -X POST --write-out %{http_code} --silent --output /dev/null --data "$data" --header "Content-Type:application/json" http://$2:8080/eic/resourceType/)
+	response=$(curl -X POST --write-out %{http_code} --silent --output /dev/null --data "$data" --header "Content-Type:application/json" http://$2:8080/eic-registry/resourceType/)
 	if ((${response} >= 200 && ${response} < 300 )); then
 		colors="\e[32m"
 		UPLOADED="$1\n$UPLOADED"
