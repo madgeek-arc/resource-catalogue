10485e63e31bf27f20e5fc30a1eeed0bed98760e
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-26 19:07:21 +0300

    Encapsulated Provider's fields

--- src/main/java/eu/einfracentral/domain/Provider.java
+++ src/main/java/eu/einfracentral/domain/Provider.java
@@ -29,4 +29,44 @@ public class Provider {
     @XmlElementWrapper(required = true)
     @XmlElement(name = "service")
     private List<User> services;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInformation() {
+        return contactInformation;
+    }
+
+    public void setContactInformation(String contactInformation) {
+        this.contactInformation = contactInformation;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<User> getServices() {
+        return services;
+    }
+
+    public void setServices(List<User> services) {
+        this.services = services;
+    }
 }
