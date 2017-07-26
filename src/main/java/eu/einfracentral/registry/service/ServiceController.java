e3623dc120bdb1d3b7045d216c098f75ed6f2f90
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-26 19:11:09 +0300

    Housekeeping

--- src/main/java/eu/einfracentral/registry/service/ServiceController.java
+++ src/main/java/eu/einfracentral/registry/service/ServiceController.java
@@ -1,18 +1,9 @@
 package eu.einfracentral.registry.service;
 
 import eu.einfracentral.domain.Service;
-import eu.openminted.registry.core.domain.Browsing;
-import eu.openminted.registry.core.domain.FacetFilter;
-import eu.openminted.registry.core.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.Base64;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by pgl on 4/7/2017.
@@ -20,7 +11,6 @@ import java.util.Base64;
 @RestController
 @RequestMapping("service")
 public class ServiceController extends GenericRestController<Service> {
-
     final private ServiceService serviceService;
 
     @Autowired
