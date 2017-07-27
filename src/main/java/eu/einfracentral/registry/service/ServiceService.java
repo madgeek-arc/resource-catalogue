f50048cdb99194f26659aafcd43f460ae13747a8
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-27 11:27:49 +0300

    Fixed Cthulhued code

--- src/main/java/eu/einfracentral/registry/service/ServiceService.java
+++ src/main/java/eu/einfracentral/registry/service/ServiceService.java
@@ -3,9 +3,13 @@ package eu.einfracentral.registry.service;
 import eu.openminted.registry.core.service.ResourceCRUDService;
 import org.springframework.stereotype.Service;
 
+import java.io.InputStream;
+
 /**
  * Created by pgl on 27/6/2017.
  */
+@Service("serviceService")
+public interface ServiceService extends ResourceCRUDService<eu.einfracentral.domain.Service> {
     /**
      * Uploads a zipped service
      *
@@ -14,6 +18,4 @@ import org.springframework.stereotype.Service;
      * @return archive id where it was saved
      */
     public String uploadService(String filename, InputStream inputStream);
-@Service("serviceService")
-public interface ServiceService extends ResourceCRUDService<eu.einfracentral.domain.Service> {
 }
