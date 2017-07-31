f84ff7ad9ebbdf4cef2c0d9a3ddf27ad67757420
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-31 18:15:40 +0300

    +getSome controller

--- src/main/java/eu/einfracentral/registry/service/ServiceController.java
+++ src/main/java/eu/einfracentral/registry/service/ServiceController.java
@@ -1,8 +1,14 @@
 package eu.einfracentral.registry.service;
 
 import eu.einfracentral.domain.Service;
+import eu.openminted.registry.core.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,5 +23,15 @@ public class ServiceController extends GenericRestController<Service> {
     ServiceController(ServiceService service) {
         super(service);
         this.serviceService = service;
+    }
+
+    @RequestMapping(value = "some/{ids}/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Service[]> getSome(@PathVariable String[] ids) {
+        Service[] ret = serviceService.getSome(ids);
+        if (ret == null) {
+            throw new ResourceNotFoundException();
+        } else {
+            return new ResponseEntity<>(ret, HttpStatus.OK);
+        }
     }
 }
