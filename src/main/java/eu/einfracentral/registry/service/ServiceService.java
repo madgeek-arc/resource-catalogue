1468198034b5bad93b095281def0c2ce511e3c2b
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-31 18:16:54 +0300

    +getSome

--- src/main/java/eu/einfracentral/registry/service/ServiceService.java
+++ src/main/java/eu/einfracentral/registry/service/ServiceService.java
@@ -1,14 +1,14 @@
 package eu.einfracentral.registry.service;
 
+import eu.einfracentral.domain.Service;
 import eu.openminted.registry.core.service.ResourceCRUDService;
-import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
 
 /**
  * Created by pgl on 27/6/2017.
  */
-@Service("serviceService")
+@org.springframework.stereotype.Service("serviceService")
 public interface ServiceService extends ResourceCRUDService<eu.einfracentral.domain.Service> {
     /**
      * Uploads a zipped service
@@ -18,4 +18,13 @@ public interface ServiceService extends ResourceCRUDService<eu.einfracentral.dom
      * @return archive id where it was saved
      */
     public String uploadService(String filename, InputStream inputStream);
+
+    /**
+     * Returns a list of Services
+     *
+     * @param ids The list of ids
+     * @return the Services with those ids
+     */
+    public Service[] getSome(String... ids);
+
 }
