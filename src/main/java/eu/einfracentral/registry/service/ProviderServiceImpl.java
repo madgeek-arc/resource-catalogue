4e8cd93945e59922717a90c468143409db2acf41
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-08-04 18:01:24 +0300

    Housekeeping

--- src/main/java/eu/einfracentral/registry/service/ProviderServiceImpl.java
+++ src/main/java/eu/einfracentral/registry/service/ProviderServiceImpl.java
@@ -1,23 +1,12 @@
 package eu.einfracentral.registry.service;
 
 import eu.einfracentral.domain.Provider;
-import eu.openminted.registry.core.domain.Browsing;
-import eu.openminted.registry.core.domain.FacetFilter;
-import eu.openminted.registry.core.domain.Resource;
-import eu.openminted.registry.core.service.ParserService;
-import eu.openminted.registry.core.service.SearchService;
-import eu.openminted.registry.core.service.ServiceException;
-import org.apache.log4j.Logger;
-
-import java.net.UnknownHostException;
-import java.util.Date;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Created by pgl on 26/7/2017.
  */
 @org.springframework.stereotype.Service("providerService")
-public class ProviderServiceImpl<T> extends BaseGenericResourceCRUDService<Provider> implements ProviderService {
+public class ProviderServiceImpl<T> extends BaseGenericResourceCRUDServiceImpl<Provider> implements ProviderService {
 
     public ProviderServiceImpl() {
         super(Provider.class);
--- src/main/java/eu/einfracentral/registry/service/VocabularyServiceImpl.java
+++ src/main/java/eu/einfracentral/registry/service/VocabularyServiceImpl.java
@@ -1,13 +1,12 @@
 package eu.einfracentral.registry.service;
 
 import eu.einfracentral.domain.Vocabulary;
-import org.apache.log4j.Logger;
 
 /**
  * Created by pgl on 24/7/2017.
  */
 @org.springframework.stereotype.Service("vocabularyService")
-public class VocabularyServiceImpl<T> extends BaseGenericResourceCRUDService<Vocabulary> implements VocabularyService {
+public class VocabularyServiceImpl<T> extends BaseGenericResourceCRUDServiceImpl<Vocabulary> implements VocabularyService {
 
     public VocabularyServiceImpl() {
         super(Vocabulary.class);
