4bc928ead61cf044e484b3dd84a4c0c9645c63d0
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-08-07 17:01:24 +0300

    +UserService

---
+++ src/main/java/eu/einfracentral/registry/service/UserService.java
@@ -0,0 +1,11 @@
+package eu.einfracentral.registry.service;
+
+import eu.einfracentral.domain.aai.User;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by pgl on 07/08/17.
+ */
+@Service("userService")
+public interface UserService extends ResourceCRUDService<User> {
+}
