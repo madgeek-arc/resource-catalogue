d5989e4576bf3eda211d137fec209fd2453920d4
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-08-07 17:01:11 +0300

    +UserController

---
+++ src/main/java/eu/einfracentral/registry/service/UserController.java
@@ -0,0 +1,21 @@
+package eu.einfracentral.registry.service;
+
+import eu.einfracentral.domain.aai.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by pgl on 07/08/17.
+ */
+@RestController
+@RequestMapping("user")
+public class UserController extends GenericRestController<User> {
+    final private UserService userService;
+
+    @Autowired
+    UserController(UserService service) {
+        super(service);
+        this.userService = service;
+    }
+}
