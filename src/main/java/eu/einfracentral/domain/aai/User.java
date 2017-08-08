e82c93fb49b2aef0493a0327211ee81e1b26d3b7
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-08-08 20:10:57 +0300

    New User model to satisfy register form

--- src/main/java/eu/einfracentral/domain/aai/User.java
+++ src/main/java/eu/einfracentral/domain/aai/User.java
@@ -1,7 +1,6 @@
 package eu.einfracentral.domain.aai;
 
 import eu.einfracentral.domain.Identifiable;
-import eu.einfracentral.domain.Provider;
 import eu.einfracentral.domain.Service;
 
 import javax.xml.bind.annotation.*;
@@ -11,69 +10,126 @@ import java.util.List;
  * Created by pgl on 30/6/2017.
  */
 
-@XmlType(namespace = "http://einfracentral.eu", propOrder = {"id", "organization", "favourites", "isServiceProvider"})
+@XmlType(namespace = "http://einfracentral.eu", propOrder = {"id", "name", "surname", "username", "email", "password",
+        "join_date", "affiliation", "isServiceProvider", "roles", "favourites"})
 @XmlAccessorType(XmlAccessType.FIELD)
 
 public class User implements Identifiable {
-
     @XmlElement(required = true)
     private String id;
 
     @XmlElement(required = true)
-    private Provider organization;
+    private String name;
 
-//    @XmlElement(required = true)
-//    private Role role;
-//
-//    @XmlElementWrapper(required = true)
-//    @XmlElement(name = "grant")
-//    private List<Grant> grants;
+    @XmlElement(required = true)
+    private String surname;
+
+    @XmlElement(required = true)
+    private String username;
+
+    @XmlElement(required = true)
+    private String email;
+
+    @XmlElement(required = true)
+    private String password;
+
+    @XmlElement(required = true)
+    private String join_date;
+
+    @XmlElement(required = true)
+    private String affiliation;
+
+    @XmlElement(required = true)
+    private boolean isServiceProvider;
+
+    @XmlElementWrapper
+    @XmlElement(name = "role")
+    private List<Role> roles;
 
     @XmlElementWrapper(required = true)
     @XmlElement(name = "favourite")
     private List<Service> favourites;
 
-    @XmlElement(required = true)
-    private boolean isServiceProvider;
-
+    @Override
     public String getId() {
         return id;
     }
 
+    @Override
     public void setId(String id) {
         this.id = id;
     }
 
-    public Provider getOrganization() {
-        return organization;
+    public String getName() {
+        return name;
     }
 
-    public void setOrganization(Provider organization) {
-        this.organization = organization;
+    public void setName(String name) {
+        this.name = name;
     }
 
-//    public Role getRole() {
-//        return role;
-//    }
-//
-//    public void setRole(Role role) {
-//        this.role = role;
-//    }
-//
-//    public List<Grant> getGrants() {
-//        return grants;
-//    }
-//
-//    public void setGrants(List<Grant> grants) {
-//        this.grants = grants;
-//    }
+    public String getSurname() {
+        return surname;
+    }
 
-    public boolean getIsServiceProvider() {
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getJoin_date() {
+        return join_date;
+    }
+
+    public void setJoin_date(String join_date) {
+        this.join_date = join_date;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public boolean isServiceProvider() {
         return isServiceProvider;
     }
 
-    public void setIsServiceProvider(boolean isServiceProvider) {
-        this.isServiceProvider = isServiceProvider;
+    public void setServiceProvider(boolean serviceProvider) {
+        isServiceProvider = serviceProvider;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
     }
 
     public List<Service> getFavourites() {
@@ -84,3 +140,6 @@ public class User implements Identifiable {
         this.favourites = favourites;
     }
 }
+
+
+
