ea053130ec6698284b0a07f6a8a205af7a914186
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-08-08 20:14:09 +0300

    +register

--- src/main/java/eu/einfracentral/registry/service/UserController.java
+++ src/main/java/eu/einfracentral/registry/service/UserController.java
@@ -2,8 +2,12 @@ package eu.einfracentral.registry.service;
 
 import eu.einfracentral.domain.aai.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by pgl on 07/08/17.
@@ -18,9 +22,25 @@ public class UserController extends GenericRestController<User> {
         super(service);
         this.userService = service;
     }
+
     @CrossOrigin
     @RequestMapping(path = "activate/{token}", method = RequestMethod.GET)
     public void activate(@PathVariable String token) {
         this.userService.activate(token);
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "register", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> register(@RequestBody User user) {
+        user.setId("token");
+        this.userService.add(user);
+
+//        SimpleMailMessage email = new SimpleMailMessage();
+//        email.setTo(user.getEmail());
+//        email.setSubject("[eInfraCentral] Activate your account");
+//        email.setText("Please visit http://einfracentral.eu/eic-registry/user/activate/" + user.getId());
+//        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+//        sender.send(email);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
