1d0c5a221ccfa6c08e712407a5c5340ee75e6835
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-08-08 20:10:20 +0300

    +rudimentary login/activate

--- src/main/java/eu/einfracentral/registry/service/UserServiceImpl.java
+++ src/main/java/eu/einfracentral/registry/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package eu.einfracentral.registry.service;
 
 import eu.einfracentral.domain.aai.User;
+import java.util.Date;
 
 /**
  * Created by pgl on 07/08/17.
@@ -15,5 +16,18 @@ public class UserServiceImpl<T> extends BaseGenericResourceCRUDServiceImpl<User>
     @Override
     public String getResourceType() {
         return "eInfraUser";
+    }
+
+    @Override
+    public User login(User creds) {
+        throw new Error("User login not yet implemented!");
+    }
+
+    @Override
+    public void activate(String token) {
+        User ret = get(token);
+        if (ret.getJoin_date() != null) {
+            ret.setJoin_date(new Date().toString());
+        }
     }
 }
