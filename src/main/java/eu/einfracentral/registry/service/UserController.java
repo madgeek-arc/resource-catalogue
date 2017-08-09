cddeba4e24cd0c648a37c5a022e2a86a56e44f40
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-08-09 17:08:50 +0300

    New mail config

--- src/main/java/eu/einfracentral/registry/service/UserController.java
+++ src/main/java/eu/einfracentral/registry/service/UserController.java
@@ -1,6 +1,9 @@
 package eu.einfracentral.registry.service;
 
 import eu.einfracentral.domain.aai.User;
+import eu.openminted.registry.core.service.ParserService;
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -8,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Properties;
 
 /**
  * Created by pgl on 07/08/17.
@@ -35,12 +40,23 @@ public class UserController extends GenericRestController<User> {
         user.setId("token");
         this.service.add(user, ParserService.ParserServiceTypes.JSON);
 
-//        SimpleMailMessage email = new SimpleMailMessage();
-//        email.setTo(user.getEmail());
-//        email.setSubject("[eInfraCentral] Activate your account");
-//        email.setText("Please visit http://einfracentral.eu/eic-registry/user/activate/" + user.getId());
-//        JavaMailSenderImpl sender = new JavaMailSenderImpl();
-//        sender.send(email);
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setTo(user.getEmail());
+        email.setSubject("[eInfraCentral] Activate your account");
+        email.setText("Please visit http://einfracentral.eu/eic-registry/user/activate/" + user.getId());
+        email.setFrom("test.espas@gmail.com");
+        email.setReplyTo("test.espas@gmail.com");
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost("smtp.gmail.com");
+        sender.setPort(465);
+        sender.setUsername("test.espas@gmail.com");
+        sender.setPassword("s.a.g.a.p.w");
+        Properties jmp = new Properties();
+        jmp.setProperty("mail.transport.protocol", "ssl");
+        jmp.setProperty("mail.smtp.auth", "true");
+        jmp.setProperty("mail.smtp.starttls.enable", "true");
+        sender.setJavaMailProperties(jmp);
+        sender.send(email);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
