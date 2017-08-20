33eb2e14dda99d73aa311c76db8c6e3f09ca6a21
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-08-20 15:11:43 +0300

    Moved register logic and mail logic to UserServiceImpl

--- src/main/java/eu/einfracentral/registry/service/UserServiceImpl.java
+++ src/main/java/eu/einfracentral/registry/service/UserServiceImpl.java
@@ -1,7 +1,13 @@
 package eu.einfracentral.registry.service;
 
 import eu.einfracentral.domain.aai.User;
+import eu.openminted.registry.core.domain.Resource;
+import eu.openminted.registry.core.service.ParserService;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 import java.util.Date;
+import java.util.Properties;
 
 /**
  * Created by pgl on 07/08/17.
@@ -30,4 +36,48 @@ public class UserServiceImpl<T> extends BaseGenericResourceCRUDServiceImpl<User>
             ret.setJoin_date(new Date().toString());
         }
     }
+
+//    public void sendMail(User user) {
+//        SimpleMailMessage email = new SimpleMailMessage();
+//        email.setTo(user.getEmail());
+//        email.setSubject("[eInfraCentral] Activate your account");
+//        email.setText("Please visit http://einfracentral.eu/eic-registry/user/activate/" + user.getId());
+//        email.setFrom("test.espas@gmail.com");
+//        email.setReplyTo("test.espas@gmail.com");
+//        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+//        sender.setHost("smtp.gmail.com");
+//        sender.setPort(465);
+//        sender.setUsername("test.espas@gmail.com");
+//        sender.setPassword("s.a.g.a.p.w");
+//        Properties jmp = new Properties();
+//        jmp.setProperty("mail.transport.protocol", "ssl");
+//        jmp.setProperty("mail.smtp.auth", "true");
+//        jmp.setProperty("mail.smtp.starttls.enable", "true");
+//        sender.setJavaMailProperties(jmp);
+//        sender.send(email);
+//    }
+//
+//    @Override
+//    public void register(User user) {
+//        user.setId("token");
+//        add(user, ParserService.ParserServiceTypes.JSON);
+//
+//
+//    }
+//
+//    @Override
+//    public void register(String userAsXML) {
+//        Resource ret = new Resource();
+//        ret.setPayload(userAsXML);
+//        ret.setCreationDate(new Date());
+//        ret.setModificationDate(new Date());
+//        ret.setPayloadFormat("xml");
+//        ret.setResourceType(getResourceType());
+//        ret.setVersion("not_set");
+//        resourceService.addResource(ret);
+//
+//        get
+//
+//        sendMail(user);
+//    }
 }
