6bdbca0b2323f6b77ab159c2cb757d37cbbca7e5
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-08-23 18:23:16 +0300

    Identifiable test

---
+++ src/main/test/eu/einfracentral/domain/InterfaceTest.java
@@ -0,0 +1,43 @@
+package eu.einfracentral.domain;
+
+import eu.einfracentral.domain.performance.Indicator;
+import org.junit.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringWriter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static javax.xml.bind.JAXBContext.newInstance;
+
+/**
+ * Created by pgl on 23/08/17.
+ */
+public class InterfaceTest {
+
+    private ExecutorService executor;
+    private JAXBContext jaxbContext = null;
+
+    public InterfaceTest() {
+        executor = Executors.newCachedThreadPool();
+        try {
+            jaxbContext = newInstance(MyPOJO.class);
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void test2() throws Exception {
+        MyPOJO resource = new MyPOJO();
+        resource.setField("field");
+        resource.setId("id");
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        StringWriter sw = new StringWriter();
+        marshaller.marshal(resource, sw);
+        System.err.println(sw.toString());
+    }
+}
---
+++ src/main/test/eu/einfracentral/domain/MyPOJO.java
@@ -0,0 +1,39 @@
+package eu.einfracentral.domain;
+
+import javax.xml.bind.annotation.*;
+
+/**
+ * Created by pgl on 23/08/17.
+ */
+@XmlType(namespace = "http://einfracentral.eu", propOrder = {"id", "field"})
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(namespace = "http://einfracentral.eu")
+public class MyPOJO implements Identifiable {
+    public MyPOJO() {
+
+    }
+
+    @XmlElement(required = false)
+    private String id;
+
+    @XmlElement(required = false)
+    private String field;
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+}
