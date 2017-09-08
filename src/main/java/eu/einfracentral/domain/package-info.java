6c64bf1fdd5c283b961311571dc68224bace7c26
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-09-08 15:37:47 +0300

    +xmlns

--- src/main/java/eu/einfracentral/domain/package-info.java
+++ src/main/java/eu/einfracentral/domain/package-info.java
@@ -1,11 +1,12 @@
 @XmlSchema(
         namespace = "http://einfracentral.eu",
-        elementFormDefault = XmlNsForm.QUALIFIED//,
-//    xmlns = {
-//        @XmlNs(prefix="tns", namespaceURI="http://einfracentral.eu")
-//    }
+        elementFormDefault = XmlNsForm.QUALIFIED,
+        xmlns = {
+                @XmlNs(prefix = "tns", namespaceURI = "http://einfracentral.eu")
+        }
 )
 package eu.einfracentral.domain;
 
+import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlNsForm;
 import javax.xml.bind.annotation.XmlSchema;
