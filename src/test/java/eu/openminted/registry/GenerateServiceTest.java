e237e57b52bf6f9a120a41a7ec043e71c4634b9d
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-09-27 14:08:52 +0300

    Brought tests in line with SDT v0.95

--- src/test/java/eu/openminted/registry/GenerateServiceTest.java
+++ src/test/java/eu/openminted/registry/GenerateServiceTest.java
@@ -36,35 +36,21 @@ public class GenerateServiceTest {
     public void checkService() throws Exception {
         String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
                 "<service xmlns=\"http://einfracentral.eu\">\n" +
-                "    <id>01.13</id>\n" +
-                "    <brandName>Accounting</brandName>\n" +
-                "    <tagline>Track and report the usage of your services</tagline>\n" +
-                "    <fullName>EGI Accounting</fullName>\n" +
-                "    <description>EGI Accounting stores user accounting records from various services offered by EGI, such as Cloud, HTC\n" +
-                "        and storage usage. It works thanks to a network of message brokers that transfer usage data from the host to a\n" +
-                "        central repository of information. The data is handled securely and can be consulted online through the EGI\n" +
-                "        Accounting Portal. EGI council members can use EGI accounting services to account for the resource usage of\n" +
-                "        their own services. EGI Accounting gives: - Increased control over resource consumption - Reduced overhead of\n" +
-                "        defining data models, architecture and setup of an accounting system - Reduced cost of maintaining an accounting\n" +
-                "        infrastructure - Access to a reliable, high available, high performance service - User friendly web interface\n" +
-                "    </description>\n" +
-                "    <targetUsers>Customer: User communities and EGI service providers. User: VO Managers, Resource providers\n" +
-                "        management\n" +
-                "    </targetUsers>\n" +
-                "    <userValue>- Reduced overhead of defining data models, architecture and setup of an accounting system - Reduced cost\n" +
-                "        of maintaining an accounting infrastructure\n" +
-                "    </userValue>\n" +
-                "    <userBase>EGI resource providers: https://www.egi.eu/federation/data-centres/</userBase>\n" +
-                "    <provider>EGI Foundation (via EGI resource providers)</provider>\n" +
-                "    <fundingSources>Development: EC H2020 projects (primarily) and national projects/open source projects. Operations:\n" +
-                "        EGI participant fees + in-kind contribution from service providers\n" +
-                "    </fundingSources>\n" +
-                "    <webpage>https://www.egi.eu/internal-services/accounting/</webpage>\n" +
-                "    <phase>Production</phase>\n" +
-                "    <technologyReadinessLevel>9</technologyReadinessLevel>\n" +
-                "    <category>Operations</category>\n" +
-                "    <subcategory>Αccounting</subcategory>\n" +
-                "    <request>https://www.egi.eu/request-service/</request>\n" +
+                "    <id>00.00</id>\n" +
+                "    <url>http://example.org/</url>\n" +
+                "    <name>Name</name>\n" +
+                "    <description>A service that does things</description>\n" +
+                "    <provider>Jo Mama</provider>\n" +
+                "    <lifeCyYcleStatus>Production</lifeCyYcleStatus>\n" +
+                "    <trl>9</trl>\n" +
+                "    <category>Cat</category>\n" +
+                "    <subcategory>Subcat</subcategory>\n" +
+                "    <place>Nowhere</place>\n" +
+                "    <language>None</language>\n" +
+                "    <tag>None</tag>\n" +
+                "    <request>https://example.org/</request>\n" +
+                "    <price>https://example.org/</price>\n" +
+                "    <serviceLevelAgreement>https://example.org/</serviceLevelAgreement>\n" +
                 "</service>";
         JAXBContext jaxbContext = JAXBContext.newInstance(Service.class);
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
