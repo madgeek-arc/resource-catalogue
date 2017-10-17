b81905d196ebb02627c2941d995ef23a45309aa0
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-10-17 17:44:23 +0300

    removed unwanted method

--- src/main/java/eu/einfracentral/registry/service/ServiceService.java
+++ src/main/java/eu/einfracentral/registry/service/ServiceService.java
@@ -1,19 +1,9 @@
 package eu.einfracentral.registry.service;
 
-import java.io.InputStream;
-
 /**
  * Created by pgl on 27/6/2017.
  */
 @org.springframework.stereotype.Service("serviceService")
 public interface ServiceService extends ResourceCRUDService<eu.einfracentral.domain.Service> {
-    /**
-     * Uploads a zipped service
-     *
-     * @param filename
-     * @param inputStream
-     * @return archive id where it was saved
-     */
-    String uploadService(String filename, InputStream inputStream);
 
 }
