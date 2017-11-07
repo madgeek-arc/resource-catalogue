5a201c7e825200c6a18e266cb5f333a9b10391b1
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-11-07 17:28:03 +0200

    formatting

--- src/main/java/eu/einfracentral/registry/service/UserController.java
+++ src/main/java/eu/einfracentral/registry/service/UserController.java
@@ -1,12 +1,15 @@
 package eu.einfracentral.registry.service;
+
 import eu.einfracentral.domain.aai.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by pgl on 07/08/17.
  */
@@ -14,31 +17,37 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("user")
 public class UserController extends GenericRestController<User> {
     final private UserService userService;
+
     @Autowired
     UserController(UserService service) {
         super(service);
         this.userService = service;
     }
+
     @CrossOrigin
     @RequestMapping(path = "activate/{token}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<User> activate(@PathVariable String token) {
         return new ResponseEntity<>(this.userService.activate(token), HttpStatus.OK);
     }
+
     @CrossOrigin
     @RequestMapping(path = "reset", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<User> reset(@RequestBody User user) {
         return new ResponseEntity<>(this.userService.reset(user), HttpStatus.OK);
     }
+
     @CrossOrigin
     @RequestMapping(path = "forgot", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<User> forgot(@PathVariable String email) {
         return new ResponseEntity<>(this.userService.forgot(email), HttpStatus.OK);
     }
+
     @CrossOrigin
     @RequestMapping(path = "register", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<User> register(@RequestBody User user) {
         return new ResponseEntity<>(this.userService.register(user), HttpStatus.OK);
     }
+
     @CrossOrigin
     @RequestMapping(path = "login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<User> login(@RequestBody User credentials, HttpServletResponse res) {
--- src/main/java/eu/einfracentral/registry/service/VocabularyController.java
+++ src/main/java/eu/einfracentral/registry/service/VocabularyController.java
@@ -1,8 +1,10 @@
 package eu.einfracentral.registry.service;
+
 import eu.einfracentral.domain.Vocabulary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by pgl on 24/7/2017.
  */
