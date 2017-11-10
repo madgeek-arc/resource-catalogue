36f4bc81ac5c3e163bb5beedc35edc269d8e0c65
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-11-10 23:24:34 +0200

    v0.95

--- src/main/java/eu/einfracentral/registry/service/UserController.java
+++ src/main/java/eu/einfracentral/registry/service/UserController.java
@@ -1,6 +1,6 @@
 package eu.einfracentral.registry.service;
 
-import eu.einfracentral.domain.aai.User;
+import eu.einfracentral.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
