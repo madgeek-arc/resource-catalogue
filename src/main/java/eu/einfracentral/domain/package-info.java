0a8427b458e2930fdfaf224786d7a210b99a909a
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-11-13 09:44:18 +0200

    Removed prefix

--- src/main/java/eu/einfracentral/domain/package-info.java
+++ src/main/java/eu/einfracentral/domain/package-info.java
@@ -1,7 +1,7 @@
 @XmlSchema(
         namespace = "http://einfracentral.eu",
-        elementFormDefault = XmlNsForm.QUALIFIED,
-        xmlns = {@XmlNs(prefix = "tns", namespaceURI = "http://einfracentral.eu")}
+        elementFormDefault = XmlNsForm.QUALIFIED
+        //,xmlns = {@XmlNs(prefix = "tns", namespaceURI = "http://einfracentral.eu")}
 )
 @XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
 @XmlAccessorType(XmlAccessType.FIELD)
