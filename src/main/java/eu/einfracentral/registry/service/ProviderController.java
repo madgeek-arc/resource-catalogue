77b5fa36b670ca3c8c8e6e4598e4acea2b0959d4
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-01-05 15:42:52 +0200

    -hardcoded providers

--- src/main/java/eu/einfracentral/registry/service/ProviderController.java
+++ src/main/java/eu/einfracentral/registry/service/ProviderController.java
@@ -2,7 +2,6 @@ package eu.einfracentral.registry.service;
 
 import eu.einfracentral.domain.Provider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -14,10 +13,5 @@ public class ProviderController extends ResourceController<Provider> {
     @Autowired
     ProviderController(ProviderService service) {
         super(service);
-    }
-
-    @RequestMapping(path = "hard", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<String[]> getHard() {
-        return new ResponseEntity<>(new String[]{"EGI Foundation", "EUDAT CDI consortium", "GÉANT", "OpenAIRE consortium", "PRACE"}, HttpStatus.OK);
     }
 }
