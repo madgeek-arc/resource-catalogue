853e5f2429ed8a6e5562a67251e7cba2288d72da
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-01-08 16:54:28 +0200

    +dump/restore controllers

---
+++ src/main/java/eu/einfracentral/controllers/DumpController.java
@@ -0,0 +1,37 @@
+package eu.einfracentral.controllers;
+
+import eu.openminted.registry.core.service.DumpService;
+import java.io.*;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by pgl on 08/01/18.
+ */
+@RestController
+public class DumpController {
+    @Autowired
+    DumpService dumpService;
+
+    @RequestMapping(path = "dump", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public void dumpAll(@RequestParam(value = "types", required = false) String[] types, HttpServletResponse response) {
+        File dump = dumpService.bringAll(false, false, types);
+        response.setContentLength((int) dump.length());
+        response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", dump.getName()));
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(dump));
+             OutputStream out = response.getOutputStream();
+             BufferedOutputStream bufout = new BufferedOutputStream(out)) {
+            byte[] buf = new byte[8192];
+            for (int bytes = in.read(buf); bytes != -1; bytes = in.read(buf)) {
+                bufout.write(buf, 0, bytes);
+            }
+            bufout.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //delete dump directory on end?
+        }
+    }
+}
---
+++ src/main/java/eu/einfracentral/controllers/RestoreController.java
@@ -0,0 +1,20 @@
+package eu.einfracentral.controllers;
+
+import eu.openminted.registry.core.service.RestoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by pgl on 08/01/18.
+ */
+@RestController
+public class RestoreController {
+    @Autowired
+    RestoreService restoreService;
+
+    @RequestMapping(path = "restore", method = RequestMethod.POST)
+    public void restoreAll(@RequestParam("file") MultipartFile file) {
+        restoreService.restoreDataFromZip(file);
+    }
+}
