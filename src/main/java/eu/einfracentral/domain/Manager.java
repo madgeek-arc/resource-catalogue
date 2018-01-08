cc9417ce6ccaa7a403e77a5b529bd707ab074575
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-01-08 12:25:38 +0200

    +Manager

---
+++ src/main/java/eu/einfracentral/domain/Manager.java
@@ -0,0 +1,45 @@
+package eu.einfracentral.domain;
+
+import java.util.List;
+import javax.xml.bind.annotation.*;
+
+/**
+ * Created by pgl on 08/01/18.
+ */
+@XmlType
+@XmlRootElement(namespace = "http://einfracentral.eu")
+public class Manager implements Identifiable {
+    @XmlElement(required = false)
+    private String id;
+    @XmlElementWrapper(name = "users", required = false)
+    @XmlElement(name = "user")
+    private List<User> users;
+    @XmlElementWrapper(name = "services", required = false)
+    @XmlElement(name = "service")
+    private List<User> services;
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<User> getServices() {
+        return services;
+    }
+
+    public void setServices(List<User> services) {
+        this.services = services;
+    }
+}
