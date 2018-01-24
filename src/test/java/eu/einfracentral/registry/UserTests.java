ce01c5927b30ca332d38387cb2f3bd44eccaa7a4
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-01-24 15:29:06 +0200

    +user tests

---
+++ src/test/java/eu/einfracentral/registry/UserTests.java
@@ -0,0 +1,58 @@
+package eu.einfracentral.registry;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.util.*;
+import javax.crypto.*;
+import javax.crypto.spec.PBEKeySpec;
+import org.junit.Test;
+
+/**
+ * Created by pgl on 14/11/17.
+ */
+public class UserTests {
+    private static final HashMap<String, String> users = new HashMap<>();
+    private static final int iterationCount = -1;
+    private static final String algorithm = "PBKDF2WithHmacSHA512";
+    private static final int keyLength = 256;
+    private static final Random r = new SecureRandom();
+    private static final byte[] salt = new byte[8];
+    private static final String base = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+            "<user xmlns=\"http://einfracentral.eu\">\n" +
+            "    <email>%s@eic</email>\n" +
+            "    <id>%s</id>\n" +
+            "    <iterationCount>%s</iterationCount>\n" +
+            "    <joinDate>%s</joinDate>\n" +
+            "    <password>%s</password>\n" +
+            "    <salt>%s</salt>\n" +
+            "</user>";
+
+    @Test
+    public void makeUsers() throws IOException {
+        //users.put("username", "password");
+        for (Map.Entry<String, String> user : users.entrySet()) {
+            r.nextBytes(salt);
+            Files.write(Paths.get(String.format("../eic-data/transformed/user.res/%s.xml", user.getKey())),
+                        String.format(base,
+                                      user.getKey(),
+                                      user.getKey(),
+                                      iterationCount,
+                                      new Date().toString(),
+                                      new String(hashPass(user.getValue().toCharArray(), salt, iterationCount)),
+                                      new String(Base64.getEncoder().encode(salt))).getBytes());
+        }
+    }
+
+    private char[] hashPass(char[] pass, byte[] salt, int iterations) {
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm);
+            PBEKeySpec spec = new PBEKeySpec(pass, salt, iterations, keyLength);
+            SecretKey key = skf.generateSecret(spec);
+            return new String(Base64.getEncoder().encode(key.getEncoded())).toCharArray();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            throw new Error(ex);
+        }
+    }
+}
