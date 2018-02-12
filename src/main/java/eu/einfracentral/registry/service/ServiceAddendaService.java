72bcca36f49373704c09e7c2c89d625d586a2fbf
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-02-12 16:05:44 +0200

    +addenda service

---
+++ src/main/java/eu/einfracentral/registry/service/ServiceAddendaService.java
@@ -0,0 +1,7 @@
+package eu.einfracentral.registry.service;
+
+import eu.einfracentral.domain.ServiceAddenda;
+
+@org.springframework.stereotype.Service("serviceAddendaService")
+public interface ServiceAddendaService extends ResourceService<ServiceAddenda> {
+}
