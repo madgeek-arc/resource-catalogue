08c71537d52af0e79190dd8dc6e7ed77cedce50e
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-02-13 21:25:44 +0200

    Swagger fix

--- src/main/java/eu/einfracentral/registry/service/ServiceController.java
+++ src/main/java/eu/einfracentral/registry/service/ServiceController.java
@@ -1,12 +1,15 @@
 package eu.einfracentral.registry.service;
 
 import eu.einfracentral.domain.Service;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by pgl on 4/7/2017.
  */
+
+@Api
 @RestController
 @RequestMapping("service")
 public class ServiceController extends ResourceController<Service> {
