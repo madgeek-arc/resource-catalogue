0821e76a08d2816665d63d98af12c9d75f2ce493
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-02-17 17:22:45 +0200

    added generalized add/update

--- src/main/java/eu/einfracentral/registry/service/ResourceService.java
+++ src/main/java/eu/einfracentral/registry/service/ResourceService.java
@@ -1,13 +1,15 @@
 package eu.einfracentral.registry.service;
 
 import eu.openminted.registry.core.domain.Browsing;
-import eu.openminted.registry.core.service.ResourceCRUDService;
+import eu.openminted.registry.core.service.*;
 import java.util.*;
 
 /**
  * Created by pgl on 04/08/17.
  */
 public interface ResourceService<T> extends ResourceCRUDService<T> {
+    T add(T t, ParserService.ParserServiceTypes format);
+    T update(T t, ParserService.ParserServiceTypes format);
     Map<String, List<T>> getBy(String field);
     List<T> getSome(String... ids);
     T get(String field, String value);
