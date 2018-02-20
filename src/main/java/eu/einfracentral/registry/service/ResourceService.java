2eec6b8d488d80aef1441b18718f1a37445f0029
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-02-20 17:08:02 +0200

    now using core interface

--- src/main/java/eu/einfracentral/registry/service/ResourceService.java
+++ src/main/java/eu/einfracentral/registry/service/ResourceService.java
@@ -1,15 +1,13 @@
 package eu.einfracentral.registry.service;
 
 import eu.openminted.registry.core.domain.Browsing;
-import eu.openminted.registry.core.service.*;
+import eu.openminted.registry.core.service.ResourceCRUDService;
 import java.util.*;
 
 /**
  * Created by pgl on 04/08/17.
  */
 public interface ResourceService<T> extends ResourceCRUDService<T> {
-    T add(T t, ParserService.ParserServiceTypes format);
-    T update(T t, ParserService.ParserServiceTypes format);
     T del(T t);
     Map<String, List<T>> getBy(String field);
     List<T> getSome(String... ids);
