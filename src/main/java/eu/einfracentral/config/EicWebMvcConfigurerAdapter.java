1f03248805fb8279e979806b68b9e247b05f712d
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-02-21 17:53:07 +0200

    beaut

--- src/main/java/eu/einfracentral/config/EicWebMvcConfigurerAdapter.java
+++ src/main/java/eu/einfracentral/config/EicWebMvcConfigurerAdapter.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.*;
  */
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class EicWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
