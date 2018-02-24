63046b7f4c333746011f358d997b026dfea7653a
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-02-24 09:38:24 +0200

    removed extraneous aggregations

--- src/main/java/eu/einfracentral/domain/ServiceAddenda.java
+++ src/main/java/eu/einfracentral/domain/ServiceAddenda.java
@@ -20,16 +20,6 @@ public class ServiceAddenda implements Identifiable {
     @XmlElement
     private List<Measurement<?>> performanceData;
     @XmlElement
-    private int externalHits;
-    @XmlElement
-    private int internalHits;
-    @XmlElement
-    private int favouriteCount;
-    @XmlElement
-    private float averageRating;
-    @XmlElement
-    private float ratings;
-    @XmlElement
     private boolean featured;
     @XmlElement(defaultValue = "false")
     private boolean published;
@@ -74,46 +64,6 @@ public class ServiceAddenda implements Identifiable {
 
     public void setPerformanceData(List<Measurement<?>> performanceData) {
         this.performanceData = performanceData;
-    }
-
-    public int getExternalHits() {
-        return externalHits;
-    }
-
-    public void setExternalHits(int externalHits) {
-        this.externalHits = externalHits;
-    }
-
-    public int getInternalHits() {
-        return internalHits;
-    }
-
-    public void setInternalHits(int internalHits) {
-        this.internalHits = internalHits;
-    }
-
-    public int getFavouriteCount() {
-        return favouriteCount;
-    }
-
-    public void setFavouriteCount(int favouriteCount) {
-        this.favouriteCount = favouriteCount;
-    }
-
-    public float getAverageRating() {
-        return averageRating;
-    }
-
-    public void setAverageRating(float averageRating) {
-        this.averageRating = averageRating;
-    }
-
-    public float getRatings() {
-        return ratings;
-    }
-
-    public void setRatings(float ratings) {
-        this.ratings = ratings;
     }
 
     public boolean isFeatured() {
