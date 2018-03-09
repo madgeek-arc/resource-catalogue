3e73e77c1a5514dcecded3b8411fdaef8e844145
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-03-09 12:42:57 +0200

    hid modification functions

--- src/main/java/eu/einfracentral/registry/controller/VocabularyController.java
+++ src/main/java/eu/einfracentral/registry/controller/VocabularyController.java
@@ -2,10 +2,13 @@ package eu.einfracentral.registry.controller;
 
 import eu.einfracentral.domain.Vocabulary;
 import eu.einfracentral.registry.service.VocabularyService;
+import eu.openminted.registry.core.exception.ResourceNotFoundException;
 import io.swagger.annotations.*;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * Created by pgl on 24/7/2017.
@@ -21,5 +24,36 @@ public class VocabularyController extends ResourceController<Vocabulary> {
     @RequestMapping(value = "getEU", method = RequestMethod.GET)
     public ResponseEntity<String[]> getEU() {
         return new ResponseEntity<>(((VocabularyService) service).getEU(), HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @RequestMapping(method = RequestMethod.POST)
+    @ApiIgnore
+    public ResponseEntity<Vocabulary> add(@RequestBody Vocabulary vocabulary, @CookieValue(defaultValue = "") String jwt) {
+        return new ResponseEntity<>(service.add(vocabulary), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    @ApiIgnore
+    public ResponseEntity<Vocabulary> update(@RequestBody Vocabulary vocabulary, @CookieValue(defaultValue = "") String jwt) throws ResourceNotFoundException {
+        return new ResponseEntity<>(service.update(vocabulary), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "validate", method = RequestMethod.POST)
+    @ApiIgnore
+    public ResponseEntity<Vocabulary> validate(@RequestBody Vocabulary vocabulary, @CookieValue(defaultValue = "") String jwt) throws ResourceNotFoundException {
+        return new ResponseEntity<>(service.validate(vocabulary), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    @ApiIgnore
+    public ResponseEntity<Vocabulary> delete(@RequestBody Vocabulary vocabulary, @CookieValue(defaultValue = "") String jwt) {
+        return new ResponseEntity<>(service.del(vocabulary), HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "all", method = RequestMethod.DELETE)
+    @ApiIgnore
+    public ResponseEntity<List<Vocabulary>> delAll(@CookieValue(defaultValue = "") String jwt) {
+        return new ResponseEntity<>(service.delAll(), HttpStatus.OK);
     }
 }
