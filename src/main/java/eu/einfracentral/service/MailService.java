f05447b3eb8bb1a5cf056162a586d424e0a2c400
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-03-09 15:59:45 +0200

    propfix

--- src/main/java/eu/einfracentral/service/MailService.java
+++ src/main/java/eu/einfracentral/service/MailService.java
@@ -1,6 +1,5 @@
 package eu.einfracentral.service;
 
-import com.sun.mail.smtp.SMTPTransport;
 import java.util.Properties;
 import javax.annotation.PostConstruct;
 import javax.mail.*;
@@ -26,12 +25,10 @@ public class MailService {
     private String password;
     @Value("${mail.smtp.port}")
     private String port;
-    @Value("${mail.smtp.socketFactory.class}")
-    private String socketFactoryClass;
-    @Value("${mail.smtp.socketFactory.port}")
-    private String socketFactoryPort;
-    @Value("${mail.smtp.starttls.enable}")
-    private String starttls;
+    @Value("${mail.smtp.protocol}")
+    private String protocol;
+    @Value("${mail.smtp.ssl.enable}")
+    private String sslEnable;
     @Value("${mail.smtp.user}")
     private String user;
 
@@ -47,9 +44,8 @@ public class MailService {
         sessionProps.setProperty("mail.smtp.host", host);
         sessionProps.setProperty("mail.smtp.password", password);
         sessionProps.setProperty("mail.smtp.port", port);
-        sessionProps.setProperty("mail.smtp.socketFactory.class", socketFactoryClass);
-        sessionProps.setProperty("mail.smtp.socketFactory.port", socketFactoryPort);
-        sessionProps.setProperty("mail.smtp.starttls.enable", starttls);
+        sessionProps.setProperty("mail.smtp.protocol", protocol);
+        sessionProps.setProperty("mail.smtp.ssl.enable", sslEnable);
         sessionProps.setProperty("mail.smtp.user", user);
         session = Session.getInstance(sessionProps, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -59,16 +55,16 @@ public class MailService {
     }
 
     public void sendMail(String to, String subject, String text) {
-        try (SMTPTransport transport = (SMTPTransport) session.getTransport("smtp")) {
-            InternetAddress sender = new InternetAddress(user + "@" + host);
+        try (Transport transport = session.getTransport()) {
+            InternetAddress sender = new InternetAddress(user);
             Message message = new MimeMessage(session);
             message.setFrom(sender);
             message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
             message.setRecipient(Message.RecipientType.BCC, sender);
             message.setSubject(subject);
             message.setText(text);
-            transport.connect(host, Integer.parseInt(port), user, password);
-            transport.send(message);
+            transport.connect();
+            Transport.send(message);
         } catch (MessagingException e) {
             e.printStackTrace();
         }
