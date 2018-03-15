bbc4f783132a51d57203a770b8556db786ce43a9
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-03-15 19:02:16 +0200

    +web security configurer

---
+++ src/main/java/eu/einfracentral/config/WebSecurityConfigurer.java
@@ -0,0 +1,22 @@
+//package eu.einfracentral.config;
+//
+//import org.springframework.context.annotation.*;
+//import org.springframework.security.config.annotation.web.configuration.*;
+//
+///**
+// * Created by pgl on 15/03/18.
+// */
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+////    @Bean
+////    public TokenAuthenticationFilter jwtAuthenticationTokenFilter() throws Exception {
+////        return new TokenAuthenticationFilter();
+////    }
+//
+//    //@Override
+//    //protected void configure(HttpSecurity http) throws Exception {
+//        //http.addFilterBefore(jwtAuthenticationTokenFilter(), BasicAuthenticationFilter.class);
+//    //}
+//}
