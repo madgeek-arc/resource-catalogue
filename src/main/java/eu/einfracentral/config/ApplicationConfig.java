150a7f587fb7c6b611b0d0d5da0fb8e765475fb6
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-03-21 14:41:53 +0200

    Implemented application config

--- src/main/java/eu/einfracentral/config/ApplicationConfig.java
+++ src/main/java/eu/einfracentral/config/ApplicationConfig.java
@@ -1,8 +1,101 @@
 package eu.einfracentral.config;
 
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by pgl on 08/12/17.
  */
+@Component
 public class ApplicationConfig {
-    //expose all properties thru this single source of user config
+    private final String activateSubject, activateText, auth, host, password, platform, port, protocol, resetSubject, resetText, secret, ssl, user;
+    private int iterations;
+
+    @Autowired
+    public ApplicationConfig(@Value("${mail.activate.subject}") String activateSubject, @Value("${mail.activate.text}") String activateText,
+                             @Value("${mail.smtp.auth}") String auth, @Value("${mail.smtp.host}") String host,
+                             @Value("${sec.user.iterations:1000}") int iterations, @Value("${mail.smtp.password}") String password,
+                             @Value("${platform.root:}") String platform, @Value("${mail.smtp.port}") String port,
+                             @Value("${mail.smtp.protocol}") String protocol, @Value("${mail.reset.subject}") String resetSubject,
+                             @Value("${mail.reset.text}") String resetText, @Value("${jwt.secret:}") String secret,
+                             @Value("${mail.smtp.ssl.enable}") String ssl, @Value("${mail.smtp.user}") String user
+    ) {
+        this.activateSubject = activateSubject;
+        this.activateText = activateText;
+        this.auth = auth;
+        this.host = host;
+        this.iterations = iterations;
+        this.password = password;
+        this.platform = platform;
+        this.port = port;
+        this.protocol = protocol;
+        this.resetSubject = resetSubject;
+        this.resetText = resetText;
+        this.secret = secret;
+        this.ssl = ssl;
+        this.user = user;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    public String getActivateSubject() {
+        return activateSubject;
+    }
+
+    public String getActivateText() {
+        return activateText;
+    }
+
+    public String getAuth() {
+        return auth;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public String getResetSubject() {
+        return resetSubject;
+    }
+
+    public String getResetText() {
+        return resetText;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public String getSsl() {
+        return ssl;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public int getIterations() {
+        return iterations;
+    }
 }
