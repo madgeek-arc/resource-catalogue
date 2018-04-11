a6440281bca47269217415c7be093a55da832996
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-04-11 12:58:53 +0300

    +analyticsService

---
+++ src/main/java/eu/einfracentral/service/AnalyticsService.java
@@ -0,0 +1,11 @@
+package eu.einfracentral.registry.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by pgl on 11/04/18.
+ */
+@Service("analyticsService")
+public class AnalyticsService {
+
+}
