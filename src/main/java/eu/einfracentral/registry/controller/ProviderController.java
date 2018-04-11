c8ad2e6ad70de05f68b2c8a76de15b1ed16d62c7
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-04-11 17:57:01 +0300

    +faves/ratings

--- src/main/java/eu/einfracentral/registry/controller/ProviderController.java
+++ src/main/java/eu/einfracentral/registry/controller/ProviderController.java
@@ -1,7 +1,7 @@
 package eu.einfracentral.registry.controller;
 
 import eu.einfracentral.domain.Provider;
-import eu.einfracentral.registry.service.*;
+import eu.einfracentral.registry.service.ProviderService;
 import io.swagger.annotations.ApiOperation;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +24,17 @@ public class ProviderController extends ResourceController<Provider> {
     @RequestMapping(value = "visits/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<Map<String, Integer>> visits(@PathVariable("id") String id, @ApiIgnore @CookieValue(defaultValue = "") String jwt) {
         return new ResponseEntity<>(((ProviderService) service).visits(id), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Returns the provider favourites")
+    @RequestMapping(value = "favourites/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Map<String, Integer>> favourites(@PathVariable("id") String id, @ApiIgnore @CookieValue(defaultValue = "") String jwt) {
+        return new ResponseEntity<>(((ProviderService) service).favourites(id), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Returns the provider ratings")
+    @RequestMapping(value = "ratings/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Map<String, Float>> ratings(@PathVariable("id") String id, @ApiIgnore @CookieValue(defaultValue = "") String jwt) {
+        return new ResponseEntity<>(((ProviderService) service).ratings(id), HttpStatus.OK);
     }
 }
