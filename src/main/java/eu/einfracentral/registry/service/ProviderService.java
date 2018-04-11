8752999c45c8b79e2f17b2b47c2c9443307b058b
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-04-11 17:56:50 +0300

    +faves/ratings

--- src/main/java/eu/einfracentral/registry/service/ProviderService.java
+++ src/main/java/eu/einfracentral/registry/service/ProviderService.java
@@ -10,4 +10,6 @@ import org.springframework.stereotype.Service;
 @Service("providerService")
 public interface ProviderService extends ResourceService<Provider> {
     Map<String, Integer> visits(String id);
+    Map<String, Integer> favourites(String id);
+    Map<String, Float> ratings(String id);
 }
