44d0c4caa76e1d4462dc392875fc46398b80e4a0
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-04-11 12:59:23 +0300

    Implemented time series of visits for provider

--- src/main/java/eu/einfracentral/registry/manager/ProviderManager.java
+++ src/main/java/eu/einfracentral/registry/manager/ProviderManager.java
@@ -1,14 +1,19 @@
 package eu.einfracentral.registry.manager;
 
-import eu.einfracentral.domain.Provider;
-import eu.einfracentral.registry.service.ProviderService;
-import org.springframework.stereotype.Service;
+import eu.einfracentral.domain.*;
+import eu.einfracentral.registry.service.*;
+import eu.openminted.registry.core.domain.FacetFilter;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by pgl on 26/7/2017.
  */
-@Service("providerService")
+@org.springframework.stereotype.Service("providerService")
 public class ProviderManager extends ResourceManager<Provider> implements ProviderService {
+    @Autowired
+    private ServiceService serviceService;
+
     public ProviderManager() {
         super(Provider.class);
     }
@@ -19,7 +24,15 @@ public class ProviderManager extends ResourceManager<Provider> implements Provid
     }
 
     @Override
-    public Integer visits(String id) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public Map<String, Integer> visits(String id) {
+        Map<String, Integer> ret = new HashMap<>();
+        for (Service s : serviceService.getAll(new FacetFilter()).getResults()) {
+            Map<String, Integer> visits = serviceService.visits(s.getId());
+            visits.forEach((k, v) -> {
+                ret.putIfAbsent(k, 0);
+                ret.put(k, ret.get(k) + v);
+            });
+        }
+        return ret;
     }
 }
