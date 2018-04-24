5997e15139d502abad162f3b468dd707ef713b26
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-04-24 13:07:03 +0300

    +provider put

--- src/main/java/eu/einfracentral/registry/controller/ProviderController.java
+++ src/main/java/eu/einfracentral/registry/controller/ProviderController.java
@@ -2,6 +2,7 @@ package eu.einfracentral.registry.controller;
 
 import eu.einfracentral.domain.Provider;
 import eu.einfracentral.registry.service.ProviderService;
+import eu.openminted.registry.core.exception.ResourceNotFoundException;
 import io.swagger.annotations.ApiOperation;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,11 @@ public class ProviderController extends ResourceController<Provider> {
     @Autowired
     ProviderController(ProviderService service) {
         super(service);
+    }
+
+    @ApiOperation(value = "Updates provider info")
+    @RequestMapping(method = RequestMethod.PUT, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseEntity<Provider> update(@RequestBody Provider provider, @ApiIgnore @CookieValue(defaultValue = "") String jwt) throws ResourceNotFoundException {
+        return super.update(provider, jwt);
     }
 }
