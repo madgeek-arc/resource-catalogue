b480969fe31633cc24e309236167e2b6b9067a59
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-04-25 16:23:18 +0300

    -rating

--- src/main/java/eu/einfracentral/registry/controller/ServiceController.java
+++ src/main/java/eu/einfracentral/registry/controller/ServiceController.java
@@ -73,11 +73,6 @@ public class ServiceController extends ResourceController<Service> {
         return super.getBy(field, jwt);
     }
 
-
-    @ApiOperation(value = "Returns the service rating")
-    @RequestMapping(value = "rating/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Integer> rating(@PathVariable("id") String id, @ApiIgnore @CookieValue(defaultValue = "") String jwt) {
-        return new ResponseEntity<>(((ServiceService) service).rating(id), HttpStatus.OK);
     @ApiOperation(value = "Retrieves service versions.")
     @RequestMapping(path = "versions", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
     public ResponseEntity<List<Service>> versions(@PathVariable String id, @PathVariable String version, @ApiIgnore @CookieValue(defaultValue = "") String jwt) throws ResourceNotFoundException {
