89079b19af57133446553f09f27ab4bbbf6c4eb2
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-04-25 19:09:51 +0300

    now setting event IDs

--- src/main/java/eu/einfracentral/registry/manager/EventManager.java
+++ src/main/java/eu/einfracentral/registry/manager/EventManager.java
@@ -1,7 +1,11 @@
 package eu.einfracentral.registry.manager;
 
 import eu.einfracentral.domain.Event;
+import eu.einfracentral.exception.ResourceException;
 import eu.einfracentral.registry.service.EventService;
+import eu.openminted.registry.core.domain.Resource;
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service("eventService")
@@ -13,5 +17,11 @@ public class EventManager extends ResourceManager<Event> implements EventService
     @Override
     public String getResourceType() {
         return "event";
+    }
+
+    @Override
+    public Event add(Event event) {
+        event.setId(UUID.randomUUID().toString());
+        return super.add(event);
     }
 }
