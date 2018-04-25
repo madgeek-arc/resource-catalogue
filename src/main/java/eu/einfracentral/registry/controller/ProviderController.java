24dced1730155f86821bc83d9540b010c6f0b340
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-04-25 16:21:41 +0300

    +provider get

--- src/main/java/eu/einfracentral/registry/controller/ProviderController.java
+++ src/main/java/eu/einfracentral/registry/controller/ProviderController.java
@@ -1,9 +1,10 @@
 package eu.einfracentral.registry.controller;
 
-import eu.einfracentral.domain.Provider;
+import eu.einfracentral.domain.*;
 import eu.einfracentral.registry.service.ProviderService;
 import eu.openminted.registry.core.exception.ResourceNotFoundException;
 import io.swagger.annotations.ApiOperation;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -24,5 +25,11 @@ public class ProviderController extends ResourceController<Provider> {
     @RequestMapping(method = RequestMethod.PUT, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
     public ResponseEntity<Provider> update(@RequestBody Provider provider, @ApiIgnore @CookieValue(defaultValue = "") String jwt) throws ResourceNotFoundException {
         return super.update(provider, jwt);
+    }
+
+    @ApiOperation(value = "Returns the provider")
+    @RequestMapping(path = "{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseEntity<Provider> get(@PathVariable("id") String id, @ApiIgnore @CookieValue(defaultValue = "") String jwt) {
+        return super.get(id, jwt);
     }
 }
