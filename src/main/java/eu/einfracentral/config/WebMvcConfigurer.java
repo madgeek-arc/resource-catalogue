12b0e059679e01401350cee799a6be8cedb225ca
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-05-03 15:54:27 +0300

    +useSuffixPatternMatch

--- src/main/java/eu/einfracentral/config/WebMvcConfigurer.java
+++ src/main/java/eu/einfracentral/config/WebMvcConfigurer.java
@@ -6,10 +6,10 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 @EnableWebMvc
 public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
-//    @Override
-//    public void configurePathMatch(PathMatchConfigurer configurer) {
-//        configurer.setUseSuffixPatternMatch(false);
-//    }
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseSuffixPatternMatch(false);
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
