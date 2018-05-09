7c3f7763cf595b739ebd963a68aa4d36d4c8f94a
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-05-09 13:26:23 +0300

    autoformatting

--- src/main/java/eu/einfracentral/registry/manager/EventManager.java
+++ src/main/java/eu/einfracentral/registry/manager/EventManager.java
@@ -1,11 +1,8 @@
 package eu.einfracentral.registry.manager;
 
 import eu.einfracentral.domain.Event;
-import eu.einfracentral.exception.ResourceException;
 import eu.einfracentral.registry.service.EventService;
-import eu.openminted.registry.core.domain.Resource;
 import java.util.UUID;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
