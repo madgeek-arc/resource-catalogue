b149cc360eee1ace201149257790133adb285f0b
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-05-15 16:40:48 +0300

    now gnostic of WW region as well

--- src/main/java/eu/einfracentral/registry/manager/VocabularyManager.java
+++ src/main/java/eu/einfracentral/registry/manager/VocabularyManager.java
@@ -5,17 +5,19 @@ import eu.einfracentral.domain.Vocabulary;
 import eu.einfracentral.registry.service.VocabularyService;
 import java.io.IOException;
 import java.net.*;
+import java.util.*;
 import java.util.stream.Stream;
 import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 @Component
 public class VocabularyManager extends ResourceManager<Vocabulary> implements VocabularyService {
-    private String[] euURLs = {"https://restcountries.eu/rest/v2/regionalbloc/EU?fields=alpha2Code"};
-    private String[] eu;
+    private Map<String, Region> regions = new HashMap<>();
 
     public VocabularyManager() {
         super(Vocabulary.class);
+        regions.put("EU", new Region("https://restcountries.eu/rest/v2/regionalbloc/EU?fields=alpha2Code"));
+        regions.put("WW", new Region("https://restcountries.eu/rest/v2?fields=alpha2Code"));
     }
 
     @Override
@@ -24,31 +26,32 @@ public class VocabularyManager extends ResourceManager<Vocabulary> implements Vo
     }
 
     @Override
-    public String[] getEU() {
-        if (eu == null || eu.length == 0) {
-            fetchEU();
+    public String[] getRegion(String name) {
+        Region region = regions.get(name);
+        if (region.getMembers() == null || region.getMembers().length == 0) {
+            fetchRegion(region);
         }
-        return eu;
+        return region.getMembers();
+    }
+
+    private void fetchRegion(Region region) {
+        try {
+            HttpURLConnection c = (HttpURLConnection) new URL(region.getSource()).openConnection();
+            c.setRequestMethod("GET");
+            c.setRequestProperty("Accept", "application/json");
+            if (c.getResponseCode() == 200) {
+                Country[] countries = new ObjectMapper().readValue(c.getInputStream(), Country[].class);
+                c.disconnect();
+                region.setMembers(Stream.of(countries).map(e -> e.alpha2Code).toArray(String[]::new));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @PostConstruct
-    private void fetchEU() {
-        //do not be fooled; this code only supports a specific type of rest data, and it's outside its scope for now to support any more
-        Stream.of(euURLs).anyMatch(url -> {
-            try {
-                HttpURLConnection c = (HttpURLConnection) new URL(url).openConnection();
-                c.setRequestMethod("GET");
-                c.setRequestProperty("Accept", "application/json");
-                if (c.getResponseCode() == 200) {
-                    Country[] countries = new ObjectMapper().readValue(c.getInputStream(), Country[].class);
-                    c.disconnect();
-                    this.eu = Stream.of(countries).map(e -> e.alpha2Code).toArray(String[]::new);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return this.eu != null && this.eu.length > 0;
-        });
+    private void postConstruct() {
+        regions.forEach((key, value) -> this.fetchRegion(value));
     }
 
     private static class Country {
@@ -57,5 +60,26 @@ public class VocabularyManager extends ResourceManager<Vocabulary> implements Vo
         public String getAlpha2Code() { return alpha2Code; }
 
         public void setAlpha2Code(String alpha2Code) { this.alpha2Code = alpha2Code; }
+    }
+
+    private static class Region {
+        private String[] members = null;
+        private String source;
+
+        private Region(String source) {
+            this.source = source;
+        }
+
+        public String[] getMembers() {
+            return members;
+        }
+
+        public void setMembers(String[] members) {
+            this.members = members;
+        }
+
+        public String getSource() {
+            return source;
+        }
     }
 }
