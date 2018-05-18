6f201adcaa462391911bc1b6f736bf11e60ad525
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-05-18 13:38:09 +0300

    removing code that was meant to be ran once by a specific machine

--- src/main/java/eu/einfracentral/registry/service/ServiceService.java
+++ src/main/java/eu/einfracentral/registry/service/ServiceService.java
@@ -1,8 +1,6 @@
 package eu.einfracentral.registry.service;
 
 import eu.einfracentral.domain.Service;
-import java.util.List;
 
 public interface ServiceService extends ResourceService<Service> {
-    List<Service> fixCatsAndSubcats();
 }
