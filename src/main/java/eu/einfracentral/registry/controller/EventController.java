032afcfc82aff75124d8636564de735e077907cc
Konstantinos Spyrou <spyroukon@gmail.com>
2018-07-05 11:36:26 +0300

    suppress uchecked warnings

--- src/main/java/eu/einfracentral/registry/controller/EventController.java
+++ src/main/java/eu/einfracentral/registry/controller/EventController.java
@@ -23,6 +23,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("event")
+@SuppressWarnings("unchecked")
 public class EventController extends ResourceController<Event> {
 
     @Autowired
@@ -36,7 +37,7 @@ public class EventController extends ResourceController<Event> {
     @Autowired
     private ParserService parserService;
 
-    Logger logger = Logger.getLogger(EventController.class);
+    private final Logger logger = Logger.getLogger(EventController.class);
 
     @ApiOperation("Retrieve all events.")
     @RequestMapping(path = "events/all", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
@@ -55,27 +56,27 @@ public class EventController extends ResourceController<Event> {
 //    @ApiIgnore
     @ApiOperation("Retrieve all the favourited events.")
     @RequestMapping(path = "favourite/all", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public ResponseEntity<List<Event>> getFavourites() throws ExecutionException, InterruptedException {
+    public ResponseEntity<List<Event>> getFavourites() {
         return new ResponseEntity<>(getEvents(Event.UserActionType.FAVOURITE.getKey()), HttpStatus.OK);
     }
 
     @ApiOperation("Retrieve all the favourited events of a user with the specified ID.")
     @RequestMapping(path = "favourite/all/user/{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public ResponseEntity<List<Event>> getUserFavourites(@PathVariable String id) throws ExecutionException, InterruptedException {
+    public ResponseEntity<List<Event>> getUserFavourites(@PathVariable String id) {
         return new ResponseEntity<>(getUserEvents(Event.UserActionType.FAVOURITE.getKey(), id), HttpStatus.OK);
     }
 
 //    @ApiIgnore
     @ApiOperation("Retrieve all the favourited events of a infraService with the specified ID.")
     @RequestMapping(path = "favourite/all/service/{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public ResponseEntity<List<Event>> getServiceFavourites(@PathVariable String id) throws ExecutionException, InterruptedException {
+    public ResponseEntity<List<Event>> getServiceFavourites(@PathVariable String id) {
         return new ResponseEntity<>(getServiceEvents(Event.UserActionType.FAVOURITE.getKey(), id), HttpStatus.OK);
     }
 
 
     @ApiOperation("Set a service as favorite for a user.")
-    @RequestMapping(path = "favorite/service/{sId}/user/{uId}", method = RequestMethod.PUT, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public ResponseEntity<Event> setFavourite(@PathVariable String sId, @PathVariable String uId) throws ExecutionException, InterruptedException, ResourceNotFoundException {
+    @RequestMapping(path = "favorite/service/{sId}/user/{uId}", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseEntity<Event> setFavourite(@PathVariable String sId, @PathVariable String uId) throws ResourceNotFoundException {
         // TODO: check if user and service exists ?
         return new ResponseEntity<>(toggleFavourite(sId, uId), HttpStatus.OK);
     }
@@ -83,7 +84,7 @@ public class EventController extends ResourceController<Event> {
 
     @ApiOperation("Check if a service is favourited by a user.")
     @RequestMapping(path = "favourite/service/{sId}/user/{uId}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public ResponseEntity<String> getFavourite(@PathVariable String sId, @PathVariable String uId) throws ExecutionException, InterruptedException {
+    public ResponseEntity<String> getFavourite(@PathVariable String sId, @PathVariable String uId) {
         List<Event> events = getEvents(Event.UserActionType.FAVOURITE.getKey(), sId, uId);
         if (events.size() > 0) {
             return new ResponseEntity<>(events.get(0).getValue(), HttpStatus.OK);
@@ -92,32 +93,31 @@ public class EventController extends ResourceController<Event> {
     }
 
 
-
     // <-------- FAVORITES
 
     // RATINGS ---------->
 //    @ApiIgnore
     @ApiOperation("Retrieve all rating events.")
     @RequestMapping(path = "rating/all", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public ResponseEntity<List<Event>> getRatings() throws ExecutionException, InterruptedException {
+    public ResponseEntity<List<Event>> getRatings() {
         return new ResponseEntity<>(getEvents(Event.UserActionType.RATING.getKey()), HttpStatus.OK);
     }
 
     @ApiOperation("Retrieve all the rating events of a user with the specified ID.")
     @RequestMapping(path = "rating/all/user/{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public ResponseEntity<List<Event>> getUserRating(@PathVariable String id) throws ExecutionException, InterruptedException {
+    public ResponseEntity<List<Event>> getUserRating(@PathVariable String id) {
         return new ResponseEntity<>(getUserEvents(Event.UserActionType.RATING.getKey(), id), HttpStatus.OK);
     }
 
-    //    @ApiIgnore
+//    @ApiIgnore
     @ApiOperation("Retrieve all the rating events of a infraService with the specified ID.")
     @RequestMapping(path = "rating/all/service/{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public ResponseEntity<List<Event>> getServiceRatings(@PathVariable String id) throws ExecutionException, InterruptedException {
+    public ResponseEntity<List<Event>> getServiceRatings(@PathVariable String id) {
         return new ResponseEntity<>(getServiceEvents(Event.UserActionType.RATING.getKey(), id), HttpStatus.OK);
     }
 
     @ApiOperation("Set a rating to a service from the given user.")
-    @RequestMapping(path = "rating/service/{sId}/user/{uId}", method = RequestMethod.PUT, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    @RequestMapping(path = "rating/service/{sId}/user/{uId}", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
     public ResponseEntity<Event> setUserRating(@PathVariable String sId, @PathVariable String uId, @RequestParam("rating") String rating)
             throws ExecutionException, InterruptedException, ResourceNotFoundException {
         // TODO: check if user and service exists ?
@@ -127,7 +127,7 @@ public class EventController extends ResourceController<Event> {
 
     @ApiOperation("Get the rating of a user for a specific service.")
     @RequestMapping(path = "rating/service/{sId}/user/{uId}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public ResponseEntity<String> getRating(@PathVariable String sId, @PathVariable String uId) throws ExecutionException, InterruptedException {
+    public ResponseEntity<String> getRating(@PathVariable String sId, @PathVariable String uId) {
         List<Event> events = getEvents(Event.UserActionType.RATING.getKey(), sId, uId);
         if (events.size() > 0) {
             return new ResponseEntity<>(events.get(0).getValue(), HttpStatus.OK);
@@ -138,7 +138,7 @@ public class EventController extends ResourceController<Event> {
     // <---------- RATINGS
 
 
-    private Event toggleFavourite(String serviceId, String userId) throws ExecutionException, InterruptedException, ResourceNotFoundException {
+    private Event toggleFavourite(String serviceId, String userId) throws ResourceNotFoundException {
         List<Event> events = getEvents(Event.UserActionType.FAVOURITE.getKey(), serviceId, userId);
         Event event;
         if (events.size() == 0) {
@@ -148,8 +148,7 @@ public class EventController extends ResourceController<Event> {
             event.setType(Event.UserActionType.FAVOURITE.getKey());
             event.setValue("true");
             event = service.add(event);
-        }
-        else {
+        } else {
             event = events.get(0);
             event = booleanToggleValue(event);
             event = service.update(event);
@@ -158,7 +157,7 @@ public class EventController extends ResourceController<Event> {
     }
 
 
-    private Event setRating(String serviceId, String userId, String value) throws ExecutionException, InterruptedException, ResourceNotFoundException {
+    private Event setRating(String serviceId, String userId, String value) throws ResourceNotFoundException {
         List<Event> events = getEvents(Event.UserActionType.RATING.getKey(), serviceId, userId);
         Event event;
         if (events.size() == 0) {
@@ -168,8 +167,7 @@ public class EventController extends ResourceController<Event> {
             event.setType(Event.UserActionType.RATING.getKey());
             event.setValue(value);
             event = service.add(event);
-        }
-        else {
+        } else {
             event = events.get(0);
             event.setValue(value);
             event = service.update(event);
@@ -177,27 +175,27 @@ public class EventController extends ResourceController<Event> {
         return event;
     }
 
-    private List<Event> getEvents(String eventType) throws ExecutionException, InterruptedException {
+    private List<Event> getEvents(String eventType) {
         Paging<Resource> event_resources = searchService
                 .cqlQuery("type=" + eventType, "event");
         return pagingToList(event_resources);
     }
 
-    private List<Event> getEvents(String eventType, String serviceId, String userId) throws ExecutionException, InterruptedException {
+    private List<Event> getEvents(String eventType, String serviceId, String userId) {
         Paging<Resource> event_resources = searchService
                 .cqlQuery("type=" + eventType + " AND service=" + serviceId + " AND event_user=" + userId, "event");
         return pagingToList(event_resources);
     }
 
-    private List<Event> getServiceEvents(String eventType, String serviceId) throws ExecutionException, InterruptedException {
+    private List<Event> getServiceEvents(String eventType, String serviceId) {
         Paging<Resource> event_resources = searchService
                 .cqlQuery("type=" + eventType + " AND service=" + serviceId, "event");
         return pagingToList(event_resources);
     }
 
-    private List<Event> getUserEvents(String eventType, String userId) throws ExecutionException, InterruptedException {
+    private List<Event> getUserEvents(String eventType, String userId) {
         Paging<Resource> event_resources = searchService
-                .cqlQuery("type=" + eventType+ " AND event_user=" + userId, "event");
+                .cqlQuery("type=" + eventType + " AND event_user=" + userId, "event");
         return pagingToList(event_resources);
     }
 
