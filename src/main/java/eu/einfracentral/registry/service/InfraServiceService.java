084cbbf5838ac9032accb1cfc03e47d2f91ca9ea
Kostas Spyrou <spyroukostas@msn.com>
2018-07-20 22:23:31 +0300

    created method getHistory()

--- src/main/java/eu/einfracentral/registry/service/InfraServiceService.java
+++ src/main/java/eu/einfracentral/registry/service/InfraServiceService.java
@@ -2,6 +2,8 @@ package eu.einfracentral.registry.service;
 
 
 import eu.einfracentral.domain.InfraService;
+import eu.einfracentral.domain.ServiceHistory;
+import eu.openminted.registry.core.domain.Browsing;
 import eu.openminted.registry.core.domain.Resource;
 
 public interface InfraServiceService extends ServiceInterface<InfraService> {
@@ -13,4 +15,11 @@ public interface InfraServiceService extends ServiceInterface<InfraService> {
      * @return Resource
      */
     Resource getResource(String id, String version);
+
+    /**
+     * Get the History of the InfraService with the specified id.
+     * @param id
+     * @return
+     */
+    Browsing<ServiceHistory> getHistory(String id);
 }
