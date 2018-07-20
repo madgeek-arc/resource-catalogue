539445675726503e074275086667831a3660fb8b
Konstantinos Spyrou <spyroukon@gmail.com>
2018-07-20 17:21:45 +0300

    spring security

--- src/main/java/eu/einfracentral/config/AppInitializer.java
+++ src/main/java/eu/einfracentral/config/AppInitializer.java
@@ -1,6 +1,8 @@
 package eu.einfracentral.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.WebApplicationInitializer; //you can use this to disable spring security
+
 
 public class AppInitializer extends AbstractSecurityWebApplicationInitializer {
 
