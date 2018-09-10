7d5a81316298f00a8643b94064dbca4e4a2c8f37
Konstantinos Spyrou <spyroukon@gmail.com>
2018-09-10 15:22:25 +0300

    Moved to registry core 2.0.1

--- src/main/java/eu/einfracentral/registry/controller/ManagerController.java
+++ src/main/java/eu/einfracentral/registry/controller/ManagerController.java
@@ -3,11 +3,12 @@ package eu.einfracentral.registry.controller;
 import eu.einfracentral.domain.Manager;
 import eu.einfracentral.registry.service.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("manager")
-public class ManagerController extends ResourceController<Manager> {
+public class ManagerController extends ResourceController<Manager, Authentication> {
     @Autowired
     ManagerController(ManagerService service) {
         super(service);
