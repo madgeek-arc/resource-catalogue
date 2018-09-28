8338584d6bf9b396725e3809e73ad10f3147cfca
Konstantinos Spyrou <spyroukon@gmail.com>
2018-09-28 17:13:21 +0300

    created EICSESSION

--- src/main/java/eu/einfracentral/config/ServiceConfig.java
+++ src/main/java/eu/einfracentral/config/ServiceConfig.java
@@ -4,6 +4,12 @@ import eu.einfracentral.domain.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.MapSessionRepository;
+import org.springframework.session.SessionRepository;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -16,11 +22,28 @@ import javax.xml.bind.JAXBException;
         "eu.einfracentral.registry.manager",
 //        "eu.einfracentral.core",
         "eu.einfracentral.service"})
-public class ServiceConfig {
+@EnableSpringHttpSession
+public class ServiceConfig extends AbstractHttpSessionApplicationInitializer {
 
     @Bean
     JAXBContext eicJAXBContext() throws JAXBException {
         return JAXBContext.newInstance(Event.class, Manager.class, Provider.class,
                 Service.class, User.class, Vocabulary.class, InfraService.class);
+    }
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer defaultCookieSerializer = new DefaultCookieSerializer();
+        defaultCookieSerializer.setCookieName("EICSESSION");
+        defaultCookieSerializer.setCookiePath("/");
+//        defaultCookieSerializer.setUseSecureCookie(Boolean.parseBoolean(env.getProperty(COOKIE_SECURE)));
+        defaultCookieSerializer.setUseHttpOnlyCookie(true);
+//        defaultCookieSerializer.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$");
+        return defaultCookieSerializer;
+    }
+
+    @Bean
+    public SessionRepository sessionRepository() {
+        return new MapSessionRepository();
     }
 }
--- src/main/java/eu/einfracentral/config/security/SessionSecurityConfig.java
+++ src/main/java/eu/einfracentral/config/security/SessionSecurityConfig.java
@@ -193,4 +193,5 @@ public class SessionSecurityConfig extends WebSecurityConfigurerAdapter {
         return ret;
     }
 
+
 }
