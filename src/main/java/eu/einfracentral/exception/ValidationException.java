c6cf248c0c62b18d48da294156a929e13f25f03a
MIke <michael.zouros@outlook.com>
2018-10-09 12:25:30 +0300

    created validation exception

---
+++ src/main/java/eu/einfracentral/exception/ValidationException.java
@@ -0,0 +1,29 @@
+package eu.einfracentral.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ValidationException extends RuntimeException {
+    private HttpStatus status;
+
+    public ValidationException(HttpStatus status) {
+        this("ValidationException", status);
+    }
+
+    public ValidationException(String msg, HttpStatus status) {
+        super(msg);
+        this.setStatus(status);
+    }
+
+    public ValidationException(Exception e, HttpStatus status) {
+        super(e);
+        this.setStatus(status);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    private void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+}
