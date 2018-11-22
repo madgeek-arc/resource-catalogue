d346fae46ba1e36c8f888df098d39e4f85b82c46
Konstantinos Spyrou <spyroukon@gmail.com>
2018-11-22 16:42:18 +0200

    log user info for debugging

--- eic-registry/src/main/java/eu/einfracentral/config/security/EICAuthoritiesMapper.java
+++ eic-registry/src/main/java/eu/einfracentral/config/security/EICAuthoritiesMapper.java
@@ -77,6 +77,10 @@ public class EICAuthoritiesMapper implements OIDCAuthoritiesMapper {
         if (userInfo.getEmail() != null) {
             String email = userInfo.getEmail();
             SimpleGrantedAuthority authority = userRolesMap.get(email);
+
+            // TODO: remove this
+            logger.info(String.format("AAI DEBUG:  userInfo: ", userInfo.toJson().toString()));
+
             if (authority != null) {
                 logger.info(String.format("%s mapped as %s", email, authority.getAuthority()));
                 out.add(authority);
