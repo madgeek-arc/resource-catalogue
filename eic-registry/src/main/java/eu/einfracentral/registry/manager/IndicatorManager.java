36b779189e89583e952c02e3586426708b89f895
MIke <michael.zouros@outlook.com>
2019-02-07 12:40:48 +0200

    WIP: KPIs

--- eic-registry/src/main/java/eu/einfracentral/registry/manager/IndicatorManager.java
+++ eic-registry/src/main/java/eu/einfracentral/registry/manager/IndicatorManager.java
@@ -1,11 +1,15 @@
 package eu.einfracentral.registry.manager;
 
 import eu.einfracentral.domain.Indicator;
+import eu.einfracentral.exception.ValidationException;
 import eu.einfracentral.registry.service.IndicatorService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
-public class IndicatorManager extends ResourceManager<Indicator> implements IndicatorService {
+public class IndicatorManager extends ResourceManager<Indicator> implements IndicatorService<Indicator, Authentication> {
+
+    //@Autowired
     public IndicatorManager() {
         super(Indicator.class);
     }
@@ -14,4 +18,37 @@ public class IndicatorManager extends ResourceManager<Indicator> implements Indi
     public String getResourceType() {
         return "indicator";
     }
+
+
+    @Override
+    public Indicator add(Indicator indicator, Authentication auth) {
+        Indicator ret;
+
+        //TODO: Create a validation method
+        //Indicator's Validation
+        if (indicator.getId() == null || indicator.getId().equals("")) {
+            throw new ValidationException("Indicator's id cannot be 'null' or 'empty'");
+        }
+        if (indicator.getDescription() == null || indicator.getDescription().equals("")) {
+            throw new ValidationException("Indicator's description cannot be 'null' or 'empty'");
+        }
+
+//        super.add(indicator, auth);
+//        return indicator;
+        ret = super.add(indicator, null);
+        return ret;
+
+    }
+
+    @Override
+    public Indicator get(String id, Authentication auth) {
+        return null;
+
+    }
+
+    @Override
+    public void delete(Indicator indicator) {
+        super.delete(indicator);
+    }
+
 }
