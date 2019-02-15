0a8e19dff2ee596848330f595abe428333888f04
Konstantinos Spyrou <spyroukon@gmail.com>
2019-02-15 13:55:51 +0200

    added security and getAll method to indicator and measurement controllers

--- eic-registry-rest/src/main/java/eu/einfracentral/registry/controller/MeasurementController.java
+++ eic-registry-rest/src/main/java/eu/einfracentral/registry/controller/MeasurementController.java
@@ -1,9 +1,13 @@
 package eu.einfracentral.registry.controller;
 
+import eu.einfracentral.domain.Indicator;
 import eu.einfracentral.domain.Measurement;
 import eu.einfracentral.registry.service.MeasurementService;
+import eu.openminted.registry.core.domain.Paging;
 import eu.openminted.registry.core.exception.ResourceNotFoundException;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,7 +20,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
-//@ApiIgnore
+import java.util.Map;
+
 @RestController
 @RequestMapping("measurement")
 @Api(value = "Get information about a Measurement")
@@ -38,23 +43,36 @@ public class MeasurementController extends ResourceController<Measurement, Authe
         return super.get(id, auth);
     }
 
-    @CrossOrigin
+    @Override
+    @ApiOperation(value = "Filter a list of measurements based on a set of filters or get a list of all measurements in the eInfraCentral Catalogue")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "query", value = "Keyword to refine the search", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "from", value = "Starting index in the resultset", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "quantity", value = "Quantity of services to be fetched", dataType = "string", paramType = "query")
+    })
+    @RequestMapping(path = "all", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseEntity<Paging<Measurement>> getAll(@ApiIgnore @RequestParam Map<String, Object> allRequestParams, @ApiIgnore Authentication authentication) {
+        return super.getAll(allRequestParams, authentication);
+    }
+
+    @Override
     @ApiOperation(value = "Adds the given measurement.")
     @RequestMapping(method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_PROVIDER') and @securityService.userIsServiceProviderAdmin(#auth,#measurement.serviceId)")
     public ResponseEntity<Measurement> add(@RequestBody Measurement measurement, @ApiIgnore Authentication auth) {
         return super.add(measurement, auth);
     }
 
+    @Override
     @ApiOperation(value = "Updates the measurement assigned the given id with the given measurement, keeping version of revisions.")
     @RequestMapping(method = RequestMethod.PUT, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_PROVIDER') and @securityService.userIsServiceProviderAdmin(#auth,#measurement.serviceId)")
     public ResponseEntity<Measurement> update(@RequestBody Measurement measurement, @ApiIgnore Authentication auth) throws ResourceNotFoundException {
         return super.update(measurement, auth);
     }
 
     @ApiOperation(value = "Deletes the given measurement")
-    @RequestMapping(path = {"delete/{id}"}, method = RequestMethod.DELETE, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    @RequestMapping(path = {"{id}"}, method = RequestMethod.DELETE, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Measurement> delete(@PathVariable("id") String id, @ApiIgnore Authentication auth) throws ResourceNotFoundException {
         Measurement measurement = measurementManager.get(id);
@@ -62,6 +80,5 @@ public class MeasurementController extends ResourceController<Measurement, Authe
         measurementManager.delete(measurement);
         return new ResponseEntity<>(measurement, HttpStatus.OK);
     }
-
 
 }
