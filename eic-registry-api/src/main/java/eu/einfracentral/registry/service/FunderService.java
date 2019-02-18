7481cb223d1b4489ec9c20eb96517bc06f331341
Konstantinos Spyrou <spyroukon@gmail.com>
2019-02-18 14:14:09 +0200

    created Funder class

---
+++ eic-registry-api/src/main/java/eu/einfracentral/registry/service/FunderService.java
@@ -0,0 +1,7 @@
+package eu.einfracentral.registry.service;
+
+import eu.einfracentral.domain.Funder;
+import org.springframework.security.core.Authentication;
+
+public interface FunderService extends ResourceService<Funder, Authentication> {
+}
---
+++ eic-registry-rest/src/main/java/eu/einfracentral/registry/controller/FunderController.java
@@ -0,0 +1,78 @@
+package eu.einfracentral.registry.controller;
+
+import eu.einfracentral.domain.Funder;
+import eu.einfracentral.domain.Indicator;
+import eu.einfracentral.registry.service.FunderService;
+import eu.openminted.registry.core.domain.Paging;
+import eu.openminted.registry.core.exception.ResourceNotFoundException;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.Map;
+
+@ApiIgnore
+@RestController
+@RequestMapping("funder")
+public class FunderController extends ResourceController<Funder, Authentication> {
+
+    private FunderService funderService;
+
+    @Autowired
+    FunderController(FunderService funderService) {
+        super(funderService);
+        this.funderService = funderService;
+    }
+
+    @Override
+    @ApiOperation(value = "Returns the Funder with the given id.")
+    @RequestMapping(path = "{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseEntity<Funder> get(@PathVariable("id") String id, @ApiIgnore Authentication auth) {
+        return super.get(id, auth);
+    }
+
+    @Override
+    @ApiOperation(value = "Filter a list of Funders based on a set of filters or get a list of all Funders.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "query", value = "Keyword to refine the search", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "from", value = "Starting index in the resultset", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "quantity", value = "Quantity of services to be fetched", dataType = "string", paramType = "query")
+    })
+    @RequestMapping(path = "all", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseEntity<Paging<Funder>> getAll(@ApiIgnore @RequestParam Map<String, Object> allRequestParams, @ApiIgnore Authentication authentication) {
+        return super.getAll(allRequestParams, authentication);
+    }
+
+    @Override
+    @ApiOperation(value = "Adds the given Funder.")
+    @RequestMapping(method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Funder> add(@RequestBody Funder funder, @ApiIgnore Authentication auth) {
+        return super.add(funder, auth);
+    }
+
+    @Override
+    @ApiOperation(value = "Updates the Funder assigned the given id with the given Funder, keeping a version of revisions.")
+    @RequestMapping(method = RequestMethod.PUT, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Funder> update(@RequestBody Funder funder, @ApiIgnore Authentication auth) throws ResourceNotFoundException {
+        return super.update(funder, auth);
+    }
+
+    @ApiOperation(value = "Deletes the given Funder")
+    @RequestMapping(path = {"{id}"}, method = RequestMethod.DELETE, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Funder> delete(@PathVariable("id") String id, @ApiIgnore Authentication auth) throws ResourceNotFoundException {
+        Funder funder = funderService.get(id);
+        funderService.delete(funder);
+        return new ResponseEntity<>(funder, HttpStatus.OK);
+    }
+}
