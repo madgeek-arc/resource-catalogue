85b7573f5044d3c808670eb4dc8981d400d2fdf2
Konstantinos Spyrou <spyroukon@gmail.com>
2019-03-26 12:56:06 +0200

    created method to format decimal digits in numeric values

--- eic-registry/src/main/java/eu/einfracentral/utils/TextUtils.java
+++ eic-registry/src/main/java/eu/einfracentral/utils/TextUtils.java
@@ -1,6 +1,11 @@
 package eu.einfracentral.utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class TextUtils {
+
+    private static final Logger logger = LogManager.getLogger(TextUtils.class);
 
     private TextUtils() {}
 
@@ -35,5 +40,34 @@ public class TextUtils {
      */
     public static String capitalizeFirstLetter(String text) {
         return text.substring(0, 1).toUpperCase() + text.substring(1);
+    }
+
+    /**
+     * Formats a numerical value's precision to a maximum number of decimal digits.
+     * @param value
+     * @param precision
+     * @return
+     */
+    public static String formatArithmeticPrecision(String value, int precision) {
+        if (!value.matches("^(?:(0?\\.)|([1-9]\\d*))\\.?\\d*$")) {
+            logger.error(String.format("Error: '%s' is not a valid numeric value", value));
+            throw new NumberFormatException("Not a valid numeric value...");
+        }
+        if (precision < 0) {
+            logger.error(String.format("Error: precision cannot be a negative value : %d", precision));
+            throw new NumberFormatException("Arithmetic precision cannot be a negative value");
+        }
+        String[] parts = value.split("\\.");
+        if (parts.length == 2) {
+            if (parts[1].length() > precision) {
+                parts[1] = parts[1].substring(0, precision);
+            }
+            if (precision > 0) {
+                return String.join(".", parts);
+            }
+            return parts[0];
+        } else {
+            return value;
+        }
     }
 }
