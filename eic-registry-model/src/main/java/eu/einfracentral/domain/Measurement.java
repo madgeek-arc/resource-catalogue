7af94733da586b0a7ebb6e671d9862515e5e0fe1
MIke <michael.zouros@outlook.com>
2019-03-26 15:35:59 +0200

    Added measurementUnit field in Measurement.class and updated MeasurementManager

--- eic-registry-model/src/main/java/eu/einfracentral/domain/Measurement.java
+++ eic-registry-model/src/main/java/eu/einfracentral/domain/Measurement.java
@@ -41,6 +41,10 @@ public class Measurement implements Identifiable {
     @ApiModelProperty(position = 8)
     private RangeValue rangeValue;
 
+    @XmlElement
+    @ApiModelProperty(position = 9)
+    private String measurementUnit;
+
 
     public Measurement() {
 
@@ -55,6 +59,7 @@ public class Measurement implements Identifiable {
         this.rangeValue = measurement.getRangeValue();
         this.time = measurement.getTime();
         this.locations = measurement.getLocations();
+        this.measurementUnit = measurement.getMeasurementUnit();
     }
 
     @Override
@@ -121,5 +126,13 @@ public class Measurement implements Identifiable {
 
     public void setValueIsRange(boolean valueIsRange) {
         this.valueIsRange = valueIsRange;
+    }
+
+    public String getMeasurementUnit() {
+        return measurementUnit;
+    }
+
+    public void setMeasurementUnit(String measurementUnit) {
+        this.measurementUnit = measurementUnit;
     }
 }
