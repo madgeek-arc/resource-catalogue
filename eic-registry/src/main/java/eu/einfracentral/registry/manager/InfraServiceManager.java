d1fe955bdbd03c1c774fa40f32e45dfe463fc982
Konstantinos Spyrou <spyroukon@gmail.com>
2019-05-13 12:15:29 +0300

    1. code refactoring 2. added logs

--- eic-registry/src/main/java/eu/einfracentral/registry/manager/InfraServiceManager.java
+++ eic-registry/src/main/java/eu/einfracentral/registry/manager/InfraServiceManager.java
@@ -1,15 +1,12 @@
 package eu.einfracentral.registry.manager;
 
 import eu.einfracentral.domain.*;
-import eu.einfracentral.exception.ValidationException;
 import eu.einfracentral.registry.service.InfraServiceService;
 import eu.einfracentral.service.SynchronizerService;
 import eu.einfracentral.utils.ObjectUtils;
 import eu.einfracentral.utils.ServiceValidators;
 import eu.openminted.registry.core.domain.FacetFilter;
 import eu.openminted.registry.core.domain.Paging;
-import eu.openminted.registry.core.service.SearchService;
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,24 +15,26 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.core.Authentication;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @org.springframework.stereotype.Service("infraServiceService")
 public class InfraServiceManager extends ServiceResourceManager implements InfraServiceService<InfraService, InfraService> {
 
     private static final Logger logger = LogManager.getLogger(InfraServiceManager.class);
 
-    private VocabularyManager vocabularyManager;
+    private ServiceValidators serviceValidators;
     private ProviderManager providerManager;
     private Random randomNumberGenerator;
     private SynchronizerService synchronizerService;
 
 
     @Autowired
-    public InfraServiceManager(VocabularyManager vocabularyManager, ProviderManager providerManager,
+    public InfraServiceManager(ServiceValidators serviceValidators, ProviderManager providerManager,
                                Random randomNumberGenerator, SynchronizerService synchronizerService) {
         super(InfraService.class);
-        this.vocabularyManager = vocabularyManager;
+        this.serviceValidators = serviceValidators;
         this.providerManager = providerManager;
         this.randomNumberGenerator = randomNumberGenerator;
         this.synchronizerService = synchronizerService;
@@ -134,11 +133,13 @@ public class InfraServiceManager extends ServiceResourceManager implements Infra
     @Scheduled(cron = "0 0 12 1/1 * ?") // daily at 12:00 PM
     @CacheEvict(value = "featuredServices", allEntries = true)
     public void refreshFeatured() {
+        logger.info("Deleting cache: 'featuredServices'");
     }
 
     @Override
     @Cacheable("featuredServices")
     public List<Service> createFeaturedServices() {
+        logger.info("Creating and caching 'featuredServices'");
         // TODO: return featured services (for now, it returns a random infraService for each provider)
         FacetFilter ff = new FacetFilter();
         ff.setQuantity(10000);
@@ -187,18 +188,19 @@ public class InfraServiceManager extends ServiceResourceManager implements Infra
     public boolean validate(InfraService service) {
         //If we want to reject bad vocab ids instead of silently accept, here's where we do it
         //just check if validateVocabularies did anything or not
-        validateServices(service);
-        validateVocabularies(service);
-        ServiceValidators.validateName(service);
-        ServiceValidators.validateURL(service);
-        ServiceValidators.validateDescription(service);
-        ServiceValidators.validateSymbol(service);
-        ServiceValidators.validateVersion(service);
-        ServiceValidators.validateLastUpdate(service);
-        ServiceValidators.validateOrder(service);
-        ServiceValidators.validateSLA(service);
-        ServiceValidators.validateMaxLength(service);
-        validateProviders(service);
+        logger.debug("Validating Service with id: " + service.getId());
+        serviceValidators.validateServices(service);
+        serviceValidators.validateVocabularies(service);
+        serviceValidators.validateName(service);
+        serviceValidators.validateURL(service);
+        serviceValidators.validateDescription(service);
+        serviceValidators.validateSymbol(service);
+        serviceValidators.validateVersion(service);
+        serviceValidators.validateLastUpdate(service);
+        serviceValidators.validateOrder(service);
+        serviceValidators.validateSLA(service);
+        serviceValidators.validateMaxLength(service);
+        serviceValidators.validateProviders(service);
         return true;
     }
 
@@ -226,133 +228,5 @@ public class InfraServiceManager extends ServiceResourceManager implements Infra
         ret.setModifiedBy(registeredBy);
         ret.setModifiedAt(ret.getRegisteredAt());
         return ret;
-    }
-
-
-    private void validateVocabularies(InfraService service) {
-
-        //Validate Categories/Subcategories
-        if (service.getCategory() != null) {
-            Vocabulary categories = vocabularyManager.get("categories");
-            VocabularyEntry category = categories.getEntries().get(service.getCategory());
-            if (category == null)
-                throw new ValidationException(String.format("category '%s' does not exist.", service.getCategory()));
-            List<VocabularyEntry> subcategory = category.getChildren();
-            if (service.getSubcategory() == null) {
-                throw new ValidationException("Field 'subcategory' is mandatory.");
-            }
-            boolean flag = false;
-            for (VocabularyEntry aSubcategory : subcategory) {
-                if (aSubcategory.getId().equals(service.getSubcategory())) {
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag) {
-                throw new ValidationException(String.format("subcategory '%s' does not exist.", service.getSubcategory()));
-            }
-        } else throw new ValidationException("Field 'category' is mandatory.");
-
-        //Validate Places
-        if (service.getPlaces() != null && !service.getPlaces().isEmpty()) {
-            Map<String, VocabularyEntry> places = vocabularyManager.get("places").getEntries();
-            List<String> servicePlaces = service.getPlaces();
-            List<String> notFoundPlaces = new ArrayList<>();
-            List<String> foundPlaces = new ArrayList<>();
-            for (String place : servicePlaces) {
-                VocabularyEntry placeFound = places.get(place);
-                if (placeFound == null) {
-                    notFoundPlaces.add(place);
-                }
-                if (places.containsKey(place) && !foundPlaces.contains(place)){
-                    foundPlaces.add(place);
-                }
-            }
-            if (!notFoundPlaces.isEmpty()) {
-                throw new ValidationException(String.format("Places not found: %s", String.join(", ", notFoundPlaces)));
-            }
-            service.setPlaces(foundPlaces);
-        } else throw new ValidationException("Field 'places' is mandatory.");
-
-        //Validate Languages
-        if (service.getLanguages() != null && !service.getLanguages().isEmpty()) {
-            Map<String, VocabularyEntry> languages = vocabularyManager.get("languages").getEntries();
-            List<String> serviceLanguages = service.getLanguages();
-            List<String> notFoundLanguages = new ArrayList<>();
-            List<String> foundLanguages = new ArrayList<>();
-            for (String language : serviceLanguages) {
-                VocabularyEntry languageFound = languages.get(language);
-                if (languageFound == null) {
-                    notFoundLanguages.add(language);
-                }
-                if (languages.containsKey(language) && !foundLanguages.contains(language)){
-                    foundLanguages.add(language);
-                }
-            }
-            if (!notFoundLanguages.isEmpty()) {
-                throw new ValidationException(String.format("Languages not found: %s", String.join(", ", notFoundLanguages)));
-            }
-            service.setLanguages(foundLanguages);
-        } else throw new ValidationException("Field 'languages' is mandatory.");
-
-        //Validate LifeCycleStatus
-        if (service.getLifeCycleStatus() != null) {
-            Vocabulary lifecyclestatus = vocabularyManager.get("lifecyclestatus");
-            VocabularyEntry lfc = lifecyclestatus.getEntries().get(service.getLifeCycleStatus());
-            if (lfc == null)
-                throw new ValidationException(String.format("LifeCycleStatus '%s' does not exist.", service.getLifeCycleStatus()));
-        } else throw new ValidationException("Field 'lifeCycleStatus' is mandatory.");
-
-        //Validate TRL
-        if (service.getTrl() != null) {
-            Vocabulary trl = vocabularyManager.get("trl");
-            VocabularyEntry trlEntry = trl.getEntries().get(service.getTrl());
-            if (trlEntry == null)
-                throw new ValidationException(String.format("TRL '%s' does not exist.", service.getTrl()));
-        } else throw new ValidationException("Field 'trl' is mandatory.");
-    }
-
-    //validates the correctness of Providers.
-    private void validateProviders(InfraService service) {
-        List<String> providers = service.getProviders();
-        List<String> validProviders = new ArrayList<>();
-        if ((providers == null) || CollectionUtils.isEmpty(service.getProviders()) ||
-                (service.getProviders().stream().filter(Objects::nonNull).mapToInt(p -> 1).sum() == 0)) {
-            throw new ValidationException("field 'providers' is obligatory");
-        }
-        if (service.getProviders().stream().filter(Objects::nonNull).anyMatch(x -> providerManager.getResource(x) == null)) {
-            throw new ValidationException("Provider does not exist");
-        }
-        for (String provider: providers){
-            if (!validProviders.contains(provider)){
-                validProviders.add(provider);
-            }
-        }
-        service.setProviders(validProviders);
-    }
-
-    //validates the correctness of Related and Required Services.
-    public void validateServices(InfraService service) {
-        List<String> relatedServices = service.getRelatedServices();
-        List<String> existingRelatedServices = new ArrayList<>();
-        if (relatedServices != null) {
-            for (String serviceRel : relatedServices) {
-                if (this.exists(new SearchService.KeyValue("infra_service_id", serviceRel)) && !existingRelatedServices.contains(serviceRel)) {
-                    existingRelatedServices.add(serviceRel);
-                }
-            }
-            service.setRelatedServices(existingRelatedServices);
-        }
-
-        List<String> requiredServices = service.getRequiredServices();
-        List<String> existingRequiredServices = new ArrayList<>();
-        if (requiredServices != null) {
-            for (String serviceReq : requiredServices) {
-                if (this.exists(new SearchService.KeyValue("infra_service_id", serviceReq)) && !existingRequiredServices.contains(serviceReq)) {
-                    existingRequiredServices.add(serviceReq);
-                }
-            }
-            service.setRequiredServices(existingRequiredServices);
-        }
     }
 }
