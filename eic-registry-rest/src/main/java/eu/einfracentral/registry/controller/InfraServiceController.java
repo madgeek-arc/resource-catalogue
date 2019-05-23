b2a0341fe28f49ce14bf78d83ab943bef5647c28
Konstantinos Spyrou <spyroukon@gmail.com>
2019-05-23 12:41:49 +0300

    minor fix in service controllers'

--- eic-registry-rest/src/main/java/eu/einfracentral/registry/controller/InfraServiceController.java
+++ eic-registry-rest/src/main/java/eu/einfracentral/registry/controller/InfraServiceController.java
@@ -18,12 +18,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("infraService")
@@ -126,12 +126,8 @@ public class InfraServiceController {
             @ApiImplicitParam(name = "orderField", value = "Order field", dataType = "string", paramType = "query")
     })
     @RequestMapping(path = "all", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public ResponseEntity<Paging<InfraService>> getAll(@ApiIgnore @RequestParam Map<String, Object> allRequestParams, @ApiIgnore Authentication authentication) {
-        FacetFilter ff = new FacetFilter();
-        ff.setKeyword(allRequestParams.get("query") != null ? (String) allRequestParams.remove("query") : "");
-        ff.setFrom(allRequestParams.get("from") != null ? Integer.parseInt((String) allRequestParams.remove("from")) : 0);
-        ff.setQuantity(allRequestParams.get("quantity") != null ? Integer.parseInt((String) allRequestParams.remove("quantity")) : 10);
-        ff.setFilter(allRequestParams);
+    public ResponseEntity<Paging<InfraService>> getAll(@ApiIgnore @RequestParam MultiValueMap<String, Object> allRequestParams, @ApiIgnore Authentication authentication) {
+        FacetFilter ff = createMultiFacetFilter(allRequestParams);
         return ResponseEntity.ok(infraService.getAll(ff, authentication));
     }
 
@@ -160,6 +156,40 @@ public class InfraServiceController {
             logger.info("User " + auth.getName() + " set InfraService " + service.getName() + " with id: " + service.getId() + " to inactive");
         }
         return ResponseEntity.ok(infraService.update(service, auth));
+    }
+
+    private FacetFilter createMultiFacetFilter(MultiValueMap<String, Object> allRequestParams) {
+        logger.debug("Request params: " + allRequestParams);
+        FacetFilter facetFilter = new FacetFilter();
+        facetFilter.setKeyword(allRequestParams.get("query") != null ? (String) allRequestParams.remove("query").get(0) : "");
+        facetFilter.setFrom(allRequestParams.get("from") != null ? Integer.parseInt((String) allRequestParams.remove("from").get(0)) : 0);
+        facetFilter.setQuantity(allRequestParams.get("quantity") != null ? Integer.parseInt((String) allRequestParams.remove("quantity").get(0)) : 10);
+        Map<String, Object> sort = new HashMap<>();
+        Map<String, Object> order = new HashMap<>();
+        String orderDirection = allRequestParams.get("order") != null ? (String) allRequestParams.remove("order").get(0) : "asc";
+        String orderField = allRequestParams.get("orderField") != null ? (String) allRequestParams.remove("orderField").get(0) : null;
+        if (orderField != null) {
+            order.put("order", orderDirection);
+            sort.put(orderField, order);
+            facetFilter.setOrderBy(sort);
+        }
+        if (!allRequestParams.isEmpty()) {
+            Set<Map.Entry<String, List<Object>>> filterSet = allRequestParams.entrySet();
+            for (Map.Entry<String, List<Object>> entry : filterSet) {
+                // split values separated by comma to entries and replace existing <key,value> pair with the new one
+                allRequestParams.replace(entry.getKey(), new LinkedList<>(
+                        entry.getValue()
+                                .stream()
+                                .flatMap(e -> Arrays.stream(e.toString().split(",")))
+                                .distinct()
+                                .collect(Collectors.toList()))
+                );
+            }
+            Map<String, Object> multiFilter = new HashMap<>();
+            multiFilter.put("multi-filter", allRequestParams);
+            facetFilter.setFilter(multiFilter);
+        }
+        return facetFilter;
     }
 
 }
