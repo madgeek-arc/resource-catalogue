03a8120784add271ad154cafb9231fa9ab4e51e4
Konstantinos Spyrou <spyroukon@gmail.com>
2019-06-10 13:02:38 +0300

    added Authorization header when requesting analytics from matomo

--- eic-registry/src/main/java/eu/einfracentral/service/AnalyticsService.java
+++ eic-registry/src/main/java/eu/einfracentral/service/AnalyticsService.java
@@ -7,13 +7,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -28,6 +29,8 @@ public class AnalyticsService {
     private static final String serviceVisitsTemplate = "%s/index.php?token_auth=%s&module=API&method=Actions.getPageUrls&format=JSON&idSite=%s&flat=1&period=range&date=2017-01-01,%s";
     private String visits;
     private String serviceVisits;
+    private RestTemplate restTemplate;
+    private HttpHeaders headers;
 
     @Value("${matomoHost:localhost}")
     private String matomoHost;
@@ -38,15 +41,21 @@ public class AnalyticsService {
     @Value("${matomoSiteId:1}")
     private String matomoSiteId;
 
+    @Value("${matomoAuthorizationHeader:}")
+    private String authorizationHeader;
+
     @PostConstruct
     void postConstruct() {
-        visits = String.format(visitsTemplate, matomoHost, matomoToken, matomoSiteId, "%s", "%s", "%s");
-        serviceVisits = String.format(serviceVisitsTemplate, matomoHost, matomoToken, matomoSiteId, "%s", "%s", "%s");
+        restTemplate = new RestTemplate();
+        headers = new HttpHeaders();
+        headers.add("Authorization", authorizationHeader);
+        visits = String.format(visitsTemplate, matomoHost, matomoToken, matomoSiteId, "%s");
+        serviceVisits = String.format(serviceVisitsTemplate, matomoHost, matomoToken, matomoSiteId, "%s");
     }
 
     public Map<String, Integer> getAllServiceVisits() {
         String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-        JsonNode json = parse(getURL(String.format(serviceVisits, date)));
+        JsonNode json = parse(getMatomoResponse(String.format(serviceVisits, date)));
         if (json != null) {
             try {
                 Spliterators.spliteratorUnknownSize(json.iterator(), Spliterator.NONNULL);
@@ -58,7 +67,7 @@ public class AnalyticsService {
                     }
                 }
                 return results;
-            } catch (Exception e){
+            } catch (Exception e) {
                 logger.error("Cannot find visits for the specific Service.", e);
             }
         }
@@ -76,14 +85,14 @@ public class AnalyticsService {
             );
             Map<String, Integer> sortedResults = new TreeMap<>(results);
             return sortedResults;
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.debug("Cannot find visits for the specific Service.", e);
         }
         return new HashMap<>();
     }
 
     private JsonNode getAnalyticsForLabel(String label, StatisticsService.Interval by) {
-        return parse(getURL(String.format(visits, by.getKey()) +"&label=" + label));
+        return parse(getMatomoResponse(String.format(visits, by.getKey()) + "&label=" + label));
     }
 
     private static JsonNode parse(String json) {
@@ -95,16 +104,13 @@ public class AnalyticsService {
         return null;
     }
 
-    private static String getURL(String url) {
-        StringBuilder ret = new StringBuilder();
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(new URL(url).openStream()))) {
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                ret.append(inputLine).append("\n");
-            }
-        } catch (IOException e) {
-            logger.error("ERROR", e);
+    private String getMatomoResponse(String url) {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class, headers);
+        if (responseEntity.getStatusCode() != HttpStatus.OK) {
+            logger.error(String.format("Could not get analytics from matomo%nResponse Code: %s%nResponse Body: %s",
+                    responseEntity.getStatusCode().toString(), responseEntity.getBody()));
+            throw new RuntimeException(responseEntity.getBody());
         }
-        return ret.toString();
+        return responseEntity.getBody();
     }
 }
