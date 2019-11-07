dbf7fbede1e32c70ab31131bf9789577ae84acf6
MIke <michael.zouros@outlook.com>
2019-11-07 17:13:51 +0200

    Updated Jtest for Provider & Service manipulation

--- eic-registry/src/test/java/eu/einfracentral/registry/manager/ServiceProviderRegistrationIT.java
+++ eic-registry/src/test/java/eu/einfracentral/registry/manager/ServiceProviderRegistrationIT.java
@@ -1,20 +1,22 @@
 package eu.einfracentral.registry.manager;
 
 import eu.einfracentral.config.ServiceConfig;
-import eu.einfracentral.domain.InfraService;
-import eu.einfracentral.domain.Provider;
-import eu.einfracentral.domain.Service;
+import eu.einfracentral.config.security.EICAuthoritiesMapper;
+import eu.einfracentral.domain.*;
 import eu.einfracentral.exception.ResourceException;
 import eu.einfracentral.registry.service.InfraServiceService;
 import eu.einfracentral.registry.service.ProviderService;
 import eu.einfracentral.registry.service.VocabularyService;
 import eu.einfracentral.service.SecurityService;
+import eu.openminted.registry.core.configuration.ElasticConfiguration;
 import eu.openminted.registry.core.exception.ResourceNotFoundException;
 import eu.openminted.registry.core.service.ServiceException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.elasticsearch.client.ElasticsearchClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ActiveProfiles;
@@ -22,12 +24,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.GregorianCalendar;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ServiceConfig.class})
@@ -51,6 +52,9 @@ public class ServiceProviderRegistrationIT {
     @Autowired
     SecurityService securityService;
 
+    @Autowired
+    ElasticConfiguration elastic;
+
     @Test
     public void addInvalidProviderTest() throws MalformedURLException {
         Provider provider = null;
@@ -65,7 +69,7 @@ public class ServiceProviderRegistrationIT {
 
     @Test
     public void addUpdateAndDeleteProvider() throws ResourceNotFoundException, MalformedURLException {
-        String providerId = "test";
+        String providerId = "wp6";
         Provider provider;
         InfraService infraService;
 
@@ -78,7 +82,7 @@ public class ServiceProviderRegistrationIT {
 
             providerService.verifyProvider(providerId, Provider.States.ST_SUBMISSION, true, securityService.getAdminAccess());
 
-            infraService = new InfraService(createService("TestService", provider));
+            infraService = new InfraService(createService("WP4_TestService", provider));
 
             infraService = infraServiceService.addService(infraService, securityService.getAdminAccess());
 
@@ -92,7 +96,7 @@ public class ServiceProviderRegistrationIT {
             providerService.verifyProvider(providerId, Provider.States.APPROVED, false, securityService.getAdminAccess());
             providerService.verifyProvider(providerId, Provider.States.REJECTED, false, securityService.getAdminAccess());
 
-        } catch (ResourceException e) {
+        } catch (RuntimeException e) {
             logger.error("ERROR", e);
         } finally {
             provider = providerService.get(providerId, securityService.getAdminAccess());
@@ -103,10 +107,58 @@ public class ServiceProviderRegistrationIT {
     }
 
     private Provider addProvider(String id) throws MalformedURLException {
+        List<String> providerTypes = new ArrayList<>();
+        providerTypes.add("provider_type-single_sited");
+        providerTypes.add("provider_type-distributed");
+
+        List<String> providerCategories = new ArrayList<>();
+        providerCategories.add("provider_category-agronomy_forestry_plant_breeding_centres");
+        providerCategories.add("provider_category-animal_facilities");
+
+        ProviderLocation providerLocation = new ProviderLocation();
+        providerLocation.setCity("Athens");
+        providerLocation.setName("ARC");
+        providerLocation.setNumber("6");
+        providerLocation.setPostalCode("12345");
+        providerLocation.setRegion("Attica");
+        providerLocation.setStreet("Epidayrou");
+
+        Contact contact1 = new Contact();
+        contact1.setEmail("contact1@gmail.com");
+        contact1.setFirstName("FirstName1");
+        contact1.setLastName("LastName1");
+        contact1.setTel("0123456789");
+        Contact contact2 = new Contact();
+        contact2.setEmail("contact2@gmail.com");
+        contact2.setFirstName("FirstName1");
+        contact2.setLastName("LastName1");
+        contact2.setTel("9876543210");
+        List<Contact> contacts = new ArrayList<>();
+        contacts.add(contact1);
+        contacts.add(contact2);
+
+        List<User> users = new ArrayList<>();
+        User user = new User();
+        user.setEmail("asdf@gmail.com");
+        user.setId("");
+        user.setName("Kostakis");
+        user.setSurname("Spyrakis");
+        users.add(user);
+
         Provider provider = new Provider();
         provider.setId(id);
-        provider.setName("Test");
-        provider.setWebsite(new URL("http://test.t"));
+        provider.setName("WP4_TestProvider");
+        provider.setAcronym("WP4");
+        provider.setWebsite(new URL("http://wp4.testprovider.com"));
+        provider.setDescription("Jtest for PDT WP4 v2.00 01/10/19");
+        provider.setLogo(new URL("https://wp4.testprovider.logo.com"));
+        provider.setTypes(providerTypes);
+        provider.setCategories(providerCategories);
+        provider.setLifeCycleStatus("provider_life_cycle_status-under_construction");
+        provider.setLocation(providerLocation);
+        provider.setCoordinatingCountry("GR");
+        provider.setContacts(contacts);
+        provider.setUsers(users);
 
         return providerService.add(provider, securityService.getAdminAccess());
     }
@@ -116,33 +168,66 @@ public class ServiceProviderRegistrationIT {
         Provider provider = providerService.get(id);
 
         // update provider
+        provider.setName("WP4_Test UPDATED");
+        provider.setAcronym("WP4UPDATED");
+        provider.setWebsite(new URL("http://wp4.test.updated.com"));
+        provider.setDescription("Jtest for PDT WP4 v2.00 01/10/19 UPDATED");
+        provider.setLogo(new URL("https://wp4.testprovider.logo.updated.com"));
+        provider.setLifeCycleStatus("provider_life_cycle_status-being_upgraded");
+        provider.setCoordinatingCountry("EU");
+
         return providerService.update(provider, securityService.getAdminAccess());
     }
 
 
     public Service createService(String serviceName, Provider provider) throws MalformedURLException {
+        List<String> scientificSubdomains = new ArrayList<>();
+        scientificSubdomains.add("scientific_subdomain-natural_sciences-mathematics");
+        scientificSubdomains.add("scientific_subdomain-natural_sciences-computer_sciences");
+
+        List<String> subcategories = new ArrayList<>();
+        subcategories.add("subcategory-access_physical_and_eInfrastructures-instrument_and_equipment-spectrometer");
+        subcategories.add("subcategory-access_physical_and_eInfrastructures-instrument_and_equipment-radiation");
+
+        List<String> targetUsers = new ArrayList<>();
+        targetUsers.add("target_users-researchers");
+        targetUsers.add("target_users-research_groups");
+
+        List<String> languages = new ArrayList<>();
+        languages.add("luba-katanga");
+        languages.add("english");
+
+        List<String> places = new ArrayList<>();
+        places.add("GR");
+        places.add("FR");
+
+        Contact contact1 = new Contact();
+        contact1.setEmail("contact1@gmail.com");
+        contact1.setFirstName("FirstName1");
+        contact1.setLastName("LastName1");
+        contact1.setTel("0123456789");
+        Contact contact2 = new Contact();
+        contact2.setEmail("contact2@gmail.com");
+        contact2.setFirstName("FirstName1");
+        contact2.setLastName("LastName1");
+        contact2.setTel("9876543210");
+        List<Contact> contacts = new ArrayList<>();
+        contacts.add(contact1);
+        contacts.add(contact2);
+
         Service service = new Service();
-
         service.setName(serviceName);
+        service.setUrl(new URL("https:wp4.testservice.com"));
+        service.setDescription("Jtest for SDT WP4 v2.00 01/10/19");
+        service.setLogo(new URL("https:wp4.testservice.logo.com"));
         service.setProviders(Collections.singletonList(provider.getId()));
-        service.setTrl(vocabularyService.get("trl-7").getId());
-        service.setPhase(vocabularyService.get("beta").getId());
-        service.setPlaces(Collections.singletonList(vocabularyService.get("WW").getId()));
-        service.setLanguages(Collections.singletonList(vocabularyService.get("english").getId()));
-//        service.setCategory(vocabularyService.get("other-other").getId());
-
-        service.setUrl(new URL("http://test.t"));
-        service.setLogo(new URL("http://test.t"));
-        service.setOrder(new URL("http://test.t"));
-        service.setSla(new URL("http://test.t"));
-
-        service.setDescription("test service");
-        service.setVersion("v1.0");
-        try {
-            service.setLastUpdate(DatatypeFactory.newInstance().newXMLGregorianCalendar(new GregorianCalendar()));
-        } catch (DatatypeConfigurationException e) {
-            logger.error("ERROR", e);
-        }
+        service.setScientificSubdomains(scientificSubdomains);
+        service.setSubcategories(subcategories);
+        service.setTargetUsers(targetUsers);
+        service.setLanguages(languages);
+        service.setPlaces(places);
+        service.setOrderType("order_type-order_required");
+        service.setContacts(contacts);
 
         return service;
     }
