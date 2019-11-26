47e18bfb3cfe80d6296aed1d17c48a4889460f08
Konstantinos Spyrou <spyroukon@gmail.com>
2019-11-26 15:16:12 +0200

    created add method in PendingServiceManager

--- eic-registry-rest/src/main/java/eu/einfracentral/registry/controller/PendingServiceController.java
+++ eic-registry-rest/src/main/java/eu/einfracentral/registry/controller/PendingServiceController.java
@@ -1,19 +1,29 @@
 package eu.einfracentral.registry.controller;
 
 import eu.einfracentral.domain.InfraService;
+import eu.einfracentral.domain.Service;
 import eu.einfracentral.registry.service.ResourceService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("pendingService")
 public class PendingServiceController extends ResourceController<InfraService, Authentication> {
 
+    public final ResourceService<InfraService, Authentication> pendingServiceManager;
+
     @Autowired
     PendingServiceController(ResourceService<InfraService, Authentication> pendingServiceManager) {
         super(pendingServiceManager);
+        this.pendingServiceManager = pendingServiceManager;
     }
 
 }
--- eic-registry/src/main/java/eu/einfracentral/registry/manager/PendingServiceManager.java
+++ eic-registry/src/main/java/eu/einfracentral/registry/manager/PendingServiceManager.java
@@ -1,7 +1,14 @@
 package eu.einfracentral.registry.manager;
 
 import eu.einfracentral.domain.InfraService;
+import eu.einfracentral.domain.Metadata;
+import eu.einfracentral.domain.User;
+import eu.einfracentral.exception.ResourceNotFoundException;
+import eu.einfracentral.registry.service.InfraServiceService;
 import eu.einfracentral.registry.service.ResourceService;
+import eu.openminted.registry.core.domain.Resource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -9,13 +16,60 @@ import org.springframework.stereotype.Service;
 @Service("pendingServiceManager")
 public class PendingServiceManager extends ResourceManager<InfraService> implements ResourceService<InfraService, Authentication> {
 
+    private static final Logger logger = LogManager.getLogger(PendingServiceManager.class);
+
+    private final InfraServiceService<InfraService, InfraService> infraServiceService;
+
     @Autowired
-    public PendingServiceManager() {
+    public PendingServiceManager(InfraServiceService<InfraService, InfraService> infraServiceService) {
         super(InfraService.class);
+        this.infraServiceService = infraServiceService;
     }
 
     @Override
     public String getResourceType() {
         return "pending_service";
     }
+
+    @Override
+    public InfraService add(InfraService service, Authentication auth) {
+
+        if (service.getMetadata() == null) {
+            service.setMetadata(Metadata.createMetadata(new User(auth).getFullName()));
+        }
+        service.setActive(true);
+        service.setLatest(true);
+
+        super.add(service, auth);
+
+        return service;
+    }
+
+//    public InfraService update(InfraService service, Authentication auth) {
+//        Resource existing;
+//        try {
+//            existing = infraServiceService.getResource(service.getService().getId(), service.getService().getVersion());
+//            if (existing == null) {
+//                logger.warn("Could not find Service with id '{}' and version '{}'. Attempting search without version",
+//                        service.getService().getId(), service.getService().getVersion());
+//                existing = infraServiceService.getResource(service.getService().getId(), null);
+//                if (existing == null) {
+//                    throw new ResourceNotFoundException("Could not find service with id: " + service.getId());
+//                }
+//            }
+//
+//            existing.setPayload(serialize(service));
+//            existing.setResourceTypeName(resourceType.getName());
+//            existing.setResourceType(null);
+//            resourceService.updateResource(existing);
+//            logger.debug("Moving Service to Pending Service: {}", service);
+//
+//        } catch (RuntimeException e) {
+//            logger.error("Could not change resource type to service: {}", service, e);
+//            return service;
+//        }
+//
+//        return service;
+//    }
+
 }
