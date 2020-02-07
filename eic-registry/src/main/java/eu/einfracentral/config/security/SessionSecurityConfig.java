56371dd8666994e9408c39154639daa74f2fe2f7
Konstantinos Spyrou <spyroukon@gmail.com>
2020-02-07 15:02:49 +0200

    removed authentication from /resourcesync/ controller

--- eic-registry/src/main/java/eu/einfracentral/config/security/SessionSecurityConfig.java
+++ eic-registry/src/main/java/eu/einfracentral/config/security/SessionSecurityConfig.java
@@ -113,6 +113,7 @@ public class SessionSecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilterBefore(openIdConnectAuthenticationFilter(),
                         AbstractPreAuthenticatedProcessingFilter.class)
                 .authorizeRequests()
+                .antMatchers("/resourcesync/").permitAll()
                 .regexMatchers("/restore/", "/resource.*", "/resourceType.*", "/search.*")
                 .hasAnyRole("ADMIN")
                 .and()
