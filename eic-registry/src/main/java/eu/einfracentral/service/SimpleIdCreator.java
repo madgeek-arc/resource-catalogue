accf6d770839b9215dee8e9a6f4ad46623df2d21
MIke <michael.zouros@outlook.com>
2020-02-14 11:43:17 +0200

    Added validation for creating the id of a Pending Service

--- eic-registry/src/main/java/eu/einfracentral/service/SimpleIdCreator.java
+++ eic-registry/src/main/java/eu/einfracentral/service/SimpleIdCreator.java
@@ -3,6 +3,7 @@ package eu.einfracentral.service;
 import eu.einfracentral.domain.InfraService;
 import eu.einfracentral.domain.Provider;
 import eu.einfracentral.domain.ProviderBundle;
+import eu.einfracentral.exception.ValidationException;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
@@ -36,11 +37,17 @@ public class SimpleIdCreator implements IdCreator {
 
     @Override
     public String createServiceId(eu.einfracentral.domain.Service service) {
-            String provider = service.getProviders().get(0);
-            return String.format("%s.%s", provider, StringUtils
-                    .stripAccents(service.getName())
-                    .replaceAll("[^a-zA-Z0-9\\s\\-\\_]+", "")
-                    .replace(" ", "_")
-                    .toLowerCase());
+        if (service.getProviders() == null || service.getProviders().isEmpty() || service.getProviders().get(0).equals("")) {
+            throw new ValidationException("Service must have at least 1 Provider.");
+        }
+        if (service.getName() == null || service.getName().equals("")){
+            throw new ValidationException("Service must have a Name.");
+        }
+        String provider = service.getProviders().get(0);
+        return String.format("%s.%s", provider, StringUtils
+                .stripAccents(service.getName())
+                .replaceAll("[^a-zA-Z0-9\\s\\-\\_]+", "")
+                .replace(" ", "_")
+                .toLowerCase());
     }
 }
