e1a79424388377b60f50beb85aed7784ae474762
Konstantinos Spyrou <spyroukon@gmail.com>
2020-06-02 16:52:53 +0300

    Changed ExceptionHandlers response to ServerError object

--- eic-registry-rest/src/main/java/eu/einfracentral/registry/controller/RestResponseEntityExceptionHandler.java
+++ eic-registry-rest/src/main/java/eu/einfracentral/registry/controller/RestResponseEntityExceptionHandler.java
@@ -3,6 +3,7 @@ package eu.einfracentral.registry.controller;
 import eu.einfracentral.exception.ResourceException;
 import eu.einfracentral.exception.ResourceNotFoundException;
 import eu.einfracentral.exception.ValidationException;
+import eu.openminted.registry.core.exception.ServerError;
 import eu.openminted.registry.core.service.ServiceException;
 import javassist.NotFoundException;
 import org.apache.logging.log4j.LogManager;
@@ -18,7 +19,7 @@ import org.springframework.security.oauth2.common.exceptions.UnauthorizedClientE
 import org.springframework.security.oauth2.common.exceptions.UnauthorizedUserException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -31,63 +32,70 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     private static final Logger logger = LogManager.getLogger(RestResponseEntityExceptionHandler.class);
 
     @ExceptionHandler(value = {IllegalStateException.class, IllegalArgumentException.class})
-    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+    protected ResponseEntity<Object> handleConflict(Exception ex, WebRequest request) {
         logger.error("", ex);
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), HttpStatus.CONFLICT, request);
+        String url = ((ServletWebRequest) request).getRequest().getRequestURL().toString();
+        ServerError se = new ServerError(url, ex);
+        return handleExceptionInternal(ex, se, new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 
     @ExceptionHandler(value = {ResourceNotFoundException.class, NotFoundException.class,
             ChangeSetPersister.NotFoundException.class})
-    protected ResponseEntity<Object> handleNotFound(RuntimeException ex, WebRequest request) {
+    protected ResponseEntity<Object> handleNotFound(Exception ex, WebRequest request) {
         logger.warn("", ex);
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        String url = ((ServletWebRequest) request).getRequest().getRequestURL().toString();
+        ServerError se = new ServerError(url, ex);
+        return handleExceptionInternal(ex, se, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(value = {AuthenticationException.class, UnauthorizedClientException.class,
             UnauthorizedUserException.class})
-    protected ResponseEntity<Object> handleUnauthorized(RuntimeException ex, WebRequest request) {
+    protected ResponseEntity<Object> handleUnauthorized(Exception ex, WebRequest request) {
         logger.warn("", ex);
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
+        String url = ((ServletWebRequest) request).getRequest().getRequestURL().toString();
+        ServerError se = new ServerError(url, ex);
+        return handleExceptionInternal(ex, se, new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
     }
 
     @ExceptionHandler(value = {AccessDeniedException.class})
-    protected ResponseEntity<Object> handleForbidden(RuntimeException ex, WebRequest request) {
+    protected ResponseEntity<Object> handleForbidden(Exception ex, WebRequest request) {
         logger.error("", ex);
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), HttpStatus.FORBIDDEN, request);
+        String url = ((ServletWebRequest) request).getRequest().getRequestURL().toString();
+        ServerError se = new ServerError(url, ex);
+        return handleExceptionInternal(ex, se, new HttpHeaders(), HttpStatus.FORBIDDEN, request);
     }
 
-    @ResponseBody
     @ExceptionHandler(value = {ResourceException.class})
     protected ResponseEntity<Object> handleResourceException(ResourceException ex, WebRequest request) {
         logger.error("", ex);
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), ex.getStatus(), request);
+        String url = ((ServletWebRequest) request).getRequest().getRequestURL().toString();
+        ServerError se = new ServerError(url, ex);
+        return handleExceptionInternal(ex, se, new HttpHeaders(), ex.getStatus(), request);
     }
 
     @ExceptionHandler(value = {ValidationException.class})
     protected ResponseEntity<Object> handleValidationException(ValidationException ex, WebRequest request) {
         logger.error("", ex);
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), ex.getStatus(), request);
+        String url = ((ServletWebRequest) request).getRequest().getRequestURL().toString();
+        ServerError se = new ServerError(url, ex);
+        return handleExceptionInternal(ex, se, new HttpHeaders(), ex.getStatus(), request);
     }
 
     @ExceptionHandler(value = {ServiceException.class})
-    protected ResponseEntity<Object> handleServiceException(RuntimeException ex, WebRequest request) {
+    protected ResponseEntity<Object> handleServiceException(Exception ex, WebRequest request) {
         logger.error("", ex);
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+        String url = ((ServletWebRequest) request).getRequest().getRequestURL().toString();
+        ServerError se = new ServerError(url, ex);
+        return handleExceptionInternal(ex, se, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 
     @ExceptionHandler(value = {RuntimeException.class})
-    protected ResponseEntity<Object> handleRuntimeException(RuntimeException ex, WebRequest request) {
+    protected ResponseEntity<Object> handleRuntimeException(Exception ex, WebRequest request) {
         String errorId = UUID.randomUUID().toString();
-        String response = String.format("Please send the following error code to the system administrators. Error Code: '%s'", errorId);
+        String response = String.format("Please send the following error code to the system administrators. Error Code: %s", errorId);
         logger.error("Error Code: {}", errorId, ex);
-        return handleExceptionInternal(ex, response,
-                new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+        String url = ((ServletWebRequest) request).getRequest().getRequestURL().toString();
+        ServerError se = new ServerError(url, new RuntimeException(response));
+        return handleExceptionInternal(ex, se, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 }
