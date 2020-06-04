11088e34a2fa18737c752a5f98a43aa47e38f15a
MIke <michael.zouros@outlook.com>
2020-06-04 15:31:21 +0300

    Commented out Service's facets order - to be rearranged

--- eic-registry/src/main/java/eu/einfracentral/registry/manager/AbstractServiceManager.java
+++ eic-registry/src/main/java/eu/einfracentral/registry/manager/AbstractServiceManager.java
@@ -129,24 +129,24 @@ public abstract class AbstractServiceManager extends AbstractGenericService<Infr
 
     @Override
     public Browsing<InfraService> getAll(FacetFilter filter, Authentication auth) {
-        List<String> orderedBrowseBy = new ArrayList<>();
-
+        //TODO: Rearrange depending on front-end's needs
         //Order Service's facets as we like (+removed Service Name - no4)
-        orderedBrowseBy.add(browseBy.get(11));   //no11 - Subcategories
-        orderedBrowseBy.add(browseBy.get(8));    // no8 - Providers
-        orderedBrowseBy.add(browseBy.get(10));   //no10 - Scientific Subdomains
-        orderedBrowseBy.add(browseBy.get(6));    // no6 - Phase
-        orderedBrowseBy.add(browseBy.get(13));   //no13 - TRL
-        orderedBrowseBy.add(browseBy.get(7));    // no7 - Places
-        orderedBrowseBy.add(browseBy.get(3));    // no3 - Languages
-        orderedBrowseBy.add(browseBy.get(1));    // no1 - Access Types
-        orderedBrowseBy.add(browseBy.get(0));    // no0 - Access Modes
-        orderedBrowseBy.add(browseBy.get(5));    // no5 - Order Type
-        orderedBrowseBy.add(browseBy.get(12));   //no12 - Target Users
-        orderedBrowseBy.add(browseBy.get(2));    // no2 - Funders
-        orderedBrowseBy.add(browseBy.get(9));    // no9 - Resource Type
-
-        filter.setBrowseBy(orderedBrowseBy);
+//        List<String> orderedBrowseBy = new ArrayList<>();
+//        orderedBrowseBy.add(browseBy.get(11));   //no11 - Subcategories
+//        orderedBrowseBy.add(browseBy.get(8));    // no8 - Providers
+//        orderedBrowseBy.add(browseBy.get(10));   //no10 - Scientific Subdomains
+//        orderedBrowseBy.add(browseBy.get(6));    // no6 - Phase
+//        orderedBrowseBy.add(browseBy.get(13));   //no13 - TRL
+//        orderedBrowseBy.add(browseBy.get(7));    // no7 - Places
+//        orderedBrowseBy.add(browseBy.get(3));    // no3 - Languages
+//        orderedBrowseBy.add(browseBy.get(1));    // no1 - Access Types
+//        orderedBrowseBy.add(browseBy.get(0));    // no0 - Access Modes
+//        orderedBrowseBy.add(browseBy.get(5));    // no5 - Order Type
+//        orderedBrowseBy.add(browseBy.get(12));   //no12 - Target Users
+//        orderedBrowseBy.add(browseBy.get(2));    // no2 - Funders
+//        orderedBrowseBy.add(browseBy.get(9));    // no9 - Resource Type
+//
+//        filter.setBrowseBy(orderedBrowseBy);
 
         filter.setResourceType(getResourceType());
         return getMatchingServices(filter);
