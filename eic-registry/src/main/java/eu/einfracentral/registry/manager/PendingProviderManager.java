67222b2a0e62640b464cb4e611f676e73534e70e
MIke <michael.zouros@outlook.com>
2020-07-09 15:27:25 +0300

    Created get method on PendingProviderManager

--- eic-registry/src/main/java/eu/einfracentral/registry/manager/PendingProviderManager.java
+++ eic-registry/src/main/java/eu/einfracentral/registry/manager/PendingProviderManager.java
@@ -16,6 +16,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -48,6 +49,17 @@ public class PendingProviderManager extends ResourceManager<ProviderBundle> impl
     @Override
     public String getResourceType() {
         return "pending_provider";
+    }
+
+    @Override
+    @Cacheable(value = CACHE_PROVIDERS)
+    public ProviderBundle get(String id) {
+        ProviderBundle provider = super.get(id);
+        if (provider == null) {
+            throw new eu.einfracentral.exception.ResourceNotFoundException(
+                    String.format("Could not find pending provider with id: %s", id));
+        }
+        return provider;
     }
 
 
