50b83b18d97bf407cde7857af1ec13fb951e86cc
Mike <michael.zouros@outlook.com>
2021-03-04 14:23:39 +0200

    Created validate method on VocabularyCurationManager

--- eic-registry/src/main/java/eu/einfracentral/registry/manager/VocabularyCurationManager.java
+++ eic-registry/src/main/java/eu/einfracentral/registry/manager/VocabularyCurationManager.java
@@ -2,6 +2,7 @@ package eu.einfracentral.registry.manager;
 
 import eu.einfracentral.domain.Provider;
 import eu.einfracentral.domain.VocabularyCuration;
+import eu.einfracentral.exception.ValidationException;
 import eu.einfracentral.registry.service.VocabularyCurationService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 @Component
@@ -33,10 +35,21 @@ public class VocabularyCurationManager extends ResourceManager<VocabularyCuratio
             vocabularyCuration.setId(UUID.randomUUID().toString());
         }
         vocabularyCuration.setStatus(Provider.States.PENDING_1.getKey());
+        validate(vocabularyCuration);
         // check if parent exists
 
         super.add(vocabularyCuration, auth);
         logger.info("Adding Vocabulary Curation: {}", vocabularyCuration);
+        return vocabularyCuration;
+    }
+
+    @Override
+    public VocabularyCuration validate(VocabularyCuration vocabularyCuration){
+        Object[] possibleValues = Arrays.stream(VocabularyCuration.Vocab.class.getDeclaringClass().getEnumConstants()).toArray();
+        String voc = vocabularyCuration.getVocabulary();
+        if (!Arrays.asList(possibleValues).contains(voc)){
+            throw new ValidationException("Vocabulary " + voc + "' does not exist.");
+        }
         return vocabularyCuration;
     }
 
