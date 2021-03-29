92a24e614c0c74a414bda5881008021cfd051970
jBalasis <cs2180008@di.uoa.gr>
2021-03-29 12:31:02 +0300

    the recommedation api now returns rich services

--- recdb/src/main/java/eu/einfracentral/recdb/controllers/RecommendationController.java
+++ recdb/src/main/java/eu/einfracentral/recdb/controllers/RecommendationController.java
@@ -1,14 +1,19 @@
 package eu.einfracentral.recdb.controllers;
 
+import eu.einfracentral.domain.InfraService;
+import eu.einfracentral.domain.RichService;
 import eu.einfracentral.recdb.services.RecommendationService;
+import eu.einfracentral.registry.service.InfraServiceService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
@@ -21,16 +26,18 @@ import java.util.List;
 public class RecommendationController {
 
     private static final Logger logger = LogManager.getLogger(RecommendationController.class);
+    private final InfraServiceService<InfraService, InfraService> infraService;
     private RecommendationService recommendationService;
 
     @Autowired
-    RecommendationController(RecommendationService recommendationService) {
+    RecommendationController(InfraServiceService<InfraService, InfraService> infraService, RecommendationService recommendationService) {
+        this.infraService = infraService;
         this.recommendationService = recommendationService;
     }
 
-    @ApiOperation(value = "Given a specific user email, returns all Service Recommendations.")
-    @GetMapping(path = "getRecommendationServices", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<String> getRecommendationServices(@ApiIgnore Authentication auth) {
-        return recommendationService.getRecommendationServices(auth);
+    @ApiOperation(value = "Given a specific user email, returns Service Recommendations.")
+    @GetMapping(path = "getRecommendationServices/{limit}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<RichService>> getRecommendationServices(@PathVariable("limit") int limit, @ApiIgnore Authentication auth) {
+        return recommendationService.getRecommendationServices(limit, auth);
     }
 }
--- recdb/src/main/java/eu/einfracentral/recdb/managers/RecommendationManager.java
+++ recdb/src/main/java/eu/einfracentral/recdb/managers/RecommendationManager.java
@@ -1,45 +1,53 @@
 package eu.einfracentral.recdb.managers;
 
+import eu.einfracentral.domain.InfraService;
+import eu.einfracentral.domain.RichService;
 import eu.einfracentral.recdb.services.RecommendationService;
+import eu.einfracentral.registry.service.InfraServiceService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.mitre.openid.connect.model.OIDCAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.security.core.Authentication;
 
 import javax.sql.DataSource;
 import java.util.List;
-import java.util.Objects;
 
 @Component
 public class RecommendationManager implements RecommendationService{
 
     private static final Logger logger = LogManager.getLogger(RecommendationManager.class);
+    private final InfraServiceService<InfraService, InfraService> infraService;
 
     @Autowired(required = true)
     @Qualifier("recdb.datasource")
     private DataSource datasource;
 
     @Autowired
-    public RecommendationManager() {
+    public RecommendationManager(InfraServiceService<InfraService, InfraService> infraService) {
+        this.infraService = infraService;
     }
 
-    public List<String> getRecommendationServices(Authentication authentication){
+    public ResponseEntity<List<RichService>> getRecommendationServices(int limit, Authentication authentication){
         JdbcTemplate jdbcTemplate = new JdbcTemplate(datasource);
-        String finalQuery = "SELECT service_name " +
+
+        /* Get user id */
+        String query = "SELECT user_pk FROM users WHERE user_email = ?;";
+        int user_id = jdbcTemplate.queryForObject(query, new Object[]{ ((OIDCAuthenticationToken) authentication).getUserInfo().getEmail() }, int.class);
+
+        query = "SELECT service_name " +
                 "FROM services " +
                 "WHERE service_pk IN " +
-                "(SELECT service_id FROM view_count R RECOMMEND R.service_id TO R.user_id ON R.visits USING ItemCosCF WHERE R.user_id = ? ORDER BY R.visits LIMIT 3 )";
+                "(SELECT service_id FROM view_count R RECOMMEND R.service_id TO R.user_id ON R.visits USING ItemCosCF WHERE R.user_id = ? ORDER BY R.visits LIMIT ? )";
 
-        List<String> serviceIds = jdbcTemplate.queryForList(finalQuery, new Object[] { ((OIDCAuthenticationToken) authentication).getUserInfo().getEmail() }, java.lang.String.class);
-        for (int i = 0; i < Objects.requireNonNull(serviceIds).size(); i++) {
-            System.out.println(serviceIds.get(i));
+        List<String> serviceIds = jdbcTemplate.queryForList(query, new Object[] {user_id, limit}, java.lang.String.class);
 
-        }
-        return serviceIds;
+        String[] ids = serviceIds.toArray(new String[0]);
+        return ResponseEntity.ok(infraService.getByIds(authentication, ids));
     }
 
 }
