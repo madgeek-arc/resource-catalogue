d2e7441b8a8c3a660cbf4a8e4702d19895a08abf
Konstantinos Spyrou <spyroukon@gmail.com>
2021-04-12 14:46:52 +0300

    fixed getting app properties

--- recdb/src/main/java/eu/einfracentral/recdb/DBConfiguration.java
+++ recdb/src/main/java/eu/einfracentral/recdb/DBConfiguration.java
@@ -4,42 +4,46 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.context.EnvironmentAware;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 @Configuration
-public class DBConfiguration implements EnvironmentAware {
+public class DBConfiguration {
 
-    public Environment environment;
-
-    private static Logger logger = LogManager.getLogger(DBConfiguration.class);
+    private static final Logger logger = LogManager.getLogger(DBConfiguration.class);
 
     @Bean(name = "recdb.datasource")
-    public DataSource dataSource(){
+    public DataSource dataSource() {
         return new HikariDataSource(hikariConfig());
     }
 
-    private HikariConfig hikariConfig(){
-//        logger.info("Connecting to Database @ "+environment.getRequiredProperty("recdb.datasource.url"));
-        logger.info("Connecting to Database @ jdbc:postgresql://0.0.0.0:5432/recdb");
+    @Value("${recdb.datasource.url}")
+    public String recdbUrl;
+
+    @Value("${recdb.datasource.driverClassName}")
+    public String driverClassName;
+
+    @Value("${recdb.datasource.username}")
+    public String recdbName;
+
+    @Value("${recdb.datasource.password}")
+    public String recdbPass;
+
+    private HikariConfig hikariConfig() {
+        logger.info("Connecting to Database @ {}", recdbUrl);
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setPoolName("recdbRegistryCP");
         hikariConfig.setConnectionTestQuery("SELECT 1");
-//        hikariConfig.setDriverClassName(environment.getRequiredProperty("recdb.datasource.driverClassName"));
-        hikariConfig.setDriverClassName("org.postgresql.Driver");
+        hikariConfig.setDriverClassName(driverClassName);
         hikariConfig.setMaximumPoolSize(15);
         hikariConfig.setIdleTimeout(120000);
         hikariConfig.setMinimumIdle(5);
-//        hikariConfig.setJdbcUrl(environment.getRequiredProperty("recdb.datasource.url"));
-        hikariConfig.setJdbcUrl("jdbc:postgresql://0.0.0.0:5432/recdb");
-//        hikariConfig.setUsername(environment.getRequiredProperty("recdb.datasource.username"));
-        hikariConfig.setUsername("postgres");
-//        hikariConfig.setPassword(environment.getRequiredProperty("recdb.datasource.password"));
-        hikariConfig.setPassword("admin");
+        hikariConfig.setJdbcUrl(recdbUrl);
+        hikariConfig.setUsername(recdbName);
+        hikariConfig.setPassword(recdbPass);
         hikariConfig.addDataSourceProperty("cachePreStmts", "true"); // Enable Prepared Statement caching
         hikariConfig.addDataSourceProperty("prepStmtCacheSize", "25"); // How many PS cache, default: 25
         hikariConfig.addDataSourceProperty("useServerPrepStmts", "true"); // If supported use PS server-side
@@ -48,11 +52,5 @@ public class DBConfiguration implements EnvironmentAware {
 
 
         return hikariConfig;
-    }
-
-
-    @Override
-    public void setEnvironment(Environment environment) {
-
     }
 }
