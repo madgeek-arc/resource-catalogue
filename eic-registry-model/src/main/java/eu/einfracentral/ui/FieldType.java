b1f9db1f00bf4a9b41e424a5ee3f7afc4be8bfbe
Konstantinos Spyrou <spyroukon@gmail.com>
2021-05-21 19:07:42 +0300

    changed URL value from 'string' to 'url'

--- eic-registry-model/src/main/java/eu/einfracentral/ui/FieldType.java
+++ eic-registry-model/src/main/java/eu/einfracentral/ui/FieldType.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public enum FieldType {
     STRING                  ("string"),
-    URL                     ("string"),
+    URL                     ("url"),
     INT                     ("int"),
     BOOLEAN                 ("boolean"),
     LIST                    ("array"),
