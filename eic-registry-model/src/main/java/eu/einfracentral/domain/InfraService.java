c8bb694617220334db1a2b8eb1f0aeb7f8d56725
Konstantinos Spyrou <spyroukon@gmail.com>
2021-06-16 15:02:37 +0300

    changed DynamicField fields

--- eic-registry-model/src/main/java/eu/einfracentral/domain/InfraService.java
+++ eic-registry-model/src/main/java/eu/einfracentral/domain/InfraService.java
@@ -1,6 +1,7 @@
 package eu.einfracentral.domain;
 
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import java.util.List;
@@ -12,6 +13,8 @@ public class InfraService extends Bundle<Service> {
     @XmlElement
     private boolean latest;
 
+    @XmlElementWrapper(name = "extras")
+    @XmlElement(name = "extra")
     private List<DynamicField> extras;
 
 
