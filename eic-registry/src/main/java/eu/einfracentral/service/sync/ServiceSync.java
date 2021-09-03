ef2fdc7bbc3300c6d641f8dfb0da66d387c594db
Konstantinos Spyrou <spyroukon@gmail.com>
2021-09-03 13:51:57 +0300

    removed Async from syncAll

--- eic-registry/src/main/java/eu/einfracentral/service/sync/ServiceSync.java
+++ eic-registry/src/main/java/eu/einfracentral/service/sync/ServiceSync.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpServerErrorException;
 
@@ -75,7 +74,6 @@ public class ServiceSync extends AbstractSyncService<InfraService> {
         }
     }
 
-    @Async
     @Override
     public void syncAll() {
         super.syncAll();
