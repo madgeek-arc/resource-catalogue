4f4986e0d2a0364ac0257ef8e33a8b0701380b73
Konstantinos Spyrou <spyroukon@gmail.com>
2021-09-03 13:38:36 +0300

    created syncAll() method

--- eic-registry/src/main/java/eu/einfracentral/service/sync/AbstractSyncService.java
+++ eic-registry/src/main/java/eu/einfracentral/service/sync/AbstractSyncService.java
@@ -1,7 +1,12 @@
 package eu.einfracentral.service.sync;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import eu.einfracentral.domain.Identifiable;
 import eu.einfracentral.service.SynchronizerService;
+import eu.openminted.registry.core.domain.Paging;
+import eu.openminted.registry.core.service.ServiceException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.javatuples.Pair;
@@ -19,6 +24,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -26,11 +34,16 @@ import java.util.concurrent.LinkedBlockingQueue;
 public abstract class AbstractSyncService<T extends Identifiable> implements SynchronizerService<T> {
     private static final Logger logger = LogManager.getLogger(AbstractSyncService.class);
 
+    private final int maxQuantity;
+    private final String filename;
+
+    protected final String host;
+    protected final String self;
+
+    protected Class<T> typeParameterClass;
     protected RestTemplate restTemplate;
     protected boolean active = false;
-    protected String host;
     protected String controller;
-    private String filename;
 
     private BlockingQueue<Pair<T, String>> queue;
 
@@ -39,8 +52,15 @@ public abstract class AbstractSyncService<T extends Identifiable> implements Syn
     public abstract boolean filterOut(T t, T previous);
 
     @Autowired
-    public AbstractSyncService(@Value("${sync.host:}") String host, @Value("${sync.token.filepath:}") String filename) {
+    public AbstractSyncService(@Value("${sync.host:}") String host,
+                               @Value("${swagger.url:}") String self,
+                               @Value("${sync.token.filepath:}") String filename,
+                               @Value("${elastic.index.max_result_window:10000}") int maxQuantity,
+                               Class<T> typeParameterClass) {
+        this.typeParameterClass = typeParameterClass;
+        this.maxQuantity = maxQuantity;
         this.host = host;
+        this.self = self;
         this.filename = filename;
         restTemplate = new RestTemplate();
         if (!"".equals(host)) {
@@ -196,6 +216,84 @@ public abstract class AbstractSyncService<T extends Identifiable> implements Syn
                 }
             }
         }
+    }
+
+    @Override
+    public void syncAll() {
+        while (active) {
+            logger.info("Retrieving resources from remote host: {}", host);
+            Paging<T> remoteResources = getResources(host, maxQuantity);
+            Paging<T> localResources = getResources(self, maxQuantity);
+            if (remoteResources != null && localResources != null) {
+                List<T> missingResources = localResources.getResults();
+                List<T> updatedResources = new ArrayList<>();
+                List<T> deletedResources = remoteResources.getResults();
+
+                Iterator<T> remoteResourceIter = deletedResources.iterator();
+                while (remoteResourceIter.hasNext()) {
+                    T remoteResource = remoteResourceIter.next();
+
+                    Iterator<T> localResourceIter = missingResources.iterator();
+                    while (localResourceIter.hasNext()) {
+                        T localResource = localResourceIter.next();
+
+                        if (localResource.getId().equals(remoteResource.getId())) {
+                            if (!filterOut(localResource, remoteResource)) {
+                                updatedResources.add(localResource);
+                            }
+                            localResourceIter.remove();
+                            remoteResourceIter.remove();
+                            break;
+                        }
+                    }
+                }
+                logger.info("Missing Resources: {}", missingResources.size());
+                logger.info("Updated Resources: {}", updatedResources.size());
+                logger.info("Deleted Resources: {}", deletedResources.size());
+
+                missingResources.forEach(this::syncAdd);
+                updatedResources.forEach(this::syncUpdate);
+                deletedResources.forEach(this::syncDelete);
+            }
+        }
+    }
+
+    private Paging<T> getResources(String host, int quantity) {
+        ResponseEntity<Paging> re = null;
+        Paging<T> paging = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            HttpEntity<Void> request = new HttpEntity<>(createHeaders());
+            // TODO: remove /all if it is removed from controllers
+            URI remoteUri = new URI(host + controller + "/all?quantity=" + quantity).normalize();
+            re = restTemplate.exchange(remoteUri.normalize(), HttpMethod.GET, request, Paging.class);
+            if (re.getStatusCode() != HttpStatus.OK || !re.hasBody()) {
+                logger.error("Retrieving '{}' resources from host '{}' returned code '{}'\nResponse body:\n{}",
+                        typeParameterClass, host, re.getStatusCodeValue(), re.getBody());
+                throw new ServiceException("Could not retrieve resources to sync catalogues");
+            }
+            paging = new Paging<>();
+            paging.setFrom(re.getBody().getFrom());
+            paging.setTo(re.getBody().getTo());
+            paging.setTotal(re.getBody().getTotal());
+            paging.setFacets(re.getBody().getFacets());
+
+            List<T> results = new ArrayList<>();
+            for (Object result : re.getBody().getResults()) {
+                results.add(objectMapper.readValue(new Gson().toJson(result), typeParameterClass));
+            }
+            paging.setResults(results);
+
+        } catch (URISyntaxException e) {
+            logger.error("could not create URI for host: {}", host, e);
+        } catch (NumberFormatException e) {
+            logger.error("quantity given is not an Integer: {}", quantity, e);
+        } catch (JsonProcessingException e) {
+            logger.error(e);
+        } catch (RuntimeException e) {
+            logger.error("syncAll failed, check if token has expired!\n{}", re);
+        }
+        return paging;
     }
 
     protected HttpHeaders createHeaders() {
