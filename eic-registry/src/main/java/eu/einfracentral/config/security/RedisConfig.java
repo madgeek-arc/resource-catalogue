090ce78cea9738f8a877372d8a8a9e8e064f7fcf
Konstantinos Spyrou <spyroukon@gmail.com>
2021-11-02 17:28:17 +0200

    removed commented out code

--- eic-registry/src/main/java/eu/einfracentral/config/security/RedisConfig.java
+++ eic-registry/src/main/java/eu/einfracentral/config/security/RedisConfig.java
@@ -6,15 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.session.SessionRepository;
-import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
-import org.springframework.session.web.http.HeaderHttpSessionStrategy;
-import org.springframework.session.web.http.HttpSessionStrategy;
 
 
 @Configuration
@@ -33,48 +28,22 @@ public class RedisConfig extends RedisHttpSessionConfiguration {
         this.redisPass = password;
     }
 
-//    @Bean
-//    public LettuceConnectionFactory connectionFactory() {
-//        RedisStandaloneConfiguration redisConf = new RedisStandaloneConfiguration(redisHost, Integer.parseInt(redisPort));
-//        if (redisPass != null) redisConf.setPassword(redisPass);
-//        return new LettuceConnectionFactory(redisConf);
-//    }
-
-//    @Bean
-//    public LettuceConnectionFactory connectionFactory() {
-//        LettuceConnectionFactory redisConf = new LettuceConnectionFactory(redisHost, Integer.parseInt(redisPort));
-//        if (redisPass != null) redisConf.setPassword(redisPass);
-//        return redisConf;
-//    }
-
-//    @Bean
-//    public HttpSessionStrategy httpSessionStrategy(){
-//        HeaderHttpSessionStrategy headerHttpSessionStrategy = new HeaderHttpSessionStrategy();
-//        headerHttpSessionStrategy.setHeaderName("EICSESSION");
-//        return headerHttpSessionStrategy;
-//    }
-
-
     @Bean
     public JedisConnectionFactory connectionFactory() {
-        logger.info(String.format("Redis connection Factory created for %s:%s", redisHost, redisPort));
+        logger.info(String.format("Redis connection listens to %s:%s", redisHost, redisPort));
         JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
         jedisConnectionFactory.setHostName(redisHost);
         jedisConnectionFactory.setPort(Integer.parseInt(redisPort));
         jedisConnectionFactory.setUsePool(true);
-        if(redisPass != null) jedisConnectionFactory.setPassword(redisPass);
+        if (redisPass != null) jedisConnectionFactory.setPassword(redisPass);
         return jedisConnectionFactory;
     }
 
-//    @Bean
-//    public CookieSerializer cookieSerializer() {
-//        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
-//        serializer.setCookieName("openAIRESession");
-//        serializer.setCookiePath("/");
-////        if(aai_mode.equalsIgnoreCase("production") || aai_mode.equalsIgnoreCase("beta"))
-////            serializer.setDomainName(".openaire.eu");
-////        serializer.setDomainName(".athenarc.gr");
-//        logger.info("Serializer : " + serializer);
-//        return serializer;
-//    }
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("SESSION");
+        serializer.setCookiePath("/");
+        return serializer;
+    }
 }
