7d7eb3dfe74a0fb408190c300751791d87e79a84
Mike <michael.zouros@outlook.com>
2022-02-02 12:46:28 +0200

    Removed unecessary imports

--- eic-registry/src/main/java/eu/einfracentral/config/CacheConfig.java
+++ eic-registry/src/main/java/eu/einfracentral/config/CacheConfig.java
@@ -1,7 +1,6 @@
 package eu.einfracentral.config;
 
 import com.google.common.cache.CacheBuilder;
-import eu.einfracentral.domain.Provider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.cache.CacheManager;
@@ -10,18 +9,11 @@ import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.*;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
