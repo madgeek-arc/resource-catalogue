771ed2dabe220ab5c8b0bddfe914c1b541562b1a
Mike <michael.zouros@outlook.com>
2022-04-06 12:43:45 +0300

    Provider has now it's own country vocabulary

--- eic-registry-model/src/main/java/eu/einfracentral/domain/ProviderLocation.java
+++ eic-registry-model/src/main/java/eu/einfracentral/domain/ProviderLocation.java
@@ -52,7 +52,7 @@ public class ProviderLocation {
     @XmlElement(required = true)
     @ApiModelProperty(position = 5, required = true)
     @FieldValidation(containsId = true, idClass = Vocabulary.class)
-    @VocabularyValidation(type = Vocabulary.Type.COUNTRY)
+    @VocabularyValidation(type = Vocabulary.Type.PROVIDER_COUNTRY)
     private String country;
 
     public ProviderLocation() {
