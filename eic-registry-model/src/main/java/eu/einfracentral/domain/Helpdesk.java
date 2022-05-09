9731c91ac6d723abf7885ac49f1b3d00d9495075
Konstantinos Spyrou <spyroukon@gmail.com>
2022-05-09 17:11:36 +0300

    added new classes for service extensions

---
+++ eic-registry-model/src/main/java/eu/einfracentral/domain/Helpdesk.java
@@ -0,0 +1,130 @@
+package eu.einfracentral.domain;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType
+@XmlRootElement(namespace = "http://eosc-portal.eu")
+public class Helpdesk {
+
+    @XmlElement
+    @ApiModelProperty(position = 1, notes = "Service ID")
+    private String service;
+
+    @XmlElement
+    @ApiModelProperty(position = 1, notes = "Service ID")
+    private HelpdeskType type;
+
+    @XmlElement
+    @ApiModelProperty(position = 2, notes = "Support group to be created in the helpdesk for the provider")
+    private String group;
+
+    @XmlElement
+    @ApiModelProperty(position = 3, notes = "Name of organisation")
+    private String organisation;
+
+    @XmlElement
+    @ApiModelProperty(position = 4, notes = "E-mail for direct assignment of the tickets, bypassing the L1 support")
+    private String email;
+
+    @XmlElement
+    @ApiModelProperty(position = 5, notes = "Person involved in ticket management")
+    private String agent;
+
+    @XmlElement
+    @ApiModelProperty(position = 6, notes = "Automatic signature to be used in the answers to the tickets")
+    private String signature;
+
+    @XmlElement
+    @ApiModelProperty(position = 7, notes = "Should the tickets be stored in the helpdesk system in dedicated group")
+    private Boolean ticketPreservation;
+
+    @XmlElement
+    @ApiModelProperty(position = 8, notes = "Webform required to generate ticket directly on webpage")
+    private Boolean webform;
+
+
+    public Helpdesk() {}
+
+    public enum HelpdeskType {
+        FULL_INTEGRATION,
+        TICKET_REDIRECTION,
+        DIRECT_USAGE
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public HelpdeskType getType() {
+        return type;
+    }
+
+    public void setType(HelpdeskType type) {
+        this.type = type;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getOrganisation() {
+        return organisation;
+    }
+
+    public void setOrganisation(String organisation) {
+        this.organisation = organisation;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAgent() {
+        return agent;
+    }
+
+    public void setAgent(String agent) {
+        this.agent = agent;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public Boolean getTicketPreservation() {
+        return ticketPreservation;
+    }
+
+    public void setTicketPreservation(Boolean ticketPreservation) {
+        this.ticketPreservation = ticketPreservation;
+    }
+
+    public Boolean getWebform() {
+        return webform;
+    }
+
+    public void setWebform(Boolean webform) {
+        this.webform = webform;
+    }
+
+}
