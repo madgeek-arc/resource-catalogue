358453f192cc8928846b8594941c14f6c882caa8
mzouros <michael.zouros@outlook.com>
2022-05-16 16:56:30 +0300

    Implemented bundles for Helpdesk and Monitoring

--- eic-registry-api/src/main/java/eu/einfracentral/registry/service/HelpdeskService.java
+++ eic-registry-api/src/main/java/eu/einfracentral/registry/service/HelpdeskService.java
@@ -3,4 +3,7 @@ package eu.einfracentral.registry.service;
 import org.springframework.security.core.Authentication;
 
 public interface HelpdeskService<T, U extends Authentication> extends ResourceService<T, Authentication> {
+
+    @Override
+    T add(T helpdesk, Authentication authentication);
 }
