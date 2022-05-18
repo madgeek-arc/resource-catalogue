0ae73dde6525be81f67b8daead646afae74e9d6c
mzouros <michael.zouros@outlook.com>
2022-05-18 11:52:23 +0300

    Added empty string validation for emails and phones

--- eic-registry/src/main/java/eu/einfracentral/validators/FieldValidator.java
+++ eic-registry/src/main/java/eu/einfracentral/validators/FieldValidator.java
@@ -108,7 +108,7 @@ public class FieldValidator {
     private void validatePhone(Field field, Object o, PhoneValidation annotation) throws IllegalAccessException {
         field.setAccessible(true);
         o = field.get(o);
-        if (annotation.nullable() && o == null) {
+        if (annotation.nullable() && (o == null || o.equals(""))) {
             return;
         } else if (o == null) {
             throw new ValidationException(String.format(MANDATORY_FIELD, getCurrentLocation()));
@@ -122,7 +122,7 @@ public class FieldValidator {
     private void validateEmail(Field field, Object o, EmailValidation annotation) throws IllegalAccessException {
         field.setAccessible(true);
         o = field.get(o);
-        if (annotation.nullable() && o == null) {
+        if (annotation.nullable() && (o == null || o.equals(""))) {
             return;
         } else if (o == null) {
             throw new ValidationException(String.format(MANDATORY_FIELD, getCurrentLocation()));
