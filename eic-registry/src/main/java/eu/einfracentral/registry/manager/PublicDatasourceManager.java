dc9529c763db3e70c88be09522d12810b8a1ba0b
Konstantinos Spyrou <spyroukon@gmail.com>
2022-10-05 12:02:19 +0300

    refactoring

--- eic-registry/src/main/java/eu/einfracentral/registry/manager/PublicDatasourceManager.java
+++ eic-registry/src/main/java/eu/einfracentral/registry/manager/PublicDatasourceManager.java
@@ -2,6 +2,7 @@ package eu.einfracentral.registry.manager;
 
 import eu.einfracentral.domain.DatasourceBundle;
 import eu.einfracentral.domain.Identifiers;
+import eu.einfracentral.domain.ResourceBundle;
 import eu.einfracentral.exception.ResourceException;
 import eu.einfracentral.exception.ResourceNotFoundException;
 import eu.einfracentral.service.SecurityService;
@@ -19,11 +20,13 @@ import org.springframework.stereotype.Service;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-public class PublicDatasourceManager extends ResourceManager<DatasourceBundle> implements ResourceCRUDService<DatasourceBundle, Authentication> {
+public class PublicDatasourceManager extends AbstractPublicResourceManager<DatasourceBundle> implements ResourceCRUDService<DatasourceBundle, Authentication> {
 
     private static final Logger logger = LogManager.getLogger(PublicDatasourceManager.class);
     private final JmsTemplate jmsTopicTemplate;
@@ -68,43 +71,10 @@ public class PublicDatasourceManager extends ResourceManager<DatasourceBundle> i
         String lowerLevelResourceId = datasourceBundle.getId();
         datasourceBundle.setIdentifiers(Identifiers.createIdentifier(datasourceBundle.getId()));
         datasourceBundle.setId(String.format("%s.%s", datasourceBundle.getDatasource().getCatalogueId(), datasourceBundle.getId()));
-        datasourceBundle.getDatasource().setResourceOrganisation(String.format("%s.%s", datasourceBundle.getDatasource().getCatalogueId(), datasourceBundle.getDatasource().getResourceOrganisation()));
-        // Resource Providers
-        List<String> oldResourceProviders = datasourceBundle.getDatasource().getResourceProviders();
-        List<String> newResourceProviders = new ArrayList<>();
-        for (String resourceProvider : oldResourceProviders){
-            if (!resourceProvider.contains(datasourceBundle.getDatasource().getCatalogueId())){
-                resourceProvider = datasourceBundle.getDatasource().getCatalogueId() + "." + resourceProvider;
-            }
-            newResourceProviders.add(resourceProvider);
-        }
-        datasourceBundle.getDatasource().setResourceProviders(newResourceProviders.stream()
-                .distinct() // remove duplicates
-                .collect(Collectors.toList()));
-        // Related Resources
-        List<String> oldRelatedResources = datasourceBundle.getDatasource().getRelatedResources();
-        List<String> newRelatedResources = new ArrayList<>();
-        for (String relatedResource : oldRelatedResources){
-            if (!relatedResource.contains(datasourceBundle.getDatasource().getCatalogueId())){
-                relatedResource = datasourceBundle.getDatasource().getCatalogueId() + "." + relatedResource;
-            }
-            newRelatedResources.add(relatedResource);
-        }
-        datasourceBundle.getDatasource().setRelatedResources(newRelatedResources.stream()
-                .distinct() // remove duplicates
-                .collect(Collectors.toList()));
-        // Required Resources
-        List<String> oldRequiredResources = datasourceBundle.getDatasource().getRequiredResources();
-        List<String> newRequiredResources = new ArrayList<>();
-        for (String requiredResource : oldRequiredResources){
-            if (!requiredResource.contains(datasourceBundle.getDatasource().getCatalogueId())){
-                requiredResource = datasourceBundle.getDatasource().getCatalogueId() + "." + requiredResource;
-            }
-            newRequiredResources.add(requiredResource);
-        }
-        datasourceBundle.getDatasource().setRequiredResources(newRequiredResources.stream()
-                .distinct() // remove duplicates
-                .collect(Collectors.toList()));
+
+        // sets public ids to resource organisation, resource providers and related/required resources
+        updateResourceIdsToPublic(datasourceBundle);
+
         datasourceBundle.getMetadata().setPublished(true);
         DatasourceBundle ret;
         logger.info(String.format("Datasource [%s] is being published with id [%s]", lowerLevelResourceId, datasourceBundle.getId()));
@@ -123,43 +93,10 @@ public class PublicDatasourceManager extends ResourceManager<DatasourceBundle> i
         } catch (IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
-        ret.getDatasource().setResourceOrganisation(ret.getDatasource().getCatalogueId() + "." + ret.getDatasource().getResourceOrganisation());
-        // Resource Providers
-        List<String> oldResourceProviders = datasourceBundle.getDatasource().getResourceProviders();
-        List<String> newResourceProviders = new ArrayList<>();
-        for (String resourceProvider : oldResourceProviders){
-            if (!resourceProvider.contains(datasourceBundle.getDatasource().getCatalogueId())){
-                resourceProvider = datasourceBundle.getDatasource().getCatalogueId() + "." + resourceProvider;
-            }
-            newResourceProviders.add(resourceProvider);
-        }
-        ret.getDatasource().setResourceProviders(newResourceProviders.stream()
-                .distinct() // remove duplicates
-                .collect(Collectors.toList()));
-        // Related Resources
-        List<String> oldRelatedResources = datasourceBundle.getDatasource().getRelatedResources();
-        List<String> newRelatedResources = new ArrayList<>();
-        for (String relatedResource : oldRelatedResources){
-            if (!relatedResource.contains(datasourceBundle.getDatasource().getCatalogueId())){
-                relatedResource = datasourceBundle.getDatasource().getCatalogueId() + "." + relatedResource;
-            }
-            newRelatedResources.add(relatedResource);
-        }
-        ret.getDatasource().setRelatedResources(newRelatedResources.stream()
-                .distinct() // remove duplicates
-                .collect(Collectors.toList()));
-        // Required Resources
-        List<String> oldRequiredResources = datasourceBundle.getDatasource().getRequiredResources();
-        List<String> newRequiredResources = new ArrayList<>();
-        for (String requiredResource : oldRequiredResources){
-            if (!requiredResource.contains(datasourceBundle.getDatasource().getCatalogueId())){
-                requiredResource = datasourceBundle.getDatasource().getCatalogueId() + "." + requiredResource;
-            }
-            newRequiredResources.add(requiredResource);
-        }
-        ret.getDatasource().setRequiredResources(newRequiredResources.stream()
-                .distinct() // remove duplicates
-                .collect(Collectors.toList()));
+
+        // sets public ids to resource organisation, resource providers and related/required resources
+        updateResourceIdsToPublic(ret);
+
         ret.setIdentifiers(published.getIdentifiers());
         ret.setId(published.getId());
         ret.setMetadata(published.getMetadata());
--- eic-registry/src/main/java/eu/einfracentral/registry/manager/PublicServiceManager.java
+++ eic-registry/src/main/java/eu/einfracentral/registry/manager/PublicServiceManager.java
@@ -23,7 +23,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class PublicServiceManager extends ResourceManager<ServiceBundle> implements ResourceCRUDService<ServiceBundle, Authentication> {
+public class PublicServiceManager extends AbstractPublicResourceManager<ServiceBundle> implements ResourceCRUDService<ServiceBundle, Authentication> {
 
     private static final Logger logger = LogManager.getLogger(PublicServiceManager.class);
     private final JmsTemplate jmsTopicTemplate;
@@ -68,43 +68,10 @@ public class PublicServiceManager extends ResourceManager<ServiceBundle> impleme
         String lowerLevelResourceId = serviceBundle.getId();
         serviceBundle.setIdentifiers(Identifiers.createIdentifier(serviceBundle.getId()));
         serviceBundle.setId(String.format("%s.%s", serviceBundle.getService().getCatalogueId(), serviceBundle.getId()));
-        serviceBundle.getService().setResourceOrganisation(String.format("%s.%s", serviceBundle.getService().getCatalogueId(), serviceBundle.getService().getResourceOrganisation()));
-        // Resource Providers
-        List<String> oldResourceProviders = serviceBundle.getService().getResourceProviders();
-        List<String> newResourceProviders = new ArrayList<>();
-        for (String resourceProvider : oldResourceProviders){
-            if (!resourceProvider.contains(serviceBundle.getService().getCatalogueId())){
-                resourceProvider = serviceBundle.getService().getCatalogueId() + "." + resourceProvider;
-            }
-            newResourceProviders.add(resourceProvider);
-        }
-        serviceBundle.getService().setResourceProviders(newResourceProviders.stream()
-                .distinct() // remove duplicates
-                .collect(Collectors.toList()));
-        // Related Resources
-        List<String> oldRelatedResources = serviceBundle.getService().getRelatedResources();
-        List<String> newRelatedResources = new ArrayList<>();
-        for (String relatedResource : oldRelatedResources){
-            if (!relatedResource.contains(serviceBundle.getService().getCatalogueId())){
-                relatedResource = serviceBundle.getService().getCatalogueId() + "." + relatedResource;
-            }
-            newRelatedResources.add(relatedResource);
-        }
-        serviceBundle.getService().setRelatedResources(newRelatedResources.stream()
-                .distinct() // remove duplicates
-                .collect(Collectors.toList()));
-        // Required Resources
-        List<String> oldRequiredResources = serviceBundle.getService().getRequiredResources();
-        List<String> newRequiredResources = new ArrayList<>();
-        for (String requiredResource : oldRequiredResources){
-            if (!requiredResource.contains(serviceBundle.getService().getCatalogueId())){
-                requiredResource = serviceBundle.getService().getCatalogueId() + "." + requiredResource;
-            }
-            newRequiredResources.add(requiredResource);
-        }
-        serviceBundle.getService().setRequiredResources(newRequiredResources.stream()
-                .distinct() // remove duplicates
-                .collect(Collectors.toList()));
+
+        // sets public ids to resource organisation, resource providers and related/required resources
+        updateResourceIdsToPublic(serviceBundle);
+
         serviceBundle.getMetadata().setPublished(true);
         ServiceBundle ret;
         logger.info(String.format("Service [%s] is being published with id [%s]", lowerLevelResourceId, serviceBundle.getId()));
@@ -123,43 +90,10 @@ public class PublicServiceManager extends ResourceManager<ServiceBundle> impleme
         } catch (IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
-        ret.getService().setResourceOrganisation(ret.getService().getCatalogueId() + "." + ret.getService().getResourceOrganisation());
-        // Resource Providers
-        List<String> oldResourceProviders = serviceBundle.getService().getResourceProviders();
-        List<String> newResourceProviders = new ArrayList<>();
-        for (String resourceProvider : oldResourceProviders){
-            if (!resourceProvider.contains(serviceBundle.getService().getCatalogueId())){
-                resourceProvider = serviceBundle.getService().getCatalogueId() + "." + resourceProvider;
-            }
-            newResourceProviders.add(resourceProvider);
-        }
-        ret.getService().setResourceProviders(newResourceProviders.stream()
-                .distinct() // remove duplicates
-                .collect(Collectors.toList()));
-        // Related Resources
-        List<String> oldRelatedResources = serviceBundle.getService().getRelatedResources();
-        List<String> newRelatedResources = new ArrayList<>();
-        for (String relatedResource : oldRelatedResources){
-            if (!relatedResource.contains(serviceBundle.getService().getCatalogueId())){
-                relatedResource = serviceBundle.getService().getCatalogueId() + "." + relatedResource;
-            }
-            newRelatedResources.add(relatedResource);
-        }
-        ret.getService().setRelatedResources(newRelatedResources.stream()
-                .distinct() // remove duplicates
-                .collect(Collectors.toList()));
-        // Required Resources
-        List<String> oldRequiredResources = serviceBundle.getService().getRequiredResources();
-        List<String> newRequiredResources = new ArrayList<>();
-        for (String requiredResource : oldRequiredResources){
-            if (!requiredResource.contains(serviceBundle.getService().getCatalogueId())){
-                requiredResource = serviceBundle.getService().getCatalogueId() + "." + requiredResource;
-            }
-            newRequiredResources.add(requiredResource);
-        }
-        ret.getService().setRequiredResources(newRequiredResources.stream()
-                .distinct() // remove duplicates
-                .collect(Collectors.toList()));
+
+        // sets public ids to resource organisation, resource providers and related/required resources
+        updateResourceIdsToPublic(ret);
+
         ret.setIdentifiers(published.getIdentifiers());
         ret.setId(published.getId());
         ret.setMetadata(published.getMetadata());
