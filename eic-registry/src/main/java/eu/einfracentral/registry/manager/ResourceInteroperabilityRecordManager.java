b69e2c515601aac4a2251fc70f072f2574a1398b
mzouros <michael.zouros@outlook.com>
2023-01-02 12:53:49 +0200

    Optimized validation messages for Monitoring/Helpdesk/RIR (EOSCP-34)

--- eic-registry/src/main/java/eu/einfracentral/registry/manager/ResourceInteroperabilityRecordManager.java
+++ eic-registry/src/main/java/eu/einfracentral/registry/manager/ResourceInteroperabilityRecordManager.java
@@ -54,9 +54,9 @@ public class ResourceInteroperabilityRecordManager extends ResourceManager<Resou
 
         // check if Resource exists and if User belongs to Resource's Provider Admins
         if (resourceType.equals("service")){
-            ResourceValidationUtils.checkIfResourceBundleActiveAndApprovedAndNotPublic(resourceId, catalogueId, serviceBundleService);
+            ResourceValidationUtils.checkIfResourceBundleActiveAndApprovedAndNotPublic(resourceId, catalogueId, serviceBundleService, resourceType);
         } else if (resourceType.equals("datasource")){
-            ResourceValidationUtils.checkIfResourceBundleActiveAndApprovedAndNotPublic(resourceId, catalogueId, datasourceBundleService);
+            ResourceValidationUtils.checkIfResourceBundleActiveAndApprovedAndNotPublic(resourceId, catalogueId, datasourceBundleService, resourceType);
         } else{
             throw new ValidationException("Field resourceType should be either 'service' or 'datasource'");
         }
--- eic-registry/src/main/java/eu/einfracentral/utils/ResourceValidationUtils.java
+++ eic-registry/src/main/java/eu/einfracentral/utils/ResourceValidationUtils.java
@@ -4,24 +4,28 @@ import eu.einfracentral.domain.ResourceBundle;
 import eu.einfracentral.exception.ResourceNotFoundException;
 import eu.einfracentral.exception.ValidationException;
 import eu.einfracentral.registry.service.ResourceBundleService;
+import org.apache.commons.lang3.StringUtils;
 
 public class ResourceValidationUtils {
 
-    public static <T extends ResourceBundle<?>> void checkIfResourceBundleActiveAndApprovedAndNotPublic(String resourceId, String catalogueId, ResourceBundleService<T> resourceBundleService) {
+    public static <T extends ResourceBundle<?>> void checkIfResourceBundleActiveAndApprovedAndNotPublic(String resourceId, String catalogueId,
+                                                                                                        ResourceBundleService<T> resourceBundleService,
+                                                                                                        String resourceType) {
         T resourceBundle;
+        resourceType = StringUtils.capitalize(resourceType);
         // check if Resource exists
         try {
             resourceBundle = resourceBundleService.get(resourceId, catalogueId);
             // check if Service is Public
             if (resourceBundle.getMetadata().isPublished()) {
-                throw new ValidationException("Please provide a Resource ID with no catalogue prefix.");
+                throw new ValidationException(String.format("Please provide a %s ID with no catalogue prefix.", resourceType));
             }
         } catch (ResourceNotFoundException e) {
-            throw new ValidationException(String.format("There is no Resource with id '%s' in the '%s' Catalogue", resourceId, catalogueId));
+            throw new ValidationException(String.format("There is no %s with id '%s' in the '%s' Catalogue", resourceType, resourceId, catalogueId));
         }
         // check if Service is Active + Approved
         if (!resourceBundle.isActive() || !resourceBundle.getStatus().equals("approved resource")) {
-            throw new ValidationException(String.format("Resource with ID [%s] is not Approved and/or Active", resourceId));
+            throw new ValidationException(String.format("%s with ID '%s' is not Approved and/or Active", resourceType, resourceId));
         }
     }
 
