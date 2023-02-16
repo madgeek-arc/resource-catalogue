161edffb545775904bacb7275b97b189936e8a8e
mzouros <michael.zouros@outlook.com>
2023-02-16 16:01:57 +0200

    Added ResourceExtras on TrainingResourceBundle

--- eic-registry-model/src/main/java/eu/einfracentral/domain/TrainingResourceBundle.java
+++ eic-registry-model/src/main/java/eu/einfracentral/domain/TrainingResourceBundle.java
@@ -1,8 +1,11 @@
 package eu.einfracentral.domain;
 
+import eu.einfracentral.annotation.FieldValidation;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
+import java.util.Objects;
 
 //@Document
 @XmlType
@@ -11,6 +14,10 @@ public class TrainingResourceBundle extends Bundle<TrainingResource> {
 
     @XmlElement
     private String status;
+
+    @XmlElement
+    @FieldValidation(nullable = true)
+    private ResourceExtras resourceExtras;
 
     public TrainingResourceBundle() {
         // No arg constructor
@@ -54,9 +61,34 @@ public class TrainingResourceBundle extends Bundle<TrainingResource> {
         super.setId(id);
     }
 
+    public ResourceExtras getResourceExtras() {
+        return resourceExtras;
+    }
+
+    public void setResourceExtras(ResourceExtras resourceExtras) {
+        this.resourceExtras = resourceExtras;
+    }
+
     @Override
     public String toString() {
-        return "TrainingResourceBundle{} " + super.toString();
+        return "TrainingResourceBundle{" +
+                "status='" + status + '\'' +
+                ", resourceExtras=" + resourceExtras +
+                '}' + super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        TrainingResourceBundle that = (TrainingResourceBundle) o;
+        return Objects.equals(status, that.status) && Objects.equals(resourceExtras, that.resourceExtras);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), status, resourceExtras);
     }
 }
 
