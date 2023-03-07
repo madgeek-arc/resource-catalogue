0a3301c4823297e171c8e797c1afe42abc3474c4
Konstantinos Spyrou <spyroukon@gmail.com>
2023-03-07 21:55:44 +0200

    refactoring

--- eic-registry-api/src/main/java/eu/einfracentral/exception/ResourceNotFoundException.java
+++ eic-registry-api/src/main/java/eu/einfracentral/exception/ResourceNotFoundException.java
@@ -1,33 +1,20 @@
 package eu.einfracentral.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-    private HttpStatus status = HttpStatus.NOT_FOUND;
 
-    public ResourceNotFoundException(HttpStatus status) {
-        this("ResourceNotFoundException", status);
+    public ResourceNotFoundException() {
+        super("Resource Not Found");
     }
 
-    public ResourceNotFoundException(String msg) {
-        super(msg);
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
 
-    public ResourceNotFoundException(String msg, HttpStatus status) {
-        super(msg);
-        this.setStatus(status);
-    }
-
-    public ResourceNotFoundException(Exception e, HttpStatus status) {
-        super(e);
-        this.setStatus(status);
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    private void setStatus(HttpStatus status) {
-        this.status = status;
+    public ResourceNotFoundException(String id, String resourceType) {
+        super(String.format("[resourceType=%s] with [id=%s] was not found",resourceType, id));
     }
 }
