e88511a9f89ff71ef66bb40b3712299a2532da43
mzouros <michael.zouros@outlook.com>
2023-09-20 12:32:32 +0300

    Removed defaultContentType on WebMvcConfig

--- eic-registry/src/main/java/eu/einfracentral/config/WebMvcConfig.java
+++ eic-registry/src/main/java/eu/einfracentral/config/WebMvcConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.*;
 
 
@@ -20,11 +19,6 @@ import org.springframework.web.servlet.config.annotation.*;
 @EnableWebMvc
 @EnableAspectJAutoProxy
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.defaultContentType(MediaType.APPLICATION_JSON);
-    }
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
