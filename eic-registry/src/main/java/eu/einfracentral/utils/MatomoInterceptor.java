a1d2c37fbfc249a90b0df1ec1535b5a837d8ec13
Konstantinos Spyrou <spyroukon@gmail.com>
2023-11-24 11:42:31 +0200

    Update MatomoInterceptor.java
    renamed variables from pascal case to camel case

--- eic-registry/src/main/java/eu/einfracentral/utils/MatomoInterceptor.java
+++ eic-registry/src/main/java/eu/einfracentral/utils/MatomoInterceptor.java
@@ -28,12 +28,12 @@ public class MatomoInterceptor implements AsyncHandlerInterceptor {
     @Value("${apitracking.matomo.host:#{null}}")
     private String matomoUrl;
 
-    private MatomoTracker MatomoTracker = null;
+    private MatomoTracker matomoTracker = null;
 
     @PostConstruct
     public void init() {
         if (matomoUrl != null && !"".equals(matomoUrl)) {
-            this.MatomoTracker = new MatomoTracker(TrackerConfiguration.builder().apiEndpoint(URI.create(matomoUrl)).build());
+            this.matomoTracker = new MatomoTracker(TrackerConfiguration.builder().apiEndpoint(URI.create(matomoUrl)).build());
             if (siteId == null) {
                 logger.warn("'apitracking.matomo.site' is undefined. Using default value 1");
                 siteId = 1;
@@ -44,22 +44,22 @@ public class MatomoInterceptor implements AsyncHandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
-        if (MatomoTracker != null) {
+        if (matomoTracker != null) {
 
             if (request.getHeader("Referer") == null) {
                 logger.debug("Referer is null. That probably means that the call did not come from the portal. Logging!");
 
-                MatomoRequest MatomoRequest = ServletMatomoRequest.fromServletRequest(
+                MatomoRequest matomoRequest = ServletMatomoRequest.fromServletRequest(
                         JavaxHttpServletWrapper.fromHttpServletRequest(request))
                         .siteId(siteId)
                         .actionUrl(request.getRequestURL().toString())
                         .build();
 
-                MatomoRequest.setActionName(request.getRequestURI());
-                MatomoRequest.setUserId(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
-                MatomoRequest.setReferrerUrl(null);
+                matomoRequest.setActionName(request.getRequestURI());
+                matomoRequest.setUserId(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
+                matomoRequest.setReferrerUrl(null);
 
-                MatomoTracker.sendRequestAsync(MatomoRequest);
+                matomoTracker.sendRequestAsync(matomoRequest);
             } else {
                 logger.debug("Referer is not null. Ignoring!");
             }
