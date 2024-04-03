56c571034e9c6a7e2e670bab7f2dcca7cbcc8647
mzouros <michael.zouros@outlook.com>
2024-04-03 19:16:32 +0300

    un-import

--- resource-catalogue-service/src/main/java/gr/uoa/di/madgik/resourcecatalogue/config/CacheConfig.java
+++ resource-catalogue-service/src/main/java/gr/uoa/di/madgik/resourcecatalogue/config/CacheConfig.java
@@ -10,7 +10,6 @@ import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
