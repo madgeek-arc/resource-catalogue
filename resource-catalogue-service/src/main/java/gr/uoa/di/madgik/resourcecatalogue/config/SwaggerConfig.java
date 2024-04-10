584a5d18e66dfa7b444207cb1a0e366be8e96fd3
mzouros <michael.zouros@outlook.com>
2024-04-10 18:00:36 +0300

    Minor refactoring on SwaggerConfig

--- resource-catalogue-service/src/main/java/gr/uoa/di/madgik/resourcecatalogue/config/SwaggerConfig.java
+++ resource-catalogue-service/src/main/java/gr/uoa/di/madgik/resourcecatalogue/config/SwaggerConfig.java
@@ -1,51 +1,48 @@
 package gr.uoa.di.madgik.resourcecatalogue.config;
 
-
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.InputStream;
-import java.util.Properties;
-
 @Configuration
 public class SwaggerConfig {
+
+    private static final Logger logger = LogManager.getLogger(SwaggerConfig.class);
 
     @Value("${swagger.url}")
     String platform;
 
-    @Value("${project.name:Resource Catalogue}")
+    @Value("${project.name}")
     String projectName;
+
+    @Value("${project.version}")
+    String projectVersion;
 
     @Value("${project.debug:false}")
     public boolean isLocalhost;
 
     @Bean
     public OpenAPI springShopOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("Resource Catalogue API")
-                        .description("-- provide description here --")
-                        .version("v5.0.0")
+        OpenAPI openAPI = new OpenAPI()
+                .info(new Info().title(projectName + " API")
+                        .description("A single platform for providers to onboard their organization, register and manage their resources.")
+                        .version(projectVersion)
                         .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
                 .externalDocs(new ExternalDocumentation()
-                        .description("Resource Catalogue Documentation")
+                        .description(projectName + " Documentation")
                         .url("https://github.com/madgeek-arc/resource-catalogue-docs"));
-    }
 
-    private String getVersion() {
-        String ret = null;
-        try (InputStream in = getClass().getResourceAsStream("/META-INF/maven/gr.uoa.di.madgik/resource-catalogue-model/pom.properties")) {
-            Properties props = new Properties();
-            props.load(in);
-            ret = props.getProperty("version");
-        } catch (Throwable ignored) {
-            // Create your own version
-            ret = "3.0.0";
+        if (!isLocalhost) {
+            logger.debug("Hiding methods");
+            // return only @Operation methods
         }
-        return ret == null ? "" : ret;
+
+        return openAPI;
     }
 }
