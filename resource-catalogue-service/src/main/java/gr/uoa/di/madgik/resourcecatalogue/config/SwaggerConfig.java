88e0286c23120b8594249071b49e4f1154803746
Konstantinos Spyrou <spyroukon@gmail.com>
2024-06-11 19:09:27 +0300

    Added: Bearer token authentication in swagger

--- resource-catalogue-service/src/main/java/gr/uoa/di/madgik/resourcecatalogue/config/SwaggerConfig.java
+++ resource-catalogue-service/src/main/java/gr/uoa/di/madgik/resourcecatalogue/config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package gr.uoa.di.madgik.resourcecatalogue.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +17,7 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(SwaggerConfig.class);
+    private final static String SECURITY = "security";
 
     @Value("${catalogue.id}")
     String projectName;
@@ -33,7 +37,14 @@ public class SwaggerConfig {
                         .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
                 .externalDocs(new ExternalDocumentation()
                         .description(projectName + " Documentation")
-                        .url("https://github.com/madgeek-arc/resource-catalogue-docs"));
+                        .url("https://github.com/madgeek-arc/resource-catalogue-docs"))
+//                .addSecurityItem(new SecurityRequirement()
+//                        .addList("bearer-key"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
 
         if (!isLocalhost) {
             logger.debug("Hiding methods");
