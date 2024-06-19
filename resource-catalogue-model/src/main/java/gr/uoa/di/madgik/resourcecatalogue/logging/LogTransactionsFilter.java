824b20b5ddc48f4ed35ed327e51cb4668b347ef7
Konstantinos Spyrou <spyroukon@gmail.com>
2024-06-19 16:02:32 +0300

    fixed logging user info

--- resource-catalogue-model/src/main/java/gr/uoa/di/madgik/resourcecatalogue/logging/LogTransactionsFilter.java
+++ resource-catalogue-model/src/main/java/gr/uoa/di/madgik/resourcecatalogue/logging/LogTransactionsFilter.java
@@ -1,8 +1,12 @@
 package gr.uoa.di.madgik.resourcecatalogue.logging;
 
+import gr.uoa.di.madgik.resourcecatalogue.domain.User;
 import org.slf4j.MDC;
 import org.slf4j.spi.MDCAdapter;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -16,12 +20,14 @@ import java.util.UUID;
 public class LogTransactionsFilter extends AbstractLogContextFilter {
 
     private static final String TRANSACTION_ID = "transaction_id";
+    private static final String USER_INFO = "user_info";
     private static final String REMOTE_IP = "remote_ip";
 
     @Override
     public void editMDC(MDCAdapter mdc, ServletRequest request, ServletResponse response) {
         String transactionId = UUID.randomUUID().toString();
         mdc.put(TRANSACTION_ID, transactionId);
+        appendUserInfo(mdc);
         if (StringUtils.hasText(((HttpServletRequest) request).getHeader("X-Forwarded-For"))) {
             mdc.put(REMOTE_IP, ((HttpServletRequest) request).getHeader("X-Forwarded-For"));
         } else {
@@ -31,5 +37,19 @@ public class LogTransactionsFilter extends AbstractLogContextFilter {
 
     public static String getTransactionId() {
         return MDC.get(TRANSACTION_ID);
+    }
+
+    private void appendUserInfo(MDCAdapter mdc) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            try {
+                User user = User.of(authentication);
+                if (user != null) {
+                    mdc.put(USER_INFO, user.toString());
+                }
+            } catch (InsufficientAuthenticationException e) {
+                mdc.put(USER_INFO, authentication.toString());
+            }
+        }
     }
 }
