c7cabe186203522876106a16425e80c97dcb2396
mzouros <michael.zouros@outlook.com>
2024-07-29 17:13:24 +0300

    Created method to get no of Services per Provider Country on CSV

--- resource-catalogue-rest/src/main/java/gr/uoa/di/madgik/resourcecatalogue/controllers/registry/CSVController.java
+++ resource-catalogue-rest/src/main/java/gr/uoa/di/madgik/resourcecatalogue/controllers/registry/CSVController.java
@@ -6,6 +6,7 @@ import gr.uoa.di.madgik.resourcecatalogue.domain.*;
 import gr.uoa.di.madgik.resourcecatalogue.service.ProviderService;
 import gr.uoa.di.madgik.resourcecatalogue.service.ServiceBundleService;
 import gr.uoa.di.madgik.resourcecatalogue.service.VocabularyService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.logging.log4j.LogManager;
@@ -13,6 +14,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,9 +25,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.*;
 
 @Profile("beyond")
 @RestController
@@ -48,7 +54,7 @@ public class CSVController {
         this.vocabularyService = vocabulary;
     }
 
-    // Downloads a csv file with Provider entries
+    @Operation(summary = "Downloads a csv file with Provider entries")
     @GetMapping(path = "providers", produces = {MediaType.APPLICATION_OCTET_STREAM_VALUE})
     @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_EPOT')")
     public ResponseEntity<String> providersToCSV(@RequestParam(required = false) Boolean published,
@@ -61,7 +67,7 @@ public class CSVController {
         return ResponseEntity.ok(csvData);
     }
 
-    // Downloads a csv file with Service entries
+    @Operation(summary = "Downloads a csv file with Service entries")
     @GetMapping(path = "services", produces = {MediaType.APPLICATION_OCTET_STREAM_VALUE})
     @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_EPOT')")
     public ResponseEntity<String> servicesToCSV(@RequestParam(required = false) Boolean published,
@@ -74,7 +80,7 @@ public class CSVController {
         return ResponseEntity.ok(csvData);
     }
 
-    // Downloads a csv file with Provider entries
+    @Operation(summary = "Downloads a csv file with Vocabulary entries")
     @GetMapping(path = "vocabularies", produces = {MediaType.APPLICATION_OCTET_STREAM_VALUE})
     @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_EPOT')")
     public ResponseEntity<String> vocabulariesToCSV(@Parameter(hidden = true) Authentication auth,
@@ -84,6 +90,78 @@ public class CSVController {
         response.setHeader("Content-disposition", "attachment; filename=" + "vocabularies.csv");
         logger.info("User {} downloaded Vocabularies CSV list", User.of(auth).getEmail());
         return ResponseEntity.ok(csvData);
+    }
+
+    @Operation(summary = "Downloads a csv file with the number of approved services per provider and country, before a specific date")
+    @GetMapping(path = "approvedServicesByProviderAndCountry", produces = {MediaType.APPLICATION_OCTET_STREAM_VALUE})
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_EPOT')")
+    public void servicesPerCountry(@Parameter(description = "Before date (format yyyy-MM-dd)", example = "2023-01-01")
+                                   @RequestParam String date,
+                                   @Parameter(hidden = true) Authentication auth,
+                                   HttpServletResponse response) throws IOException {
+        long timestamp;
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDate localDate = LocalDate.parse(date, formatter);
+            timestamp = localDate.atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli();
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Invalid date format. Please use yyyy-MM-dd.");
+        }
+        Map<String, String> providerIdToCountry = new TreeMap<>();
+        Map<String, String> providerIdToName = new TreeMap<>();
+        Map<String, Integer> providerToServiceCountApprovedBeforeTimestamp = new TreeMap<>();
+        List<String> providerOfServicesWithMalformedLoggingInfo = new ArrayList<>();
+        Paging<ProviderBundle> providers = providerService.getAll(createFacetFilter(false), auth);
+        for (ProviderBundle provider : providers.getResults()) {
+            providerIdToName.put(provider.getId(), provider.getProvider().getName());
+            providerIdToCountry.put(provider.getId(), provider.getProvider().getLocation().getCountry());
+        }
+        Paging<ServiceBundle> services = serviceBundleService.getAll(createFacetFilter(false), auth);
+        for (ServiceBundle service : services.getResults()) {
+            boolean approvedFound = false;
+            boolean rejectedFound = false;
+            String resourceOrganisation = service.getService().getResourceOrganisation();
+            List<LoggingInfo> loggingInfoList = service.getLoggingInfo();
+            for (LoggingInfo loggingInfo : loggingInfoList) {
+                try {
+                    if (loggingInfo.getActionType().equals("rejected")) {
+                        rejectedFound = true;
+                    }
+                    if (loggingInfo.getActionType().equals("approved")) {
+                        approvedFound = true;
+                        long dateInMillis = Long.parseLong(loggingInfo.getDate());
+                        if (dateInMillis < timestamp) {
+                            providerToServiceCountApprovedBeforeTimestamp.put(resourceOrganisation,
+                                    providerToServiceCountApprovedBeforeTimestamp.getOrDefault(resourceOrganisation, 0) + 1);
+                        }
+                        break;
+                    }
+                } catch (Exception e) {
+                    if (!providerOfServicesWithMalformedLoggingInfo.contains(resourceOrganisation)
+                            && service.getStatus().equals("approved resource")) {
+                        providerOfServicesWithMalformedLoggingInfo.add(resourceOrganisation);
+                    }
+                }
+            }
+            // for old registries with no or malformed status
+            if (!approvedFound && !rejectedFound) {
+                providerToServiceCountApprovedBeforeTimestamp.put(resourceOrganisation,
+                        providerToServiceCountApprovedBeforeTimestamp.getOrDefault(resourceOrganisation, 0) + 1);
+            }
+        }
+
+        // for old registries with no or malformed logging info
+        for (String resourceOrganisation : providerOfServicesWithMalformedLoggingInfo) {
+            providerToServiceCountApprovedBeforeTimestamp.put(resourceOrganisation,
+                    providerToServiceCountApprovedBeforeTimestamp.getOrDefault(resourceOrganisation, 0) + 1);
+        }
+
+        listNumberOfServicesPerProviderCountryToCSV(providerIdToCountry, providerIdToName,
+                providerToServiceCountApprovedBeforeTimestamp, response);
+
+//        for (Map.Entry<String, Integer> entry : providerToServiceCountApprovedBeforeTimestamp.entrySet()) {
+//            logger.info(providerIdToName.get(entry.getKey()) + " : " + providerIdToCountry.get(entry.getKey()) + " : " + entry.getValue());
+//        }
     }
 
     private FacetFilter createFacetFilter(Boolean published) {
@@ -149,13 +227,53 @@ public class CSVController {
         return csv.toString();
     }
 
+    private static void listNumberOfServicesPerProviderCountryToCSV(Map<String, String> providerIdToCountry,
+                                                                    Map<String, String> providerIdToName,
+                                                                    Map<String, Integer> providerToServiceCountApprovedBeforeTimestamp,
+                                                                    HttpServletResponse response) throws IOException {
+        // Sort the entries by country
+        List<Map.Entry<String, Integer>> sortedEntries = providerToServiceCountApprovedBeforeTimestamp.entrySet().stream()
+                .sorted((entry1, entry2) -> {
+                    String country1 = providerIdToCountry.get(entry1.getKey());
+                    String country2 = providerIdToCountry.get(entry2.getKey());
+                    return country1.compareTo(country2);
+                })
+                .toList();
+
+        // Create CSV content
+        StringBuilder csvContent = new StringBuilder();
+        csvContent.append("Provider Name,Country,Service Count\n");
+        for (Map.Entry<String, Integer> entry : providerToServiceCountApprovedBeforeTimestamp.entrySet()) {
+            String providerName = formatCSVField(providerIdToName.get(entry.getKey()));
+            String country = formatCSVField(providerIdToCountry.get(entry.getKey()));
+            String serviceCount = entry.getValue().toString();
+
+            csvContent.append(providerName)
+                    .append(',')
+                    .append(country)
+                    .append(',')
+                    .append(serviceCount)
+                    .append('\n');
+        }
+
+        // Set the response headers
+        response.setContentType("text/csv");
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"services_per_country.csv\"");
+
+        // Write the CSV content to the response
+        response.getWriter().write(csvContent.toString());
+        response.getWriter().flush();
+    }
+
     private static String formatCSVField(String field) {
         if (field == null) return "";
-        if (field.contains(",")) {
-            // If the field contains commas, enclose it in double quotes
-            return "\"" + field + "\"";
-        } else {
-            return field;
+        if (field.contains("\"")) {
+            field = field.replace("\"", "\"\"");
         }
+        if (field.contains(",") || field.contains("\"") || field.contains("\n")) {
+            field = "\"" + field + "\"";
+        }
+        return field;
     }
 }
