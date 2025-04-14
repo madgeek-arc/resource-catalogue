6fe04079da0f80c9c1c4b2468ce20ca638e57cf6
Konstantinos Spyrou <spyroukon@gmail.com>
2025-04-14 17:30:43 +0300

    chore: remove definition of default namespace

--- resource-catalogue-model-lot1/src/main/java/gr/uoa/di/madgik/resourcecatalogue/domain/package-info.java
+++ resource-catalogue-model-lot1/src/main/java/gr/uoa/di/madgik/resourcecatalogue/domain/package-info.java
@@ -14,14 +14,6 @@
  * limitations under the License.
  */
 
-@XmlSchema(
-        namespace = "http://einfracentral.eu",
-        elementFormDefault = XmlNsForm.QUALIFIED,
-        xmlns = {
-                @XmlNs(prefix = "tns", namespaceURI = "http://einfracentral.eu")//,
-                //@XmlNs(prefix = "ns0", namespaceURI = "http://einfracentral.eu")
-        }
-)
 @XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
 @XmlAccessorType(XmlAccessType.FIELD)
 package gr.uoa.di.madgik.resourcecatalogue.domain;
