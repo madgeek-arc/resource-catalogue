7326fd0585671a916f199785d4e6537382a3bde6
mike <michael.zouros@outlook.com>
2025-07-14 16:39:08 +0300

    feature: deployable service initial commit

---
+++ resource-catalogue-api/src/main/java/gr/uoa/di/madgik/resourcecatalogue/service/DeployableServiceService.java
@@ -0,0 +1,7 @@
+package gr.uoa.di.madgik.resourcecatalogue.service;
+
+import gr.uoa.di.madgik.resourcecatalogue.domain.DeployableServiceBundle;
+
+public interface DeployableServiceService extends ResourceCatalogueService<DeployableServiceBundle>,
+        BundleOperations<DeployableServiceBundle> {
+}
