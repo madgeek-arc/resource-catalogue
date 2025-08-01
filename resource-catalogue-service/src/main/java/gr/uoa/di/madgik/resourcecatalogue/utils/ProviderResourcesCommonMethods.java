48e046fe2fd370ef63f92ebd7df1700d569d0da7
mike <michael.zouros@outlook.com>
2025-08-01 18:01:36 +0300

    refactor: remove unnecessary method

--- resource-catalogue-service/src/main/java/gr/uoa/di/madgik/resourcecatalogue/utils/ProviderResourcesCommonMethods.java
+++ resource-catalogue-service/src/main/java/gr/uoa/di/madgik/resourcecatalogue/utils/ProviderResourcesCommonMethods.java
@@ -30,10 +30,6 @@ import org.springframework.security.authentication.InsufficientAuthenticationExc
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @Component
@@ -368,16 +364,5 @@ public class ProviderResourcesCommonMethods {
             users.add(authUser);
             adapter.setAdmins(new ArrayList<>(users));
         }
-    }
-
-    public String transformTimestampToDate(String timestampStr) {
-        long timestamp = Long.parseLong(timestampStr);
-
-        LocalDate date = Instant.ofEpochMilli(timestamp)
-                .atZone(ZoneId.systemDefault())
-                .toLocalDate();
-
-        // Format as "YYYY-MM-DD"
-        return date.format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 }
