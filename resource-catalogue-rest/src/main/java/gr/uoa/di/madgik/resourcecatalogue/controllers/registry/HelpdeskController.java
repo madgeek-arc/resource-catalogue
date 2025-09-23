ddcb81e0d4044853d9892fe8e66a8db480a2732f
Michael Zouros <michael.zouros@outlook.com>
2025-09-23 13:09:20 +0300

    refactor: replace string concatenation with uriBuilder

--- resource-catalogue-rest/src/main/java/gr/uoa/di/madgik/resourcecatalogue/controllers/registry/HelpdeskController.java
+++ resource-catalogue-rest/src/main/java/gr/uoa/di/madgik/resourcecatalogue/controllers/registry/HelpdeskController.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017-2025 OpenAIRE AMKE & Athena Research and Innovation Center
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package gr.uoa.di.madgik.resourcecatalogue.controllers.registry;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -37,7 +53,9 @@ public class HelpdeskController {
     public ResponseEntity<Object> getTicket(@PathVariable("ticketId") String ticketId) {
         try {
             Object ticket = webClient.get()
-                    .uri(helpdeskEndpoint + "/tickets/{ticketId}", ticketId)
+                    .uri(uriBuilder -> uriBuilder
+                            .path(helpdeskEndpoint + "/tickets/{ticketId}")
+                            .build(ticketId))
                     .retrieve()
                     .bodyToMono(Object.class)
                     .block();
@@ -83,7 +101,9 @@ public class HelpdeskController {
 
         try {
             Object response = webClient.post()
-                    .uri(helpdeskEndpoint + "/tickets/" + ticketId + "/articles")
+                    .uri(uriBuilder -> uriBuilder
+                            .path(helpdeskEndpoint + "/tickets/{ticketId}/articles")
+                            .build(ticketId))
                     .bodyValue(articleData)
                     .retrieve()
                     .bodyToMono(Object.class)
