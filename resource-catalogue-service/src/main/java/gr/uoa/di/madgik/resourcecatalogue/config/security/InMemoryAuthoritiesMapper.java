921a3cd7ebadc838ee92c931ba012230acff47e8
Konstantinos Spyrou <spyroukon@gmail.com>
2026-05-05 12:22:55 +0300

    refactor: change security configuration for lot1 build and no-auth profile

--- resource-catalogue-service/src/main/java/gr/uoa/di/madgik/resourcecatalogue/config/security/InMemoryAuthoritiesMapper.java
+++ resource-catalogue-service/src/main/java/gr/uoa/di/madgik/resourcecatalogue/config/security/InMemoryAuthoritiesMapper.java
@@ -29,6 +29,7 @@ import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
@@ -42,6 +43,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Profile("!no-auth")
 @Component
 public class InMemoryAuthoritiesMapper implements AuthoritiesMapper {
 
