0355609cda527731d77c926b9f6345661f5352c3
mike <michael.zouros@outlook.com>
2026-05-18 13:01:47 +0300

    fix: correct exception catch

--- resource-catalogue-service/src/main/java/gr/uoa/di/madgik/resourcecatalogue/manager/aspects/PostProcessingAspect.java
+++ resource-catalogue-service/src/main/java/gr/uoa/di/madgik/resourcecatalogue/manager/aspects/PostProcessingAspect.java
@@ -17,6 +17,7 @@
 package gr.uoa.di.madgik.resourcecatalogue.manager.aspects;
 
 import gr.uoa.di.madgik.catalogue.exception.ValidationException;
+import gr.uoa.di.madgik.registry.exception.ResourceNotFoundException;
 import gr.uoa.di.madgik.registry.service.GenericResourceService;
 import gr.uoa.di.madgik.registry.exception.ResourceException;
 import gr.uoa.di.madgik.resourcecatalogue.domain.*;
@@ -145,7 +146,7 @@ public class PostProcessingAspect {
                         hle.getId(), hle.getName());
                 vocabularyService.update(hle, null);
             }
-        } catch (ResourceException e) {
+        } catch (ResourceNotFoundException e) {
             hle = new Vocabulary();
             hle.setId(hleId);
             hle.setName(name);
